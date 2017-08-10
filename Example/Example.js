facc23342634625bad3f02639b60f4336c62db8a
Krzysztof Magiera <krzys@swmansion.com>
2017-08-10 14:19:08 +0200

    Swipeable row in example

--- Example/Example.js
+++ Example/Example.js
@@ -31,6 +31,8 @@ import {
   BorderlessButton,
 } from 'react-native-gesture-handler';
 
+const USE_NATIVE_DRIVER = true;
+
 const UNDERLAY_REF = 'UNDERLAY_REF';
 const CHILD_REF = 'CHILD_REF';
 
@@ -304,6 +306,102 @@ class PinchableBox extends React.Component {
   }
 }
 
+const RATIO = 3;
+
+class Swipeable extends Component {
+  constructor(props) {
+    super(props);
+    this._width = 0;
+    this._dragX = new Animated.Value(0);
+    this._transX = this._dragX.interpolate({
+      inputRange: [0, RATIO],
+      outputRange: [0, 1],
+    });
+    this._showLeftAction = this._dragX.interpolate({
+      inputRange: [-1, 0, 1],
+      outputRange: [0, 0, 1],
+    });
+    this._showRightAction = this._dragX.interpolate({
+      inputRange: [-1, 0, 1],
+      outputRange: [1, 0, 0],
+    });
+    this._onGestureEvent = Animated.event(
+      [{ nativeEvent: { translationX: this._dragX } }],
+      { useNativeDriver: USE_NATIVE_DRIVER }
+    );
+  }
+  _onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      const dragToss = 0.05;
+      const endOffsetX =
+        event.nativeEvent.translationX + dragToss * event.nativeEvent.velocityX;
+
+      let toValue = 0;
+      if (endOffsetX > this._width / 2) {
+        toValue = this._width * RATIO;
+      } else if (endOffsetX < -this._width / 2) {
+        toValue = -this._width * RATIO;
+      }
+
+      Animated.spring(this._dragX, {
+        velocity: event.nativeEvent.velocityX,
+        tension: 15,
+        friction: 5,
+        toValue,
+        useNativeDriver: USE_NATIVE_DRIVER,
+      }).start();
+    }
+  };
+  _onLayout = event => {
+    this._width = event.nativeEvent.layout.width;
+  };
+  _reset = () => {
+    Animated.spring(this._dragX, {
+      toValue: 0,
+      useNativeDriver: USE_NATIVE_DRIVER,
+      tension: 15,
+      friction: 5,
+    }).start();
+  };
+  render() {
+    const { children } = this.props;
+    return (
+      <View>
+        <Animated.View
+          style={[styles.rowAction, { opacity: this._showLeftAction }]}>
+          <RectButton
+            style={[styles.rowAction, styles.leftAction]}
+            onPress={this._reset}>
+            <Text style={styles.actionButtonText}>Green</Text>
+          </RectButton>
+        </Animated.View>
+        <Animated.View
+          style={[styles.rowAction, { opacity: this._showRightAction }]}>
+          <RectButton
+            style={[styles.rowAction, styles.rightAction]}
+            onPress={this._reset}>
+            <Text style={styles.actionButtonText}>Red</Text>
+          </RectButton>
+        </Animated.View>
+        <PanGestureHandler
+          {...this.props}
+          minDeltaX={10}
+          onGestureEvent={this._onGestureEvent}
+          onHandlerStateChange={this._onHandlerStateChange}>
+          <Animated.View
+            style={{
+              backgroundColor: 'white',
+              transform: [{ translateX: this._transX }],
+            }}
+            onLayout={this._onLayout}>
+            {children}
+          </Animated.View>
+        </PanGestureHandler>
+      </View>
+    );
+  }
+}
+
 const InfoButton = props =>
   <BorderlessButton
     {...props}
@@ -392,19 +490,21 @@ export default class Example extends Component {
           <PressBox />
           <ControlledSwitch />
           <View style={styles.table}>
-            <RectButton
-              style={styles.rectButton}
-              onPress={() => Alert.alert('First row clicked')}>
-              <Text style={styles.buttonText}>
-                Observe highlight delay on the row
-              </Text>
-              {/* Info icon will cancel when you scroll in the direction of the scrollview
+            <Swipeable>
+              <RectButton
+                style={styles.rectButton}
+                onPress={() => Alert.alert('First row clicked')}>
+                <Text style={styles.buttonText}>
+                  Swipe this row & observe highlight delay
+                </Text>
+                {/* Info icon will cancel when you scroll in the direction of the scrollview
                   but if you move finger horizontally it would allow you to "re-enter" into
                   an active state. This is typical for most of the buttons on iOS (but not
                   on Android where the touch cancels as soon as you leave the area of the
                   button). */}
-              <InfoButton name="first" />
-            </RectButton>
+                <InfoButton name="first" />
+              </RectButton>
+            </Swipeable>
             <View style={styles.buttonDelimiter} />
             <RectButton
               style={styles.rectButton}
@@ -455,6 +555,21 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  rowAction: {
+    ...StyleSheet.absoluteFillObject,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  leftAction: {
+    backgroundColor: '#4CAF50',
+  },
+  rightAction: {
+    backgroundColor: '#F44336',
+  },
+  actionButtonText: {
+    color: 'white',
+    fontSize: 16,
   },
   table: {
     marginTop: 20,
