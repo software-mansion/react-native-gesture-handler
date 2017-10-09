bea754fd0f633174fdee935206b1d050459b344b
Krzysztof Magiera <krzys@swmansion.com>
2017-10-09 12:34:58 +0200

    Add vector icons to example apps

--- Example/android/app/src/main/java/com/swmansion/gesturehandler/react/example/MainApplication.java
+++ Example/android/app/src/main/java/com/swmansion/gesturehandler/react/example/MainApplication.java
@@ -3,6 +3,7 @@ package com.swmansion.gesturehandler.react.example;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
           new RNGestureHandlerPackage()
       );
     }
