13908d2e304ca519fe0ca52c991b845e99b31331
osdnk <micosa97@gmail.com>
2018-04-14 19:54:20 +0200

    Remove useless code and style issue

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.java
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.java
@@ -71,7 +71,7 @@ public class RNGestureHandlerModule extends ReactContextBaseJavaModule {
   private static final String KEY_PAN_MIN_POINTERS = "minPointers";
   private static final String KEY_PAN_MAX_POINTERS = "maxPointers";
   private static final String KEY_PAN_AVG_TOUCHES = "avgTouches";
-  private static final String KEY_NUMBER_OF_TOUCHES= "numberOfTouches";
+  private static final String KEY_NUMBER_OF_TOUCHES = "numberOfTouches";
   private static final String KEY_DIRECTION= "direction";
 
   private abstract static class HandlerFactory<T extends GestureHandler>
--- ios/RNGestureHandlerEvents.m
+++ ios/RNGestureHandlerEvents.m
@@ -23,16 +23,6 @@
                            @"absoluteY": @(absolutePosition.y)}];
 }
 
-+ (RNGestureHandlerEventExtraData *) withNumberOfTouches:(NSUInteger)numberOfTouches
-                                           withDirection:(UISwipeGestureRecognizerDirection)direction
-{
-    return [[RNGestureHandlerEventExtraData alloc]
-            initWithData:@{
-                           @"numberOfTouches": @(numberOfTouches),
-                           @"direction": @(direction)}];
-}
-
-
 + (RNGestureHandlerEventExtraData *)forPan:(CGPoint)position
                       withAbsolutePosition:(CGPoint)absolutePosition
                            withTranslation:(CGPoint)translation
