750726d839836a1b3a67e1f72c1b7ba99b998658
osdnk <micosa97@gmail.com>
2018-04-14 20:14:48 +0200

    Shadow useless YellowBox

--- Example/App.js
+++ Example/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet } from 'react-native';
+import { Text, View, FlatList, StyleSheet, YellowBox } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
@@ -15,6 +15,13 @@ import Bouncing from './bouncing';
 import HorizontalDrawer from './horizontalDrawer';
 import ChatHeads from './chatHeads';
 import { ComboWithGHScroll, ComboWithRNScroll } from './combo';
+
+YellowBox.ignoreWarnings([
+  'Warning: isMounted(...) is deprecated',
+  'Module RCTImageLoader',
+]);
+// refers to bug in React Navigation which should be fixed soon
+// https://github.com/react-navigation/react-navigation/issues/3956
 
 const SCREENS = {
   Rows: { screen: Rows, title: 'Table rows & buttons' },
