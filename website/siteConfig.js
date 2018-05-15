dddf537d942122a0f82c2183825da635c9a916c2
Krzysztof Magiera <krzys@swmansion.com>
2018-05-15 17:37:33 +0200

    Update highlight theme

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,7 +32,7 @@ const siteConfig = {
 
   highlight: {
     // Highlight.js theme to use for syntax highlighting in code blocks
-    theme: 'atom-one-dark',
+    theme: 'rainbow',
   },
 
   // Add custom scripts here that would be placed in <script> tags
