cd49d5767c5fb996d648f775ec0636ace08b90a1
Krzysztof Magiera <krzys@swmansion.com>
2018-05-15 22:51:31 +0200

    Very last final touches I swear

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -45,8 +45,8 @@ const siteConfig = {
   onPageNav: 'separate',
 
   /* Open Graph and Twitter card images */
-  ogImage: 'img/docusaurus.png',
-  twitterImage: 'img/docusaurus.png',
+  ogImage: 'img/ghlogo.png',
+  twitterImage: 'img/ghlogo.png',
 
   // You may provide arbitrary config keys to be used as needed by your
   // template. For example, if you need your repo's URL...
