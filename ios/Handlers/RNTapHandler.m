91d9a4c236b6c52fc557eeaa38bac23b5a4f3000
osdnk <micosa97@gmail.com>
2018-07-06 23:34:49 +0200

    Add touchesMoved invoke to touches began

--- ios/Handlers/RNTapHandler.m
+++ ios/Handlers/RNTapHandler.m
@@ -37,131 +37,136 @@
 @end
 
 @implementation RNBetterTapGestureRecognizer {
-    __weak RNGestureHandler *_gestureHandler;
-    NSUInteger _tapsSoFar;
+  __weak RNGestureHandler *_gestureHandler;
+  NSUInteger _tapsSoFar;
 }
 
 - (id)initWithGestureHandler:(RNGestureHandler*)gestureHandler
 {
-    if ((self = [super initWithTarget:gestureHandler action:@selector(handleGesture:)])) {
-        _gestureHandler = gestureHandler;
-        _tapsSoFar = 0;
-        _numberOfTaps = 1;
-        _maxDelay = 0.2;
-        _minPointers = 1;
-        _maxDuration = NAN;
-        _maxDeltaX = NAN;
-        _maxDeltaY = NAN;
-        _maxDistSq = NAN;
-        _maxNumberOfTouches = 0;
-        super.minimumNumberOfTouches = 20;
-    }
-    return self;
+  if ((self = [super initWithTarget:gestureHandler action:@selector(handleGesture:)])) {
+    _gestureHandler = gestureHandler;
+    _tapsSoFar = 0;
+    _numberOfTaps = 1;
+    _maxDelay = 0.2;
+    _minPointers = 1;
+    _maxDuration = NAN;
+    _maxDeltaX = NAN;
+    _maxDeltaY = NAN;
+    _maxDistSq = NAN;
+    _maxNumberOfTouches = 0;
+    super.minimumNumberOfTouches = 20;
+  }
+  return self;
 }
 
 - (void)triggerAction
 {
-    [_gestureHandler handleGesture:self];
+  [_gestureHandler handleGesture:self];
 }
 
 - (void)cancel
 {
-    self.enabled = NO;
+  self.enabled = NO;
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    _tapsSoFar++;
-    NSInteger numberOfTouches = [touches count];
-    if (numberOfTouches > _maxNumberOfTouches) {
-        _maxNumberOfTouches = numberOfTouches;
-    }
-    if (_tapsSoFar) {
-        [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(cancel) object:nil];
-    }
-    if (!isnan(_maxDuration)) {
-        [self performSelector:@selector(cancel) withObject:nil afterDelay:_maxDuration];
-    }
-
-    [super touchesBegan:touches withEvent:event];
+  _tapsSoFar++;
+  NSInteger numberOfTouches = [touches count];
+  if (numberOfTouches > _maxNumberOfTouches) {
+    _maxNumberOfTouches = numberOfTouches;
+  }
+  if (_tapsSoFar) {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(cancel) object:nil];
+  }
+  if (!isnan(_maxDuration)) {
+    [self performSelector:@selector(cancel) withObject:nil afterDelay:_maxDuration];
+  }
+  
+  [super touchesBegan:touches withEvent:event];
+  // workaround below refers to error with recognizing gesture if there was no move event
+  // beetween touching down and up. As in TapHandler it's not a special case (contrary to PanHandler)
+  // the first touch (down event) has to be handled in the same way as move
+  // in order to allow handle to change its state not only if there was some move event during recognizing
+  [super touchesMoved:touches withEvent:event];
+    
 }
 
 - (void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [super touchesMoved:touches withEvent:event];
-    NSInteger numberOfTouches = [touches count];
-    if (self.state != UIGestureRecognizerStatePossible) {
-        return;
-    }
-    
-    if (numberOfTouches > _maxNumberOfTouches) {
-        _maxNumberOfTouches = numberOfTouches;
-    }
-
-    
-    if ([self shouldFailUnderCustomCriteria]) {
-        self.state = UIGestureRecognizerStateFailed;
-        [self triggerAction];
-        [self reset];
-        return;
-    }
-
-    self.state = UIGestureRecognizerStatePossible;
-    [self triggerAction];
+  [super touchesMoved:touches withEvent:event];
+  NSInteger numberOfTouches = [touches count];
+  if (self.state != UIGestureRecognizerStatePossible) {
     return;
+  }
+  
+  if (numberOfTouches > _maxNumberOfTouches) {
+    _maxNumberOfTouches = numberOfTouches;
+  }
+  
+  if ([self shouldFailUnderCustomCriteria]) {
+    self.state = UIGestureRecognizerStateFailed;
+    [self triggerAction];
+    [self reset];
+    return;
+  }
+  
+  self.state = UIGestureRecognizerStatePossible;
+  [self triggerAction];
+  return;
 }
 
 - (BOOL)shouldFailUnderCustomCriteria
 {
-    if (_gestureHandler.shouldCancelWhenOutside) {
-        CGPoint pt = [self locationInView:self.view];
-        if (!CGRectContainsPoint(self.view.bounds, pt)) {
-            return YES;
-        }
+  if (_gestureHandler.shouldCancelWhenOutside) {
+    CGPoint pt = [self locationInView:self.view];
+    if (!CGRectContainsPoint(self.view.bounds, pt)) {
+      return YES;
     }
-
-    CGPoint trans = [self translationInView:self.view];
-    if (TEST_MAX_IF_NOT_NAN(fabs(trans.x), _maxDeltaX)) {
-        return YES;
-    }
-    if (TEST_MAX_IF_NOT_NAN(fabs(trans.y), _maxDeltaY)) {
-        return YES;
-    }
-    if (TEST_MAX_IF_NOT_NAN(fabs(trans.y * trans.y + trans.x + trans.x), _maxDistSq)) {
-        return YES;
-    }
-    return NO;
+  }
+  
+  CGPoint trans = [self translationInView:self.view];
+  if (TEST_MAX_IF_NOT_NAN(fabs(trans.x), _maxDeltaX)) {
+    return YES;
+  }
+  if (TEST_MAX_IF_NOT_NAN(fabs(trans.y), _maxDeltaY)) {
+    return YES;
+  }
+  if (TEST_MAX_IF_NOT_NAN(fabs(trans.y * trans.y + trans.x + trans.x), _maxDistSq)) {
+    return YES;
+  }
+  return NO;
 }
 
 - (void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    if (_numberOfTaps == _tapsSoFar && _maxNumberOfTouches >= _minPointers) {
-        self.state = UIGestureRecognizerStateBegan;
-        [super touchesEnded:touches withEvent:event];
-        self.state = UIGestureRecognizerStateEnded;
-        [self reset];
-    } else {
-        [self performSelector:@selector(cancel) withObject:nil afterDelay:_maxDelay];
-    }
+  if (_numberOfTaps == _tapsSoFar && _maxNumberOfTouches >= _minPointers) {
+    self.state = UIGestureRecognizerStateBegan;
+    [super touchesEnded:touches withEvent:event];
+    self.state = UIGestureRecognizerStateEnded;
+    [self reset];
+  } else {
+    [self performSelector:@selector(cancel) withObject:nil afterDelay:_maxDelay];
+  }
 }
 
 - (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
-    [super touchesCancelled:touches withEvent:event];
-    self.state = UIGestureRecognizerStateCancelled;
-    [self reset];
+  [super touchesCancelled:touches withEvent:event];
+  self.state = UIGestureRecognizerStateCancelled;
+  [self reset];
 }
 
 - (void)reset
 {
-    if (self.state == UIGestureRecognizerStateFailed) {
-        [self triggerAction];
-    }
-    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(cancel) object:nil];
-    _tapsSoFar = 0;
-    _maxNumberOfTouches = 0;
-    self.enabled = YES;
-    [super reset];
+  if (self.state == UIGestureRecognizerStateFailed) {
+    [self triggerAction];
+  }
+  [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(cancel) object:nil];
+  _tapsSoFar = 0;
+  _maxNumberOfTouches = 0;
+  self.enabled = YES;
+  [super reset];
 }
 
 @end
@@ -170,38 +175,38 @@
 
 - (instancetype)initWithTag:(NSNumber *)tag
 {
-    if ((self = [super initWithTag:tag])) {
-        _recognizer = [[RNBetterTapGestureRecognizer alloc] initWithGestureHandler:self];
-    }
-    return self;
+  if ((self = [super initWithTag:tag])) {
+    _recognizer = [[RNBetterTapGestureRecognizer alloc] initWithGestureHandler:self];
+  }
+  return self;
 }
 
 - (void)configure:(NSDictionary *)config
 {
-    [super configure:config];
-    RNBetterTapGestureRecognizer *recognizer = (RNBetterTapGestureRecognizer *)_recognizer;
-
-    APPLY_INT_PROP(numberOfTaps);
-    APPLY_INT_PROP(minPointers);
-    APPLY_FLOAT_PROP(maxDeltaX);
-    APPLY_FLOAT_PROP(maxDeltaY);
-
-    id prop = config[@"maxDelayMs"];
-    if (prop != nil) {
-        recognizer.maxDelay = [RCTConvert CGFloat:prop] / 1000.0;
-    }
-
-    prop = config[@"maxDurationMs"];
-    if (prop != nil) {
-        recognizer.maxDuration = [RCTConvert CGFloat:prop] / 1000.0;
-    }
-
-    prop = config[@"maxDist"];
-    if (prop != nil) {
-        CGFloat dist = [RCTConvert CGFloat:prop];
-        recognizer.maxDistSq = dist * dist;
-    }
-
+  [super configure:config];
+  RNBetterTapGestureRecognizer *recognizer = (RNBetterTapGestureRecognizer *)_recognizer;
+  
+  APPLY_INT_PROP(numberOfTaps);
+  APPLY_INT_PROP(minPointers);
+  APPLY_FLOAT_PROP(maxDeltaX);
+  APPLY_FLOAT_PROP(maxDeltaY);
+  
+  id prop = config[@"maxDelayMs"];
+  if (prop != nil) {
+    recognizer.maxDelay = [RCTConvert CGFloat:prop] / 1000.0;
+  }
+  
+  prop = config[@"maxDurationMs"];
+  if (prop != nil) {
+    recognizer.maxDuration = [RCTConvert CGFloat:prop] / 1000.0;
+  }
+  
+  prop = config[@"maxDist"];
+  if (prop != nil) {
+    CGFloat dist = [RCTConvert CGFloat:prop];
+    recognizer.maxDistSq = dist * dist;
+  }
+  
 }
 
 @end
