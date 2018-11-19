77491194049b82f26f358a09abc662ef27265527
Erivelton Elias <eriveltonelias@icloud.com>
2018-11-19 10:42:42 -0200

    remove cycles imports (#340)
    this PR is related with https://github.com/kmagiera/react-native-gesture-handler/issues/300

--- Example/App.js
+++ Example/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Text, View, FlatList, StyleSheet, YellowBox } from 'react-native';
-import { StackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 import { RectButton, ScrollView } from 'react-native-gesture-handler';
 
 import SwipeableTable from './swipeable';
@@ -111,7 +111,7 @@ class MainScreenItem extends React.Component {
   }
 }
 
-const ExampleApp = StackNavigator(
+const ExampleApp = createStackNavigator(
   {
     Main: { screen: MainScreen },
     ...SCREENS,
