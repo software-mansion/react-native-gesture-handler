1d400548ee591a2567307a5742557af5f9bcb328
osdnk <micosa97@gmail.com>
2018-12-12 13:21:18 +0100

    XD

--- Example/test/e2e/sample.spec.js
+++ Example/test/e2e/sample.spec.js
@@ -1,5 +1,11 @@
 describe('Example', () => {
   beforeEach(async () => {
+    if (typeof device === 'undefined') {
+      const detox = require('detox');
+      const config = require('../../package.json').detox;
+      await detox.init(config);
+    }
+    // XD
     await device.reloadReactNative();
   });
   it('should pass', async () => {
