ee685df5c2dc3e786e45df10f76a08cce62e2271
Julian Hundeloh <julian@hundeloh-consulting.ch>
2019-01-14 15:26:18 +0100

    fix: use responder events

--- GestureHandler.web.js
+++ GestureHandler.web.js
@@ -7,54 +7,25 @@ import {
   ToolbarAndroid,
   ViewPagerAndroid,
   DrawerLayoutAndroid,
-  WebView,
   FlatList,
   TouchableWithoutFeedback,
-  findNodeHandle,
 } from 'react-native';
 import gestureHandlerRootHOC from './gestureHandlerRootHOC';
 import Directions from './Directions';
 import State from './State';
 
-// Factory for Handler components
-function createHandler(name, attachNativeHandler) {
-  return class Handler extends React.Component {
+// Factory for stub Handler components
+function createStubHandler(name) {
+  return class NativeViewGestureHandler extends React.Component {
     static displayName = name;
 
     container = React.createRef();
-    detachNativeHandler = null;
-
-    componentDidMount() {
-      // Get current DOM node
-      const node = findNodeHandle(this.container.current);
-
-      // Attach handler to DOM node
-      const { enabled } = this.props;
-      if (node && attachNativeHandler && enabled !== false) {
-        this.detachNativeHandler = attachNativeHandler(node, this.props);
-      }
-    }
-
-    componentDidUpdate() {
-      // Get current DOM node
-      const node = findNodeHandle(this.container.current);
-
-      if (node && attachNativeHandler) {
-        // Detach existing handlers
-        if (this.detachNativeHandler) {
-          this.detachNativeHandler(node);
-        }
-
-        // Attach handler to DOM node again
-        const { enabled } = this.props;
-        if (enabled !== false) {
-          attachNativeHandler(node, this.props);
-        }
-      }
-    }
 
     setNativeProps() {
-      // No implementation so far but is needed to avoid null calls
+      // Since this is a stub we do not need to pass on native props.
+      // However, we need to implement it here to avoid null calls.
+      // If for any reason we need to pass on native props we can fallback to this:
+      // this.container.current.setNativeProps(...args)
     }
 
     render() {
@@ -72,47 +43,64 @@ function createHandler(name, attachNativeHandler) {
 
 // Create all Handler components with their respective handler functions
 // (at the moment only TapGestureHandler is properly supported)
-const NativeViewGestureHandler = createHandler('NativeViewGestureHandler');
-const TapGestureHandler = createHandler('TapGestureHandler', (node, props) => {
-  const { onHandlerStateChange = () => {} } = props;
-  const clickHandler = ({ x }) =>
-    onHandlerStateChange({
-      nativeEvent: {
-        oldState: State.ACTIVE,
-        state: State.UNDETERMINED,
-        x,
-      },
-    });
-  node.addEventListener('click', clickHandler);
+const NativeViewGestureHandler = createStubHandler('NativeViewGestureHandler');
 
-  // Detach event listeners
-  return () => {
-    node.removeEventListener('click', clickHandler);
-  };
-});
-const FlingGestureHandler = createHandler('FlingGestureHandler');
-const ForceTouchGestureHandler = createHandler('ForceTouchGestureHandler');
-const LongPressGestureHandler = createHandler('LongPressGestureHandler');
-const PanGestureHandler = createHandler('PanGestureHandler');
-const PinchGestureHandler = createHandler('PinchGestureHandler');
-const RotationGestureHandler = createHandler('RotationGestureHandler');
+class TapGestureHandler extends React.Component {
+  setNativeProps() {}
 
-// Factory for Button component
+  render() {
+    const { children, enabled, onHandlerStateChange, style } = this.props;
+
+    return (
+      <TouchableWithoutFeedback
+        style={style}
+        onPress={({ x }) => {
+          enabled !== false &&
+            onHandlerStateChange &&
+            onHandlerStateChange({
+              nativeEvent: {
+                oldState: State.ACTIVE,
+                state: State.UNDETERMINED,
+                x,
+              },
+            });
+        }}>
+        {children}
+      </TouchableWithoutFeedback>
+    );
+  }
+}
+
+const FlingGestureHandler = createStubHandler('FlingGestureHandler');
+
+const ForceTouchGestureHandler = createStubHandler('ForceTouchGestureHandler');
+
+const LongPressGestureHandler = createStubHandler('LongPressGestureHandler');
+
+const PanGestureHandler = createStubHandler('PanGestureHandler');
+
+const PinchGestureHandler = createStubHandler('PinchGestureHandler');
+
+const RotationGestureHandler = createStubHandler('RotationGestureHandler');
+
+// Factory for stub Button component
 // (at the moment this is a plain TouchableWithoutFeedback)
-function createButton(name) {
+function createStubButton(name) {
   return class Button extends React.Component {
     static displayName = name;
 
     render() {
-      return <TouchableWithoutFeedback {...this.props} />;
+      return (
+        <TouchableWithoutFeedback accessibilityRole="button" {...this.props} />
+      );
     }
   };
 }
 
-const RawButton = createButton('RawButton');
-const BaseButton = createButton('BaseButton');
-const RectButton = createButton('RectButton');
-const BorderlessButton = createButton('BorderlessButton');
+const RawButton = createStubButton('RawButton');
+const BaseButton = createStubButton('BaseButton');
+const RectButton = createStubButton('RectButton');
+const BorderlessButton = createStubButton('BorderlessButton');
 
 // Export same components as in GestureHandler.js
 export {
@@ -123,7 +111,6 @@ export {
   ToolbarAndroid,
   ViewPagerAndroid,
   DrawerLayoutAndroid,
-  WebView,
   NativeViewGestureHandler,
   TapGestureHandler,
   FlingGestureHandler,
