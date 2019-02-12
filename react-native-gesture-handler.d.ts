33506bf2e4cf5da1fc4a20c29a6f67c429cb7df0
Michał Osadnik <micosa97@gmail.com>
2019-02-12 14:01:36 +0100

    Remove WebView (#438)
    WebView has been moved outside RN core and therefore importing it is deprecated.

--- react-native-gesture-handler.d.ts
+++ react-native-gesture-handler.d.ts
@@ -10,7 +10,6 @@ declare module 'react-native-gesture-handler' {
     SliderProperties,
     SwitchProperties,
     TextInputProperties,
-    WebViewProperties,
     ToolbarAndroidProperties,
     ViewPagerAndroidProperties,
     DrawerLayoutAndroidProperties,
@@ -412,10 +411,6 @@ declare module 'react-native-gesture-handler' {
 
   export class DrawerLayoutAndroid extends React.Component<
     NativeViewGestureHandlerProperties & DrawerLayoutAndroidProperties
-  > {}
-
-  export class WebView extends React.Component<
-    NativeViewGestureHandlerProperties & WebViewProperties
   > {}
 
   /* OTHER */
