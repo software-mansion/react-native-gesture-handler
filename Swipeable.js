daac0c5289bd2063ae016980952e7500105cb528
ShaMan123 <shacharnen@gmail.com>
2019-03-12 01:10:32 +0200

    Update Swipeable.js

--- Swipeable.js
+++ Swipeable.js
@@ -5,7 +5,7 @@
 // to move faster and fix possible issues quicker
 
 import React, { Component } from 'react';
-import { Animated, StyleSheet, View } from 'react-native';
+import { Animated, StyleSheet, View, I18nManager } from 'react-native';
 
 import { PanGestureHandler, TapGestureHandler, State } from './GestureHandler';
 
