8887c9c0969adfb05b6fb9154d39326a027a9107
Aaron Grider <aaron.grider@gmail.com>
2019-04-25 09:29:52 -0700

    Bump additional dependencies

--- AndroidNativeExample/app/src/main/java/com/swmansion/gesturehandler/example/MainActivity.java
+++ AndroidNativeExample/app/src/main/java/com/swmansion/gesturehandler/example/MainActivity.java
@@ -1,7 +1,7 @@
 package com.swmansion.gesturehandler.example;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
