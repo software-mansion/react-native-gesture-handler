da658c2de2e7135871e27284d9c8c6d323c5d803
Krzysztof Magiera <krzys@swmansion.com>
2019-05-29 21:00:01 +0200

    Lazy import RN components
    This PR changes the way we export gesture-handler-wrapped components. Instead of importing them from RN we rely on lazy-load mechanism (that is also utilize in RN core) to only pull in core components that we need. On top of that I added memoization mechanism such that for each component only one wrapper component is created.
    
    This change makes GH more future proof as some core components (e.g. AndroidToolbar) are being removed from react-native. This will allow consumers to keep using these so long they rely on RN version that have them included (full backward compatibility). And also allow for consumers relying on new RN versions to wrap components from react-native-community github in case they want to continue using these.

--- GestureHandler.js
+++ GestureHandler.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import {
   Animated,
-  ScrollView,
-  Slider,
-  Switch,
-  TextInput,
-  ToolbarAndroid,
-  ViewPagerAndroid,
-  DrawerLayoutAndroid,
   StyleSheet,
-  FlatList,
   Platform,
   processColor,
+  default as ReactNative,
 } from 'react-native';
 import PropTypes from 'prop-types';
 
@@ -400,30 +393,6 @@ function createNativeWrapper(Component, config = {}) {
   return ComponentWrapper;
 }
 
-const WrappedScrollView = createNativeWrapper(ScrollView, {
-  disallowInterruption: true,
-});
-const WrappedSlider = createNativeWrapper(Slider, {
-  shouldCancelWhenOutside: false,
-  shouldActivateOnStart: true,
-  disallowInterruption: true,
-});
-const WrappedSwitch = createNativeWrapper(Switch, {
-  shouldCancelWhenOutside: false,
-  shouldActivateOnStart: true,
-  disallowInterruption: true,
-});
-const WrappedTextInput = createNativeWrapper(TextInput);
-
-const WrappedToolbarAndroid = createNativeWrapper(ToolbarAndroid);
-const WrappedViewPagerAndroid = createNativeWrapper(ViewPagerAndroid, {
-  disallowInterruption: true,
-});
-const WrappedDrawerLayoutAndroid = createNativeWrapper(DrawerLayoutAndroid, {
-  disallowInterruption: true,
-});
-WrappedDrawerLayoutAndroid.positions = DrawerLayoutAndroid.positions;
-
 State.print = state => {
   const keys = Object.keys(State);
   for (let i = 0; i < keys.length; i++) {
@@ -590,23 +559,71 @@ class BorderlessButton extends React.Component {
   }
 }
 
-/* Other */
+const MEMOIZED = {};
 
-const FlatListWithGHScroll = props => (
-  <FlatList
-    {...props}
-    renderScrollComponent={props => <WrappedScrollView {...props} />}
-  />
-);
+function memoizeWrap(Component, config) {
+  const memoized = MEMOIZED[Component.displayName];
+  if (memoized) {
+    return memoized;
+  }
+  return (MEMOIZED[Component.displayName] = createNativeWrapper(
+    Component,
+    config
+  ));
+}
 
-export {
-  WrappedScrollView as ScrollView,
-  WrappedSlider as Slider,
-  WrappedSwitch as Switch,
-  WrappedTextInput as TextInput,
-  WrappedToolbarAndroid as ToolbarAndroid,
-  WrappedViewPagerAndroid as ViewPagerAndroid,
-  WrappedDrawerLayoutAndroid as DrawerLayoutAndroid,
+module.exports = {
+  /* RN's components */
+  get ScrollView() {
+    return memoizeWrap(ReactNative.ScrollView, {
+      disallowInterruption: true,
+    });
+  },
+  get Slider() {
+    return createNativeWrapper(ReactNative.Slider, {
+      shouldCancelWhenOutside: false,
+      shouldActivateOnStart: true,
+      disallowInterruption: true,
+    });
+  },
+  get Switch() {
+    return memoizeWrap(ReactNative.Switch, {
+      shouldCancelWhenOutside: false,
+      shouldActivateOnStart: true,
+      disallowInterruption: true,
+    });
+  },
+  get TextInput() {
+    return memoizeWrap(ReactNative.TextInput);
+  },
+  get ToolbarAndroid() {
+    return memoizeWrap(ReactNative.ToolbarAndroid);
+  },
+  get ViewPagerAndroid() {
+    return memoizeWrap(ReactNative.ViewPagerAndroid, {
+      disallowInterruption: true,
+    });
+  },
+  get DrawerLayoutAndroid() {
+    const DrawerLayoutAndroid = memoizeWrap(ReactNative.DrawerLayoutAndroid, {
+      disallowInterruption: true,
+    });
+    DrawerLayoutAndroid.positions = ReactNative.DrawerLayoutAndroid.positions;
+    return DrawerLayoutAndroid;
+  },
+  get FlatList() {
+    if (!MEMOIZED.FlatList) {
+      const ScrollView = this.ScrollView;
+      MEMOIZED.FlatList = React.forwardRef((props, ref) => (
+        <ReactNative.FlatList
+          ref={ref}
+          {...props}
+          renderScrollComponent={scrollProps => <ScrollView {...scrollProps} />}
+        />
+      ));
+    }
+    return MEMOIZED.FlatList;
+  },
   NativeViewGestureHandler,
   TapGestureHandler,
   FlingGestureHandler,
@@ -622,7 +639,6 @@ export {
   RectButton,
   BorderlessButton,
   /* Other */
-  FlatListWithGHScroll as FlatList,
   gestureHandlerRootHOC,
   Directions,
 };
