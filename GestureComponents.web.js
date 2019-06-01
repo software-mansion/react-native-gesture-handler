0ea3d95d486544f9f64afb278ac6d149cf9bda5f
Evan Bacon <baconbrix@gmail.com>
2019-06-01 13:45:40 -0700

    [web] Fixed lazy exports (#619)
    # Why
    
    Default import was removed from react-native-web. https://github.com/necolas/react-native-web/pull/1277
    The use of module.exports and import/export breaks tree-shaking in with babel-preset-expo.
    
    Related: https://github.com/kmagiera/react-native-gesture-handler/pull/617
    Fixes: https://github.com/expo/expo/issues/4339
    
    # How
    
    * Moved the shims to `Gestures(.web).js`
    * Moved all modules out of main re-export file. (optional)

---
+++ GestureComponents.web.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  DrawerLayoutAndroid as RNDrawerLayoutAndroid,
+  FlatList as RNFlatList,
+  Switch as RNSwitch,
+  TextInput as RNTextInput,
+  ToolbarAndroid as RNToolbarAndroid,
+  ScrollView as RNScrollView,
+} from 'react-native';
+
+import createNativeWrapper from './createNativeWrapper';
+
+export const ScrollView = createNativeWrapper(RNScrollView, {
+  disallowInterruption: true,
+});
+
+export const Switch = createNativeWrapper(RNSwitch, {
+  shouldCancelWhenOutside: false,
+  shouldActivateOnStart: true,
+  disallowInterruption: true,
+});
+export const TextInput = createNativeWrapper(RNTextInput);
+export const ToolbarAndroid = createNativeWrapper(RNToolbarAndroid);
+export const DrawerLayoutAndroid = createNativeWrapper(RNDrawerLayoutAndroid, {
+  disallowInterruption: true,
+});
+DrawerLayoutAndroid.positions = RNDrawerLayoutAndroid.positions;
+
+export const FlatList = React.forwardRef((props, ref) => (
+  <RNFlatList
+    ref={ref}
+    {...props}
+    renderScrollComponent={scrollProps => <ScrollView {...scrollProps} />}
+  />
+));
