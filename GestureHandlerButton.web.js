419fbb3415f7e58b3149773993d262b1449e5da8
Evan Bacon <baconbrix@gmail.com>
2019-06-27 12:28:47 -0700

    Update GestureHandlerButton.web.js

--- GestureHandlerButton.web.js
+++ GestureHandlerButton.web.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
 
-// Use TouchableOpacity with the transparency disabled to most closely emulate
-// the native view's ability to wrap children and apply styles.
 export default React.forwardRef((props, ref) => (
-  <View
-    ref={ref}
-    accessibilityRole="button"
-    {...props}
-    activeOpacity={1.0}
-    focusOpacity={1.0}
-  />
+  <View ref={ref} accessibilityRole="button" {...props} />
 ));
