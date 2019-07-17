5713701f4359dc11f4fb7ad3e4d78100b49730ce
Evan Bacon <baconbrix@gmail.com>
2019-07-17 13:07:28 -0700

    Web Support (#555)
    # Why
    
    Using hammer.js to emulate the native gesture API. Ideally we'll reuse as much API logic as possible between native and web.
    
    # TODO
    
    ## Components
    
    - [x] `ScrollView`
    - [ ] `Switch`
    - [ ] `TextInput`
    - [ ] `ToolbarAndroid`
    - [ ] `DrawerLayoutAndroid`
    - [ ] `FlatList`
    - [x] `Swipeable`
    
    ## Gesture Handlers
    
    - [x] `PanGestureHandler`
    - [x] `RotationGestureHandler`
    - [x] `PinchGestureHandler`
    - [x] `TapGestureHandler`
    - [x] `NativeViewGestureHandler`
    - [x] `LongPressGestureHandler`
    - [ ] `ForceTouchGestureHandler`
    - [x] `FlingGestureHandler`
    
    ### Props 
    
    - [x] `waitFor`
    - [x] `shouldCancelWhenOutside`
    
    ### Native View Gesture Handler
    
    - [x] Delay content touches
    - [x] Disallow Interruptions
    
    ## Buttons
    
    - [x] `RawButton`
    - [x] `BaseButton`
    - [x] `RectButton`
    - [x] `BorderlessButton`
    - [x] `PureNativeButton`
    
    # Test Plan
    
    TBD. Running in NCL by cloning into the [expo/expo](https://github.com/expo/expo) repo `packages/`. 
    
    ![expo hammer-low](https://user-images.githubusercontent.com/9664363/55779912-97fd6280-5a5b-11e9-83a3-010f272256f5.gif)
    
    lol disregard frame-rate, it's smooth IRL.

---
+++ web/RotationGestureHandler.js
@@ -0,0 +1,24 @@
+import Hammer from 'hammerjs';
+
+import { DEG_RAD } from './constants';
+import IndiscreteGestureHandler from './IndiscreteGestureHandler';
+
+class RotationGestureHandler extends IndiscreteGestureHandler {
+  get name() {
+    return 'rotate';
+  }
+
+  get NativeGestureClass() {
+    return Hammer.Rotate;
+  }
+
+  transformNativeEvent({ rotation, velocity, center }) {
+    return {
+      rotation: (rotation - this.initialRotation) * DEG_RAD,
+      anchorX: center.x,
+      anchorY: center.y,
+      velocity,
+    };
+  }
+}
+export default RotationGestureHandler;
---
+++ web/utils.js
@@ -0,0 +1,14 @@
+export const isnan = v => Number.isNaN(v);
+export const TEST_MIN_IF_NOT_NAN = (value, limit) =>
+  !isnan(limit) && ((limit < 0 && value <= limit) || (limit >= 0 && value >= limit));
+export const VEC_LEN_SQ = ({ x = 0, y = 0 } = {}) => x * x + y * y;
+export const TEST_MAX_IF_NOT_NAN = (value, max) =>
+  !isnan(max) && ((max < 0 && value < max) || (max >= 0 && value > max));
+
+export function fireAfterInterval(method, interval) {
+  if (!interval) {
+    method();
+    return null;
+  }
+  return setTimeout(() => method(), interval);
+}
