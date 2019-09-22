4e2c49b649f2b265d231fea12dcbdf729b74c4b5
Poyan Nabati <poyan.nabati@gmail.com>
2019-09-22 16:37:43 +0300

    Remove unused imports

--- Example/scaleAndRotate/index.js
+++ Example/scaleAndRotate/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Animated, StyleSheet } from 'react-native';
 
 import {
   PanGestureHandler,
