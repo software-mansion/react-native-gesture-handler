7411201273eada2dd77fe6f75e1cf00a9027cf1a
José Muñoz <jdmg93@gmail.com>
2019-11-13 09:34:14 -0600

    Added mocks for pan and scroll gesture components (#795)

--- __mocks__/RNGestureHandlerModule.js
+++ __mocks__/RNGestureHandlerModule.js
@@ -1,4 +1,8 @@
+import { View, ScrollView } from 'react-native'
+
 export default {
+  ScrollView,
+  PanGestureHandler: View,
   attachGestureHandler: () => {},
   createGestureHandler: () => {},
   dropGestureHandler: () => {},
