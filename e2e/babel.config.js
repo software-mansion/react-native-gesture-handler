a7921b57f50d445cb556a01a906adc2671935d63
Jakub Gonet <jakub.gonet@swmansion.com>
2019-12-06 11:08:10 +0100

    Bump e2e to 0.61.5
    Bumped using diffs from https://react-native-community.github.io/upgrade-helper/?from=0.59.8&to=0.61.5

--- e2e/babel.config.js
+++ e2e/babel.config.js
@@ -1,19 +1,37 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        alias: {
-          'react-native-gesture-handler': '../',
+module.exports = api => {
+  const isWeb = api.caller(isTargetWeb);
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      '@babel/plugin-transform-modules-commonjs',
+      isWeb ? [
+        'module-resolver',
+        {
+          alias: {
+            'react-native-gesture-handler': '../',
+          },
         },
-      },
-    ],
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        regenerator: true,
-      },
-    ],
-  ],
+      ] : [
+        'module-resolver',
+        {
+          alias: {
+            '@babel': './node_modules/@babel',
+            "react-native-gesture-handler": "../",
+            "react": "./node_modules/react",
+            "react-native": "./node_modules/react-native",
+            "hammerjs": "./node_modules/hammerjs",
+            "fbjs": "./node_modules/fbjs",
+            "hoist-non-react-statics": "./node_modules/hoist-non-react-statics",
+            "invariant": "./node_modules/invariant",
+            "prop-types": "./node_modules/prop-types"
+          },
+        },
+      ],
+    ].filter(Boolean),
+  };
 };
+
+function isTargetWeb(caller) {
+  return caller && caller.name === 'babel-loader';
+}
--- e2e/metro.config.js
+++ e2e/metro.config.js
@@ -7,36 +7,11 @@ function getBlacklist() {
   const nodeModuleDirs = [
     glob(`${path.resolve(__dirname, '..')}/node_modules/*`),
     glob(`${path.resolve(__dirname, '..')}/Example/*`),
-    glob(`${path.resolve(__dirname)}/node_modules/metro/node_modules/fbjs/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
     glob(
       `${path.resolve(
         __dirname
-      )}/node_modules/create-react-context/node_modules/fbjs/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/create-react-class/node_modules/fbjs/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/@react-navigation/native/node_modules/hoist-non-react-statics/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/@react-navigation/core/node_modules/hoist-non-react-statics/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/react-navigation-tabs/node_modules/hoist-non-react-statics/*`
-    ),
-    glob(
-      `${path.resolve(
-        __dirname
-      )}/node_modules/react-native-safe-area-view/node_modules/hoist-non-react-statics/*`
+      )}/node_modules/*/node_modules/hoist-non-react-statics/*`
     ),
   ];
   return blacklist(nodeModuleDirs);
@@ -52,7 +27,6 @@ module.exports = {
       'fbjs',
       'prop-types',
       'hoist-non-react-statics',
-      '@babel/runtime',
     ],
   },
   watchFolders: [path.resolve(__dirname, '..')],
