6fc5e07ccd5027b090c17c59e0971ee32f559991
Evan Bacon <baconbrix@gmail.com>
2020-01-16 18:06:26 -0800

    Multiply velocity to match velocity on native devices (#918)
    Velocity based gestures on web don't seem to match native gestures. This will make the interactions a little bit closer.

--- web/DraggingGestureHandler.js
+++ web/DraggingGestureHandler.js
@@ -1,19 +1,27 @@
 import GestureHandler from './GestureHandler';
+import { PixelRatio } from 'react-native';
 
 class DraggingGestureHandler extends GestureHandler {
   get shouldEnableGestureOnSetup() {
     return true;
   }
 
-  transformNativeEvent({ deltaX, deltaY, velocityX, velocityY, center: { x, y } }) {
-    const rect = this.view.getBoundingClientRect(); 
+  transformNativeEvent({
+    deltaX,
+    deltaY,
+    velocityX,
+    velocityY,
+    center: { x, y },
+  }) {
+    const rect = this.view.getBoundingClientRect();
+    const ratio = PixelRatio.get();
     return {
       translationX: deltaX - (this.__initialX || 0),
       translationY: deltaY - (this.__initialY || 0),
       absoluteX: x,
       absoluteY: y,
-      velocityX,
-      velocityY,
+      velocityX: velocityX * ratio,
+      velocityY: velocityY * ratio,
       x: x - rect.left,
       y: y - rect.top,
     };
