289694703408fa4688dc88548391b11b609ea481
osdnk <micosa97@gmail.com>
2020-01-30 01:21:59 +0100

    lint

--- android/src/main/java/com/facebook/react/views/modal/RNGHModalUtils.java
+++ android/src/main/java/com/facebook/react/views/modal/RNGHModalUtils.java
@@ -6,12 +6,11 @@ import android.view.ViewGroup;
 import android.view.ViewParent;
 
 public class RNGHModalUtils {
-    static public void dialogRootViewGroupOnChildStartedNativeGesture(ViewGroup modal, MotionEvent androidEvent) {
-        ((ReactModalHostView.DialogRootViewGroup) modal).onChildStartedNativeGesture(androidEvent);
-    }
+  static public void dialogRootViewGroupOnChildStartedNativeGesture(ViewGroup modal, MotionEvent androidEvent) {
+    ((ReactModalHostView.DialogRootViewGroup) modal).onChildStartedNativeGesture(androidEvent);
+  }
 
-    static public boolean isDialogRootViewGroup(ViewParent modal) {
-        return modal instanceof ReactModalHostView.DialogRootViewGroup;
-    }
-
+  static public boolean isDialogRootViewGroup(ViewParent modal) {
+    return modal instanceof ReactModalHostView.DialogRootViewGroup;
+  }
 }
