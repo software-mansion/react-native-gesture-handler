software-mansion/react-native-gesture-handler

commit cc5e348cecb0a6403906a6f49df8f54b4b5c98ef

Export a component for gesture handler root view (#925)
Currently, the user always needs to add code to wrap the root view in their MainActivity.java on Android, or in case of libraries such as Wix's native navigation, wrap each screen in gestureHandlerRootHOC.

This works well for apps, but for libraries that use gesture handler, it often increases the setup steps for the user. In the case of React Navigation and other libraries I work on, we have found that people always miss this configuration step and open issues about their gestures not working. This wrapper will let library authors avoid this extra step for the user by wrapping their library code in GestureHandlerRootView, and will only need auto-linking to work.

Overview

New GestureHandlerRootView component for libraries to use, takes care of nesting multiple root views with context API so only 1 of them is rendered and rest are replaced with View
Change gestureHandlerRootHOC to use the GestureHandlerRootView component which also fixes a platform inconsistency
Fix the native RNGestureHandlerRootView (Android) to make sure that nesting it inside RNGestureHandlerEnabledRootView doesn't crash it, which can happen if the user already configured their Mainactivity.java.
Test plan

Tested by wrapping one of the examples in the example app in GestureHandlerRootView and whole app in gestureHandlerRootHOC, both with and without the setup code in MainActivity.java

Questions

Should we remove the MainActivity.java step from the docs and only use the component/hoc? Arguably it's easier to setup than MainActivity.java because you don't have to touch native code.

gestureHandlerRootHOC.js
@@ -1,7 +1,28 @@
-export default function gestureHandlerRootHOC(Component) {
-  // Empty implementation that just returns component directly,
-  // GestureHandlerRootView is only required to be instantiated on Android.
-  // All other targets (ios, web, macos, ect...) should do nothing.
-  return Component;
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import hoistNonReactStatics from 'hoist-non-react-statics';
+import GestureHandlerRootView from './GestureHandlerRootView';
+
+export default function gestureHandlerRootHOC(
+  Component,
+  containerStyles = undefined
+) {
+  function Wrapper() {
+    return (
+      <GestureHandlerRootView style={[styles.container, containerStyles]}>
+        <Component {...this.props} />
+      </GestureHandlerRootView>
+    );
+  }
+
+  Wrapper.displayName = `gestureHandlerRootHOC(${Component.displayName ||
+    Component.name})`;
+
+  hoistNonReactStatics(Wrapper, Component);
+
+  return Wrapper;
 }
-  
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});

GestureHandler.js
@@ -1,6 +1,7 @@
 export { default as createNativeWrapper } from './createNativeWrapper';
 export { default as Directions } from './Directions';
 export { default as gestureHandlerRootHOC } from './gestureHandlerRootHOC';
+export { default as GestureHandlerRootView } from './GestureHandlerRootView';
 export {
   default as NativeViewGestureHandler,
 } from './NativeViewGestureHandler';

GestureHandlerRootView.android.expo.js
@@ -1,0 +1,4 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default View;

GestureHandlerRootView.android.js
@@ -1,0 +1,39 @@
+import React from 'react';
+import { View, ViewPropTypes, requireNativeComponent } from 'react-native';
+
+const iface = {
+  name: 'GestureHandlerRootView',
+  propTypes: {
+    ...ViewPropTypes,
+  },
+};
+
+const GestureHandlerRootViewNative = requireNativeComponent(
+  'GestureHandlerRootView',
+  iface
+);
+
+const GestureHandlerRootViewContext = React.createContext(false);
+
+export default function GestureHandlerRootView({ children, ...rest }) {
+  return (
+    <GestureHandlerRootViewContext.Consumer>
+      {available => {
+        if (available) {
+          // If we already have a parent wrapped in the gesture handler root view,
+          // We don't need to wrap it again in root view
+          // We still wrap it in a normal view so our styling stays the same
+          return <View {...rest}>{children}</View>;
+        }
+
+        return (
+          <GestureHandlerRootViewContext.Provider value={true}>
+            <GestureHandlerRootViewNative {...rest}>
+              {children}
+            </GestureHandlerRootViewNative>
+          </GestureHandlerRootViewContext.Provider>
+        );
+      }}
+    </GestureHandlerRootViewContext.Consumer>
+  );
+}

GestureHandlerRootView.js
@@ -1,0 +1,4 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default View;

android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootView.java
@@ -1,16 +1,34 @@
 package com.swmansion.gesturehandler.react;
 
+import android.util.Log;
 import android.content.Context;
 import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.ViewParent;
 
 import com.facebook.infer.annotation.Assertions;
 import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.UiThreadUtil;
+import com.facebook.react.common.ReactConstants;
 import com.facebook.react.views.view.ReactViewGroup;
 
 import androidx.annotation.Nullable;
 
 public class RNGestureHandlerRootView extends ReactViewGroup {
 
+  private static boolean hasGestureHandlerEnabledRootView(ViewGroup viewGroup) {
+    UiThreadUtil.assertOnUiThread();
+    ViewParent parent = viewGroup.getParent();
+    while (parent != null) {
+      if (parent instanceof RNGestureHandlerEnabledRootView || parent instanceof RNGestureHandlerRootView) {
+        return true;
+      }
+      parent = parent.getParent();
+    }
+    return false;
+  }
+
+  private boolean mEnabled;
   private @Nullable RNGestureHandlerRootHelper mRootHelper;
 
   public RNGestureHandlerRootView(Context context) {
@@ -20,7 +38,16 @@ public class RNGestureHandlerRootView extends ReactViewGroup {
   @Override
   protected void onAttachedToWindow() {
     super.onAttachedToWindow();
-    if (mRootHelper == null) {
+
+    mEnabled = !hasGestureHandlerEnabledRootView(this);
+
+    if (!mEnabled) {
+      Log.i(
+              ReactConstants.TAG,
+              "[GESTURE HANDLER] Gesture handler is already enabled for a parent view");
+    }
+
+    if (mEnabled && mRootHelper == null) {
       mRootHelper = new RNGestureHandlerRootHelper((ReactContext) getContext(), this);
     }
   }
@@ -33,7 +60,7 @@ public class RNGestureHandlerRootView extends ReactViewGroup {
 
   @Override
   public boolean dispatchTouchEvent(MotionEvent ev) {
-    if (Assertions.assertNotNull(mRootHelper).dispatchTouchEvent(ev)) {
+    if (mEnabled && Assertions.assertNotNull(mRootHelper).dispatchTouchEvent(ev)) {
       return true;
     }
     return super.dispatchTouchEvent(ev);
@@ -41,7 +68,9 @@ public class RNGestureHandlerRootView extends ReactViewGroup {
 
   @Override
   public void requestDisallowInterceptTouchEvent(boolean disallowIntercept) {
-    Assertions.assertNotNull(mRootHelper).requestDisallowInterceptTouchEvent(disallowIntercept);
+    if (mEnabled) {
+      Assertions.assertNotNull(mRootHelper).requestDisallowInterceptTouchEvent(disallowIntercept);
+    }
     super.requestDisallowInterceptTouchEvent(disallowIntercept);
   }
 }

gestureHandlerRootHOC.android.js
@@ -1,40 +1,0 @@
-import React from 'react';
-import {
-  requireNativeComponent,
-  StyleSheet,
-  ViewPropTypes,
-} from 'react-native';
-import hoistNonReactStatics from 'hoist-non-react-statics';
-
-const iface = {
-  name: 'GestureHandlerRootView',
-  propTypes: {
-    ...ViewPropTypes,
-  },
-};
-
-const GestureHandlerRootView = requireNativeComponent(
-  'GestureHandlerRootView',
-  iface
-);
-
-export default function gestureHandlerRootHOC(
-  Component,
-  containerStyles = undefined
-) {
-  class Wrapper extends React.Component {
-    render() {
-      return (
-        <GestureHandlerRootView style={[styles.container, containerStyles]}>
-          <Component {...this.props} />
-        </GestureHandlerRootView>
-      );
-    }
-  }
-  hoistNonReactStatics(Wrapper, Component);
-  return Wrapper;
-}
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-});

gestureHandlerRootHOC.ios.js
@@ -1,5 +1,0 @@
-export default function gestureHandlerRootHOC(Component) {
-  // Empty implementation that just returns component directly,
-  // GestureHandlerRootView is only required to be instantiated on Android
-  return Component;
-}

commit 5bb191bc8f4196bbd4c4bc8a5e8be5bfe964bcea

Fix passing props to gestureHandlerRootHOC.js (#944)
fixes: #942

regression after: cc5e348

gestureHandlerRootHOC.js
@@ -7,10 +7,10 @@ export default function gestureHandlerRootHOC(
   Component,
   containerStyles = undefined
 ) {
-  function Wrapper() {
+  function Wrapper(props) {
     return (
       <GestureHandlerRootView style={[styles.container, containerStyles]}>
-        <Component {...this.props} />
+        <Component {...props} />
       </GestureHandlerRootView>
     );
   }