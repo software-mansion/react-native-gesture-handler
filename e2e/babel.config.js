78137f6af75caacec53e3d64d34e7ee0ef18c759
Evan Bacon <baconbrix@gmail.com>
2020-02-06 11:17:29 -0800

    Remove extra hammerjs code

--- e2e/babel.config.js
+++ e2e/babel.config.js
@@ -5,29 +5,32 @@ module.exports = api => {
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [
       '@babel/plugin-transform-modules-commonjs',
-      isWeb ? [
-        'module-resolver',
-        {
-          alias: {
-            'react-native-gesture-handler': '../',
-          },
-        },
-      ] : [
-        'module-resolver',
-        {
-          alias: {
-            '@babel': './node_modules/@babel',
-            "react-native-gesture-handler": "../",
-            "react": "./node_modules/react",
-            "react-native": "./node_modules/react-native",
-            "hammerjs": "./node_modules/hammerjs",
-            "fbjs": "./node_modules/fbjs",
-            "hoist-non-react-statics": "./node_modules/hoist-non-react-statics",
-            "invariant": "./node_modules/invariant",
-            "prop-types": "./node_modules/prop-types"
-          },
-        },
-      ],
+      isWeb
+        ? [
+            'module-resolver',
+            {
+              alias: {
+                'react-native-gesture-handler': '../',
+              },
+            },
+          ]
+        : [
+            'module-resolver',
+            {
+              alias: {
+                '@babel': './node_modules/@babel',
+                'react-native-gesture-handler': '../',
+                react: './node_modules/react',
+                'react-native': './node_modules/react-native',
+                '@egjs/hammerjs': './node_modules/@egjs/hammerjs',
+                fbjs: './node_modules/fbjs',
+                'hoist-non-react-statics':
+                  './node_modules/hoist-non-react-statics',
+                invariant: './node_modules/invariant',
+                'prop-types': './node_modules/prop-types',
+              },
+            },
+          ],
     ].filter(Boolean),
   };
 };
