860fa06fc731de431fdb5ee3d073b1836d35018a
Chris Drackett <chrisdrackett@users.noreply.github.com>
2020-02-26 12:13:11 -0800

    add support for `enabled` as well

--- GestureHandlerButton.web.js
+++ GestureHandlerButton.web.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default React.forwardRef(({onPress, ...props}, ref) => {
-  return <View ref={ref} accessibilityRole={onPress ? "button" : undefined} onPress={onPress} {...props} />
+export default React.forwardRef(({enabled, onPress, ...props}, ref) => {
+  const isPressable = enabled && !!onPress
+  
+  return <View ref={ref} accessibilityRole={isPressable ? "button" : undefined} onPress={onPress} {...props} />
 });
