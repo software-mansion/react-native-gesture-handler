bafd243173537773c7fdead212977b97b74892de
Chris Drackett <chrisdrackett@users.noreply.github.com>
2020-02-25 16:48:05 -0800

    add `button` role conditionally
    only add `accessibilityRole` of `button` when `onPress` is present.

--- GestureHandlerButton.web.js
+++ GestureHandlerButton.web.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
 
-export default React.forwardRef((props, ref) => (
-  <View ref={ref} accessibilityRole="button" {...props} />
-));
+export default React.forwardRef(({onPress, ...props}, ref) => {
+  return <View ref={ref} accessibilityRole={onPress ? "button" : undefined} onPress={onPress} {...props} />
+});
