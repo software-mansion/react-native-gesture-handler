9db7f5288d4796f824a0902bb2308aee59eba483
ShaMan123 <shacharnen@gmail.com>
2020-03-15 15:57:30 +0200

    Create react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  dependency: {
+    android: {
+      packageImportPath: "import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;",
+      packageInstance: "new RNGestureHandlerPackage()"
+    },
+  }
+}
