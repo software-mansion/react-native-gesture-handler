54e3403d7c03d996c43b1a15c46fa05d615e2235
Peter Argany <petetheheat@fb.com>
2020-08-07 15:50:26 -0700

    change imports

--- ios/RNGestureHandlerManager.h
+++ ios/RNGestureHandlerManager.h
@@ -1,7 +1,7 @@
 #import <Foundation/Foundation.h>
 
-#import <React/RCTBridgeModule.h>
-#import <React/RCTUIManager.h>
+@class RCTUIManager;
+@class RCTEventDispatcher;
 
 @interface RNGestureHandlerManager : NSObject
 
