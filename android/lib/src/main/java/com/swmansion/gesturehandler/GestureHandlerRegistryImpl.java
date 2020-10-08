cef5d34813f03207b197d24f81fd7e07b81b97e0
ShaMan123 <shacharnen@gmail.com>
2020-10-08 10:48:56 +0300

    review fixes
    rename `mViewForTag` -> `mViewForHandlerTag` (less confusing)
    add methods for dropping handlers

--- android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerRegistryImpl.java
+++ android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerRegistryImpl.java
@@ -11,7 +11,7 @@ import java.util.WeakHashMap;
 public class GestureHandlerRegistryImpl implements GestureHandlerRegistry {
 
   private final WeakHashMap<View, ArrayList<GestureHandler>> mHandlersForView = new WeakHashMap<>();
-  private final SparseArray<View> mViewForTag = new SparseArray<>();
+  private final SparseArray<View> mViewForHandlerTag = new SparseArray<>();
   private final SparseArray<GestureHandler> mHandlers = new SparseArray<>();
 
   public <T extends GestureHandler> T registerHandlerForView(View view, T handler) {
@@ -25,9 +25,52 @@ public class GestureHandlerRegistryImpl implements GestureHandlerRegistry {
     }
     if (mHandlers.get(handler.getTag()) == null) {
       mHandlers.put(handler.getTag(), handler);
-      mViewForTag.put(handler.getTag(), view);
+      mViewForHandlerTag.put(handler.getTag(), view);
     }
     return handler;
+  }
+
+  private void detachHandler(GestureHandler handler) {
+    View handlerView = mViewForHandlerTag.get(handler.getTag());
+    if (handlerView != null) {
+      mViewForHandlerTag.remove(handler.getTag());
+      ArrayList<GestureHandler> attachedHandlers = mHandlersForView.get(handlerView);
+      if (attachedHandlers != null) {
+        attachedHandlers.remove(handler);
+        if (attachedHandlers.size() == 0) {
+          mHandlersForView.remove(handlerView);
+        }
+      }
+    }
+    if (handler.getView() != null) {
+      // Handler is in "prepared" state which means it is registered in the orchestrator and can
+      // receive touch events. This means that before we remove it from the registry we need to
+      // "cancel" it so that orchestrator does no longer keep a reference to it.
+      handler.cancel();
+    }
+  }
+
+  public void dropHandler(int handlerTag) {
+    GestureHandler handler = mHandlers.get(handlerTag);
+    if (handler != null) {
+      detachHandler(handler);
+      mHandlers.remove(handlerTag);
+    }
+  }
+
+  public void dropHandlersForView(View view) {
+    ArrayList<GestureHandler> handlers = mHandlersForView.get(view);
+    if (handlers != null) {
+      for (GestureHandler handler: handlers) {
+        dropHandler(handler.getTag());
+      }
+    }
+  }
+
+  public synchronized void dropAllHandlers() {
+    mHandlersForView.clear();
+    mViewForHandlerTag.clear();
+    mHandlers.clear();
   }
 
   @Override
@@ -43,6 +86,6 @@ public class GestureHandlerRegistryImpl implements GestureHandlerRegistry {
 
   @Override
   public View getViewForHandler(GestureHandler handler) {
-    return mViewForTag.get(handler.getTag());
+    return mViewForHandlerTag.get(handler.getTag());
   }
 }
