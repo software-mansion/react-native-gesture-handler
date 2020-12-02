57ae53a3f1cb03e2d2d6fae110adb36556e70eb1
Jakub Gonet <jakub.gonet@swmansion.com>
2020-12-02 23:07:27 +0100

    Remove print in state, make it const

--- src/State.ts
+++ src/State.ts
@@ -1,30 +1,10 @@
-/* GESTURE HANDLER STATE */
-
-const StateTypes: Record<string, number> = {
-  UNDETERMINED: 0,
-  FAILED: 1,
-  BEGAN: 2,
-  CANCELLED: 3,
-  ACTIVE: 4,
-  END: 5,
-};
-
 const State = {
-  // for some reason using spread operator resolves in wrong type
   UNDETERMINED: 0,
   FAILED: 1,
   BEGAN: 2,
   CANCELLED: 3,
   ACTIVE: 4,
   END: 5,
-  print: (state: number) => {
-    const keys = Object.keys(StateTypes);
-    for (const key of keys) {
-      if (state === StateTypes[key]) {
-        return key;
-      }
-    }
-  },
-};
+} as const;
 
 export default State;
