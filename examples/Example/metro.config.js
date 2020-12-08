8e357319d8b5c17d9e7f679fb5d1eb660110ec86
WoLewicki <wojciech.lewicki@swmansion.com>
2020-12-08 17:14:59 +0100

    feat: add first look on RNGH resolving

--- examples/Example/metro.config.js
+++ examples/Example/metro.config.js
@@ -1,17 +1,25 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const blacklist = require('metro-config/src/defaults/blacklist');
+const path = require('path');
+
+const glob = require('glob-to-regexp');
+
+function getBlacklist() {
+  const nodeModuleDirs = [
+    glob(`${path.resolve(__dirname, '../..')}/node_modules/*`),
+    glob(`${path.resolve(__dirname, '../..')}/e2e/*`),
+    glob(`${path.resolve(__dirname)}/node_modules/*/node_modules/fbjs/*`),
+    glob(
+      `${path.resolve(
+        __dirname,
+      )}/node_modules/*/node_modules/hoist-non-react-statics/*`,
+    ),
+  ];
+  return blacklist(nodeModuleDirs);
+}
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+  resolver: {
+    blacklistRE: getBlacklist(),
   },
+  watchFolders: [path.resolve(__dirname, '../..')],
 };
