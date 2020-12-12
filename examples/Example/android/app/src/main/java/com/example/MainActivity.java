af97c69e3a362d8dfcd6052277d65c68b2fb58d5
Jakub Gonet <jakub.gonet@swmansion.com>
2020-12-12 12:06:22 +0100

    Register RNGH root view

--- examples/Example/android/app/src/main/java/com/example/MainActivity.java
+++ examples/Example/android/app/src/main/java/com/example/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -10,6 +13,16 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Example";
+    return "RN Gesture Handler example";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
