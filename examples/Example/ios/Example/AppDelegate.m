392a5eaa40b4f6e024835c802328b44cf4217d19
Jakub Gonet <jakub.gonet@swmansion.com>
2020-12-13 17:30:56 +0100

    Re-add MBFingerTipWindow library

--- examples/Example/ios/Example/AppDelegate.m
+++ examples/Example/ios/Example/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "MBFingerTipWindow.h"
 
 #ifdef FB_SONARKIT_ENABLED
 #import <FlipperKit/FlipperClient.h>
@@ -38,7 +39,9 @@ static void InitializeFlipper(UIApplication *application) {
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  MBFingerTipWindow *window = [[MBFingerTipWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+  window.alwaysShowTouches = YES;
+  self.window = window;
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
