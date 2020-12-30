ef8d5c59b92feafbb2c852e8aad70874daed511f
Jakub Gonet <jakub.gonet@swmansion.com>
2020-12-30 10:33:05 +0100

    Add index.html and register it

--- examples/Example/index.js
+++ examples/Example/index.js
@@ -2,3 +2,6 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 
 AppRegistry.registerComponent('Example', () => App);
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root'),
+});
