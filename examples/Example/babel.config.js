4cbf2a4ff3d5f8e4cac3b307fded4fbdab80a289
Jakub Gonet <jakub.gonet@swmansion.com>
2021-01-05 08:02:34 +0100

    Remove plugin-transform-modules-commonjs and use expo preset

--- examples/Example/babel.config.js
+++ examples/Example/babel.config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
   plugins: [
-    '@babel/plugin-transform-modules-commonjs',
     [
       'module-resolver',
       {
