adec7b2c9cd4d426b751935601bbc6cf1f1ddab4
Jakub Gonet <jakub.gonet@swmansion.com>
2021-02-03 16:16:56 +0100

    Explicitly list exports

--- index.ts
+++ index.ts
@@ -1,15 +1,81 @@
-export { default as createNativeWrapper } from './src/handlers/createNativeWrapper';
 export { default as Directions } from './src/Directions';
+export { State } from './src/State';
 export { default as gestureHandlerRootHOC } from './src/gestureHandlerRootHOC';
 export { default as GestureHandlerRootView } from './src/GestureHandlerRootView';
 
-export * from './src/components/GestureButtons';
-export * from './src/components/GestureComponents';
-export * from './src/handlers/gestureHandlers';
-export * from './src/handlers/gestureHandlerTypesCompat';
-export * from './src/handlers/NativeViewGestureHandler';
-export * from './src/State';
+export {
+  // event types
+  GestureEvent,
+  HandlerStateChangeEvent,
+  // event payloads types
+  GestureEventPayload,
+  HandlerStateChangeEventPayload,
+  TapGestureHandlerEventPayload,
+  ForceTouchGestureHandlerEventPayload,
+  LongPressGestureHandlerEventPayload,
+  PanGestureHandlerEventPayload,
+  PinchGestureHandlerEventPayload,
+  RotationGestureHandlerEventPayload,
+  FlingGestureHandlerEventPayload,
+  // gesture handlers
+  TapGestureHandler,
+  ForceTouchGestureHandler,
+  LongPressGestureHandler,
+  PanGestureHandler,
+  PinchGestureHandler,
+  RotationGestureHandler,
+  FlingGestureHandler,
+} from './src/handlers/gestureHandlers';
+export { default as createNativeWrapper } from './src/handlers/createNativeWrapper';
+export {
+  NativeViewGestureHandler,
+  NativeViewGestureHandlerPayload,
+  NativeViewGestureHandlerProps,
+} from './src/handlers/NativeViewGestureHandler';
+
+export {
+  RawButton,
+  BaseButton,
+  RectButton,
+  BorderlessButton,
+} from './src/components/GestureButtons';
+export {
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from './src/components/touchables';
+export {
+  ScrollView,
+  Switch,
+  TextInput,
+  DrawerLayoutAndroid,
+  FlatList,
+} from './src/components/GestureComponents';
+export {
+  //events
+  GestureHandlerGestureEvent,
+  GestureHandlerStateChangeEvent,
+  //event payloads
+  GestureHandlerGestureEventNativeEvent,
+  GestureHandlerStateChangeNativeEvent,
+  NativeViewGestureHandlerGestureEvent,
+  NativeViewGestureHandlerStateChangeEvent,
+  TapGestureHandlerGestureEvent,
+  TapGestureHandlerStateChangeEvent,
+  ForceTouchGestureHandlerGestureEvent,
+  ForceTouchGestureHandlerStateChangeEvent,
+  LongPressGestureHandlerGestureEvent,
+  LongPressGestureHandlerStateChangeEvent,
+  PanGestureHandlerGestureEvent,
+  PanGestureHandlerStateChangeEvent,
+  PinchGestureHandlerGestureEvent,
+  PinchGestureHandlerStateChangeEvent,
+  RotationGestureHandlerGestureEvent,
+  RotationGestureHandlerStateChangeEvent,
+  FlingGestureHandlerGestureEvent,
+  FlingGestureHandlerStateChangeEvent,
+} from './src/handlers/gestureHandlerTypesCompat';
 
 export { default as Swipeable } from './src/components/Swipeable';
 export { default as DrawerLayout } from './src/components/DrawerLayout';
-export * from './src/components/touchables';
