c1ff44794a9b404f8a52092e69da6a71c340a436
Jakub Gonet <jakub.gonet@swmansion.com>
2021-02-16 17:18:42 +0100

    Add missing backtick

--- src/RNGestureHandlerModule.ts
+++ src/RNGestureHandlerModule.ts
@@ -5,7 +5,7 @@ if (RNGestureHandlerModule == null) {
   console.error(
     `react-native-gesture-handler module was not found. Make sure you're running your app on the native platform and your code is linked properly (cd ios && pod install && cd ..).
 
-    For installation instructions, please refer to https://docs.swmansion.com/react-native-gesture-handler/docs/#installation
+    For installation instructions, please refer to https://docs.swmansion.com/react-native-gesture-handler/docs/#installation`
       .split('\n')
       .map((line) => line.trim())
       .join('\n')
