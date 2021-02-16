7ee1265ce0045455a74e232b208cbd3f58ab2cec
Jakub <jakub.gonet@swmansion.com>
2021-02-16 16:12:38 +0100

    Point to installation page in error message

--- src/RNGestureHandlerModule.ts
+++ src/RNGestureHandlerModule.ts
@@ -5,7 +5,7 @@ if (RNGestureHandlerModule == null) {
   console.error(
     `react-native-gesture-handler module was not found. Make sure you're running your app on the native platform and your code is linked properly (cd ios && pod install && cd ..).
 
-    You can submit a bug report here: https://github.com/software-mansion/react-native-gesture-handler/issues`
+    For installation instructions, please refer to https://docs.swmansion.com/react-native-gesture-handler/docs/#installation
       .split('\n')
       .map((line) => line.trim())
       .join('\n')
