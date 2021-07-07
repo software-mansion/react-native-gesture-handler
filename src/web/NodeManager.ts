b172a7937e47d7e613b104f228f1166c255a64ad
Janic Duplessis <janicduplessis@gmail.com>
2021-07-07 16:01:14 -0400

    Fix compatibility with React 18 (#1500)
    On web, there are cases now in React 18 where componentWillUnmount gets called twice in a row. I think it is related to reactwg/react-18#31. This causes a No handler for tag error since the handler was cleaned up already in the first componentWillUnmount call.
    
    To workaround the issue I added a check to no-op of the handler tag is not found. I think this is fine in that case and simpler than tracking whether the handler was cleaned up in the createHandler component.

--- src/web/NodeManager.ts
+++ src/web/NodeManager.ts
@@ -22,6 +22,11 @@ export function createGestureHandler(
 }
 
 export function dropGestureHandler(handlerTag: number) {
+  // Since React 18, there are cases where componentWillUnmount gets called twice in a row
+  // so skip this if the tag was already removed.
+  if (!(handlerTag in gestures)) {
+    return;
+  }
   getHandler(handlerTag).destroy();
   // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
   delete gestures[handlerTag];
