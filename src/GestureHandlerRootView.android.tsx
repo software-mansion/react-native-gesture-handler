5c21919735eff74d4d8eb979cd09a321fcc87bd6
Parichit <parichitkotawala@urbancompany.com>
2021-07-13 18:13:31 +0530

    Added isForced prop to GestureHandlerRootView

--- src/GestureHandlerRootView.android.tsx
+++ src/GestureHandlerRootView.android.tsx
@@ -10,11 +10,11 @@ const GestureHandlerRootViewContext = React.createContext(false);
 
 type Props = PropsWithChildren<Record<string, unknown>>;
 
-export default function GestureHandlerRootView({ children, ...rest }: Props) {
+export default function GestureHandlerRootView({ children, isForced = false, ...rest }: Props) {
   return (
     <GestureHandlerRootViewContext.Consumer>
       {(available) => {
-        if (available) {
+        if (available && !isForced) {
           // If we already have a parent wrapped in the gesture handler root view,
           // We don't need to wrap it again in root view
           // We still wrap it in a normal view so our styling stays the same
