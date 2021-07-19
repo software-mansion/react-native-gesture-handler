18e8d444472681ab16897e28d1774feb4ba7fc25
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-19 19:43:40 +0200

    Make RootView more idiomatic

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootView.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootView.kt
@@ -4,43 +4,41 @@ import android.content.Context
 import android.util.Log
 import android.view.MotionEvent
 import android.view.ViewGroup
-import com.facebook.infer.annotation.Assertions
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.bridge.UiThreadUtil
 import com.facebook.react.common.ReactConstants
 import com.facebook.react.views.view.ReactViewGroup
 
 class RNGestureHandlerRootView(context: Context?) : ReactViewGroup(context) {
-  private var mEnabled = false
-  private var mRootHelper: RNGestureHandlerRootHelper? = null
+  private var _enabled = false
+  private var rootHelper: RNGestureHandlerRootHelper? = null
   override fun onAttachedToWindow() {
     super.onAttachedToWindow()
-    mEnabled = !hasGestureHandlerEnabledRootView(this)
-    if (!mEnabled) {
+    _enabled = !hasGestureHandlerEnabledRootView(this)
+    if (!_enabled) {
       Log.i(
           ReactConstants.TAG,
-          "[GESTURE HANDLER] Gesture handler is already enabled for a parent view")
+          "[GESTURE HANDLER] Gesture handler is already enabled for a parent view"
+      )
     }
-    if (mEnabled && mRootHelper == null) {
-      mRootHelper = RNGestureHandlerRootHelper(context as ReactContext, this)
+    if (_enabled && rootHelper == null) {
+      rootHelper = RNGestureHandlerRootHelper(context as ReactContext, this)
     }
   }
 
   fun tearDown() {
-    if (mRootHelper != null) {
-      mRootHelper!!.tearDown()
-    }
+    rootHelper?.tearDown()
   }
 
-  override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
-    return if (mEnabled && Assertions.assertNotNull(mRootHelper).dispatchTouchEvent(ev)) {
-      true
-    } else super.dispatchTouchEvent(ev)
-  }
+  override fun dispatchTouchEvent(ev: MotionEvent) =
+      if (_enabled && rootHelper!!.dispatchTouchEvent(ev)) {
+        true
+      } else super.dispatchTouchEvent(ev)
+
 
   override fun requestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {
-    if (mEnabled) {
-      Assertions.assertNotNull(mRootHelper).requestDisallowInterceptTouchEvent(disallowIntercept)
+    if (_enabled) {
+      rootHelper!!.requestDisallowInterceptTouchEvent(disallowIntercept)
     }
     super.requestDisallowInterceptTouchEvent(disallowIntercept)
   }
@@ -48,6 +46,7 @@ class RNGestureHandlerRootView(context: Context?) : ReactViewGroup(context) {
   companion object {
     private fun hasGestureHandlerEnabledRootView(viewGroup: ViewGroup): Boolean {
       UiThreadUtil.assertOnUiThread()
+
       var parent = viewGroup.parent
       while (parent != null) {
         if (parent is RNGestureHandlerEnabledRootView || parent is RNGestureHandlerRootView) {
