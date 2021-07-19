8b61c21b2b40d0ce5f0bf825cc0d43a509b9c596
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-19 20:00:46 +0200

    Make ConfigurationHelper more idiomatic

--- android/src/main/java/com/swmansion/gesturehandler/react/RNViewConfigurationHelper.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNViewConfigurationHelper.kt
@@ -1,6 +1,5 @@
 package com.swmansion.gesturehandler.react
 
-import android.os.Build
 import android.view.View
 import android.view.ViewGroup
 import com.facebook.react.uimanager.PointerEvents
@@ -11,8 +10,9 @@ import com.swmansion.gesturehandler.ViewConfigurationHelper
 
 class RNViewConfigurationHelper : ViewConfigurationHelper {
   override fun getPointerEventsConfigForView(view: View): PointerEventsConfig {
-    val pointerEvents: PointerEvents
-    pointerEvents = if (view is ReactPointerEventsView) (view as ReactPointerEventsView).pointerEvents else PointerEvents.AUTO
+    val pointerEvents = if (view is ReactPointerEventsView) {
+      (view as ReactPointerEventsView).pointerEvents
+    } else PointerEvents.AUTO
 
     // Views that are disabled should never be the target of pointer events. However, their children
     // can be because some views (SwipeRefreshLayout) use enabled but still have children that can
@@ -24,12 +24,13 @@ class RNViewConfigurationHelper : ViewConfigurationHelper {
         return PointerEventsConfig.NONE
       }
     }
-    when (pointerEvents) {
-      PointerEvents.BOX_ONLY -> return PointerEventsConfig.BOX_ONLY
-      PointerEvents.BOX_NONE -> return PointerEventsConfig.BOX_NONE
-      PointerEvents.NONE -> return PointerEventsConfig.NONE
+
+    return when (pointerEvents!!) {
+      PointerEvents.BOX_ONLY -> PointerEventsConfig.BOX_ONLY
+      PointerEvents.BOX_NONE -> PointerEventsConfig.BOX_NONE
+      PointerEvents.NONE -> PointerEventsConfig.NONE
+      PointerEvents.AUTO -> PointerEventsConfig.AUTO
     }
-    return PointerEventsConfig.AUTO
   }
 
   override fun getChildInDrawingOrderAtIndex(parent: ViewGroup, index: Int): View {
@@ -39,13 +40,11 @@ class RNViewConfigurationHelper : ViewConfigurationHelper {
   }
 
   override fun isViewClippingChildren(view: ViewGroup): Boolean {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2 && !view.clipChildren) {
-      if (view is ReactViewGroup) {
-        val overflow = view.overflow
-        return "hidden" == overflow
-      }
-      return false
+    if (view.clipChildren) {
+      return true
     }
-    return true
+    return if (view is ReactViewGroup) {
+      "hidden" == view.overflow
+    } else false
   }
 }
