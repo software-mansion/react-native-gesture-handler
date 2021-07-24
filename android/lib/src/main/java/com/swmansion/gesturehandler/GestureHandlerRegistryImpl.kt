7e33adc876b966fa06ddbe85109b7c19d9f676b5
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-24 07:47:34 +0200

    Make GHRegistry more idiomatic

--- android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerRegistryImpl.kt
+++ android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerRegistryImpl.kt
@@ -5,19 +5,17 @@ import java.util.*
 
 class GestureHandlerRegistryImpl : GestureHandlerRegistry {
   private val mHandlers = WeakHashMap<View?, ArrayList<GestureHandler<*>>>()
-  fun <T : GestureHandler<*>?> registerHandlerForView(view: View?, handler: T): T {
+  fun <T : GestureHandler<*>> registerHandlerForView(view: View?, handler: T): T {
     var listToAdd = mHandlers[view]
     if (listToAdd == null) {
       listToAdd = ArrayList(1)
-      listToAdd.add(handler!!)
+      listToAdd.add(handler)
       mHandlers[view] = listToAdd
     } else {
-      listToAdd.add(handler!!)
+      listToAdd.add(handler)
     }
     return handler
   }
 
-  override fun getHandlersForView(view: View?): ArrayList<GestureHandler<*>> {
-    return mHandlers[view]!!
-  }
+  override fun getHandlersForView(view: View?) = mHandlers[view]
 }
