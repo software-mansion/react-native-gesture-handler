9393e65475eb04d4a12d3554971f67f0748b4b48
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-26 12:03:56 +0200

    Convert EventDataExtractor

---
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerEventDataExtractor.kt
@@ -0,0 +1,8 @@
+package com.swmansion.gesturehandler.react
+
+import com.facebook.react.bridge.WritableMap
+import com.swmansion.gesturehandler.GestureHandler
+
+interface RNGestureHandlerEventDataExtractor<T : GestureHandler<*>> {
+  fun extractEventData(handler: T, eventData: WritableMap?)
+}
