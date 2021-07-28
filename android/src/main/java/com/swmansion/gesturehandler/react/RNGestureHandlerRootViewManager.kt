79a3cb75975e8484526df4c17e046e5d7479f7b6
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-28 16:27:27 +0200

    Use expression returns

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootViewManager.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerRootViewManager.kt
@@ -1,10 +1,8 @@
 package com.swmansion.gesturehandler.react
 
-import com.facebook.react.common.MapBuilder
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.uimanager.ThemedReactContext
 import com.facebook.react.uimanager.ViewGroupManager
-import com.swmansion.gesturehandler.react.RNGestureHandlerRootViewManager
 
 /**
  * React native's view manager used for creating instances of []RNGestureHandlerRootView}. It
@@ -13,13 +11,8 @@ import com.swmansion.gesturehandler.react.RNGestureHandlerRootViewManager
  */
 @ReactModule(name = RNGestureHandlerRootViewManager.REACT_CLASS)
 class RNGestureHandlerRootViewManager : ViewGroupManager<RNGestureHandlerRootView>() {
-  override fun getName(): String {
-    return REACT_CLASS
-  }
-
-  override fun createViewInstance(reactContext: ThemedReactContext): RNGestureHandlerRootView {
-    return RNGestureHandlerRootView(reactContext)
-  }
+  override fun getName() = REACT_CLASS
+  override fun createViewInstance(reactContext: ThemedReactContext) = RNGestureHandlerRootView(reactContext)
 
   override fun onDropViewInstance(view: RNGestureHandlerRootView) {
     view.tearDown()
@@ -29,13 +22,11 @@ class RNGestureHandlerRootViewManager : ViewGroupManager<RNGestureHandlerRootVie
    * The following event configuration is necessary even if you are not using
    * GestureHandlerRootView component directly.
    */
-  override fun getExportedCustomDirectEventTypeConstants(): MutableMap<String, Any>? {
-    return MapBuilder.of<String, Map<String, String>>(
-      RNGestureHandlerEvent.EVENT_NAME,
-      MapBuilder.of("registrationName", RNGestureHandlerEvent.EVENT_NAME),
-      RNGestureHandlerStateChangeEvent.EVENT_NAME,
-      MapBuilder.of("registrationName", RNGestureHandlerStateChangeEvent.EVENT_NAME))
-  }
+  override fun getExportedCustomDirectEventTypeConstants(): Map<String, Map<String, String>> = mutableMapOf(
+    RNGestureHandlerEvent.EVENT_NAME to
+      mutableMapOf("registrationName" to RNGestureHandlerEvent.EVENT_NAME),
+    RNGestureHandlerStateChangeEvent.EVENT_NAME to
+      mutableMapOf("registrationName" to RNGestureHandlerStateChangeEvent.EVENT_NAME))
 
   companion object {
     const val REACT_CLASS = "GestureHandlerRootView"
