6e4f31faf3fe414cfa26177cc862a24c3491fd1f
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-29 17:10:43 +0200

    Rename fields

--- android/lib/src/main/java/com/swmansion/gesturehandler/LongPressGestureHandler.kt
+++ android/lib/src/main/java/com/swmansion/gesturehandler/LongPressGestureHandler.kt
@@ -6,54 +6,54 @@ import android.os.SystemClock
 import android.view.MotionEvent
 
 class LongPressGestureHandler(context: Context) : GestureHandler<LongPressGestureHandler>() {
-  private var mMinDurationMs = DEFAULT_MIN_DURATION_MS
-  private val mDefaultMaxDistSq: Float
-  private var mMaxDistSq: Float
-  private var mStartX = 0f
-  private var mStartY = 0f
-  private var mStartTime: Long = 0
-  private var mPreviousTime: Long = 0
-  private var mHandler: Handler? = null
-  
+  private var minDurationMs = DEFAULT_MIN_DURATION_MS
+  private val defaultMaxDistSq: Float
+  private var maxDistSq: Float
+  private var startX = 0f
+  private var startY = 0f
+  private var startTime: Long = 0
+  private var previousTime: Long = 0
+  private var handler: Handler? = null
+
   init {
     setShouldCancelWhenOutside(true)
-    mDefaultMaxDistSq = DEFAULT_MAX_DIST_DP * context.resources.displayMetrics.density
-    mMaxDistSq = mDefaultMaxDistSq
+    defaultMaxDistSq = DEFAULT_MAX_DIST_DP * context.resources.displayMetrics.density
+    maxDistSq = defaultMaxDistSq
   }
 
   override fun resetConfig() {
     super.resetConfig()
-    mMinDurationMs = DEFAULT_MIN_DURATION_MS
-    mMaxDistSq = mDefaultMaxDistSq
+    minDurationMs = DEFAULT_MIN_DURATION_MS
+    maxDistSq = defaultMaxDistSq
   }
 
   fun setMinDurationMs(minDurationMs: Long) {
-    mMinDurationMs = minDurationMs
+    this.minDurationMs = minDurationMs
   }
 
   fun setMaxDist(maxDist: Float): LongPressGestureHandler {
-    mMaxDistSq = maxDist * maxDist
+    maxDistSq = maxDist * maxDist
     return this
   }
 
   override fun onHandle(event: MotionEvent) {
     if (state == STATE_UNDETERMINED) {
-      mPreviousTime = SystemClock.uptimeMillis()
-      mStartTime = mPreviousTime
+      previousTime = SystemClock.uptimeMillis()
+      startTime = previousTime
       begin()
-      mStartX = event.rawX
-      mStartY = event.rawY
-      mHandler = Handler()
-      if (mMinDurationMs > 0) {
-        mHandler!!.postDelayed({ activate() }, mMinDurationMs)
-      } else if (mMinDurationMs == 0L) {
+      startX = event.rawX
+      startY = event.rawY
+      handler = Handler()
+      if (minDurationMs > 0) {
+        handler!!.postDelayed({ activate() }, minDurationMs)
+      } else if (minDurationMs == 0L) {
         activate()
       }
     }
     if (event.actionMasked == MotionEvent.ACTION_UP) {
-      if (mHandler != null) {
-        mHandler!!.removeCallbacksAndMessages(null)
-        mHandler = null
+      if (handler != null) {
+        handler!!.removeCallbacksAndMessages(null)
+        handler = null
       }
       if (state == STATE_ACTIVE) {
         end()
@@ -62,10 +62,10 @@ class LongPressGestureHandler(context: Context) : GestureHandler<LongPressGestur
       }
     } else {
       // calculate distance from start
-      val deltaX = event.rawX - mStartX
-      val deltaY = event.rawY - mStartY
+      val deltaX = event.rawX - startX
+      val deltaY = event.rawY - startY
       val distSq = deltaX * deltaX + deltaY * deltaY
-      if (distSq > mMaxDistSq) {
+      if (distSq > maxDistSq) {
         if (state == STATE_ACTIVE) {
           cancel()
         } else {
@@ -76,24 +76,24 @@ class LongPressGestureHandler(context: Context) : GestureHandler<LongPressGestur
   }
 
   override fun onStateChange(newState: Int, previousState: Int) {
-    if (mHandler != null) {
-      mHandler!!.removeCallbacksAndMessages(null)
-      mHandler = null
+    if (handler != null) {
+      handler!!.removeCallbacksAndMessages(null)
+      handler = null
     }
   }
 
   override fun dispatchStateChange(newState: Int, prevState: Int) {
-    mPreviousTime = SystemClock.uptimeMillis()
+    previousTime = SystemClock.uptimeMillis()
     super.dispatchStateChange(newState, prevState)
   }
 
   override fun dispatchTouchEvent(event: MotionEvent?) {
-    mPreviousTime = SystemClock.uptimeMillis()
+    previousTime = SystemClock.uptimeMillis()
     super.dispatchTouchEvent(event)
   }
 
   val duration: Int
-    get() = (mPreviousTime - mStartTime).toInt()
+    get() = (previousTime - startTime).toInt()
 
   companion object {
     private const val DEFAULT_MIN_DURATION_MS: Long = 500 // 1 sec
