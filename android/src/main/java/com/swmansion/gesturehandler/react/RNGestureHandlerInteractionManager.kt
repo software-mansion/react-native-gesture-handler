ea0063b45de1f6388ec289000e29894c9ece5124
Jakub Gonet <jakub.gonet@swmansion.com>
2021-07-29 16:40:29 +0200

    Simplify logic in shouldRecognizeSimultaneously & shouldWaitForHandlerFailure

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerInteractionManager.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerInteractionManager.kt
@@ -34,17 +34,8 @@ class RNGestureHandlerInteractionManager : GestureHandlerInteractionController {
     }
   }
 
-  override fun shouldWaitForHandlerFailure(handler: GestureHandler<*>, otherHandler: GestureHandler<*>): Boolean {
-    val waitForTags = mWaitForRelations[handler.tag]
-    if (waitForTags != null) {
-      for (i in waitForTags.indices) {
-        if (waitForTags[i] == otherHandler.tag) {
-          return true
-        }
-      }
-    }
-    return false
-  }
+  override fun shouldWaitForHandlerFailure(handler: GestureHandler<*>, otherHandler: GestureHandler<*>) =
+    mWaitForRelations[handler.tag]?.any { tag -> tag == otherHandler.tag } ?: false
 
   override fun shouldRequireHandlerToWaitForFailure(
     handler: GestureHandler<*>,
@@ -59,18 +50,8 @@ class RNGestureHandlerInteractionManager : GestureHandlerInteractionController {
 
   override fun shouldRecognizeSimultaneously(
     handler: GestureHandler<*>,
-    otherHandler: GestureHandler<*>
-  ): Boolean {
-    val simultHandlerTags = mSimultaneousRelations[handler.tag]
-    if (simultHandlerTags != null) {
-      for (i in simultHandlerTags.indices) {
-        if (simultHandlerTags[i] == otherHandler.tag) {
-          return true
-        }
-      }
-    }
-    return false
-  }
+    otherHandler: GestureHandler<*>,
+  ) = mSimultaneousRelations[handler.tag]?.any { tag -> tag == otherHandler.tag } ?: false
 
   fun reset() {
     mWaitForRelations.clear()
