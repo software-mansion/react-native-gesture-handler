922e454052161e801b29b6216ef01a2346501d04
Jakub Gonet <jakub.gonet@swmansion.com>
2021-08-03 18:59:57 +0200

    Convert GestureHandlerRegistryImpl to Kotlin

---
+++ android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerRegistryImpl.kt
@@ -0,0 +1,24 @@
+package com.swmansion.gesturehandler
+
+import android.view.View
+import com.swmansion.gesturehandler.GestureHandlerRegistry
+import java.util.*
+
+class GestureHandlerRegistryImpl : GestureHandlerRegistry {
+  private val mHandlers = WeakHashMap<View, ArrayList<GestureHandler<*>>>()
+  fun <T : GestureHandler<*>?> registerHandlerForView(view: View, handler: T): T {
+    var listToAdd = mHandlers[view]
+    if (listToAdd == null) {
+      listToAdd = ArrayList(1)
+      listToAdd.add(handler!!)
+      mHandlers[view] = listToAdd
+    } else {
+      listToAdd.add(handler!!)
+    }
+    return handler
+  }
+
+  override fun getHandlersForView(view: View): ArrayList<GestureHandler<*>> {
+    return mHandlers[view]!!
+  }
+}
