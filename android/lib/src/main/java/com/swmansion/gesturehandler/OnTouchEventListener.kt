52fe710d9066ffc64b1c20fb41985e8f5f943f8d
Jakub Gonet <jakub.gonet@swmansion.com>
2021-08-04 12:58:50 +0200

    Fix type errors

--- android/lib/src/main/java/com/swmansion/gesturehandler/OnTouchEventListener.kt
+++ android/lib/src/main/java/com/swmansion/gesturehandler/OnTouchEventListener.kt
@@ -2,7 +2,7 @@ package com.swmansion.gesturehandler
 
 import android.view.MotionEvent
 
-interface OnTouchEventListener<T : GestureHandler<*>?> {
+interface OnTouchEventListener<T : GestureHandler<T>> {
   fun onTouchEvent(handler: T, event: MotionEvent?)
   fun onStateChange(handler: T, newState: Int, oldState: Int)
 }
