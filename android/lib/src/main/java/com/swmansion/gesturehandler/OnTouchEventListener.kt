3bc460c878d86a33d2f3478f474890e4be3be67e
Jakub Gonet <jakub.gonet@swmansion.com>
2021-08-04 12:58:37 +0200

    Convert OnTouchEventListener to Kotlin

---
+++ android/lib/src/main/java/com/swmansion/gesturehandler/OnTouchEventListener.kt
@@ -0,0 +1,8 @@
+package com.swmansion.gesturehandler
+
+import android.view.MotionEvent
+
+interface OnTouchEventListener<T : GestureHandler<*>?> {
+  fun onTouchEvent(handler: T, event: MotionEvent?)
+  fun onStateChange(handler: T, newState: Int, oldState: Int)
+}
