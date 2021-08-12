f4137c74a314d9859c7fafce2c224d24d72bb66d
Jakub Piasecki <jakub.piasecki@swmansion.com>
2021-08-12 08:33:01 +0200

    Remove module export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export { Directions } from './Directions';
 export { State } from './State';
 export { default as gestureHandlerRootHOC } from './gestureHandlerRootHOC';
 export { default as GestureHandlerRootView } from './GestureHandlerRootView';
-export { default as GestureHandlerModule } from './RNGestureHandlerModule';
 export type {
   // event types
   GestureEvent,
@@ -47,7 +46,6 @@ export {
   LongPress,
   Gesture,
   findHandler,
-  Root,
 } from './handlers/GestureMonitor';
 export type {
   NativeViewGestureHandlerPayload,
