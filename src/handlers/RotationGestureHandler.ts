51eea049d6eb4dc7038ff50b8c3f72ed11762f7a
Jakub Gonet <jakub.gonet@swmansion.com>
2021-08-15 05:28:21 +0200

    Add RotationGH JSDoc

--- src/handlers/RotationGestureHandler.ts
+++ src/handlers/RotationGestureHandler.ts
@@ -5,9 +5,29 @@ import {
 } from './gestureHandlerCommon';
 
 export type RotationGestureHandlerEventPayload = {
+  /**
+   * Amount rotated, expressed in radians, from the gesture's focal point
+   * (anchor).
+   */
   rotation: number;
+
+  /**
+   * X coordinate, expressed in points, of the gesture's central focal point
+   * (anchor).
+   */
   anchorX: number;
+
+  /**
+   * Y coordinate, expressed in points, of the gesture's central focal point
+   * (anchor).
+   */
   anchorY: number;
+
+  /**
+   *
+   * Instantaneous velocity, expressed in point units per second, of the
+   * gesture.
+   */
   velocity: number;
 };
 
