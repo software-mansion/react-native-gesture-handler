68a617b8e03d5a7d5d96367fc107b845d8e414c6
Jakub Piasecki <jakub.piasecki@swmansion.com>
2021-08-18 12:01:48 +0200

    Add Reanimated as dependency

--- examples/Example/babel.config.js
+++ examples/Example/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: [
+    'react-native-reanimated/plugin',
     '@babel/plugin-transform-modules-commonjs',
     [
       'module-resolver',
