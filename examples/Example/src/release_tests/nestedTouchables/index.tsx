9d6c81435d0bbdebc70b447feae9b834a476a528
Jakub Piasecki <jakub.piasecki@swmansion.com>
2021-08-27 14:51:57 +0200

    Add example desccription

--- examples/Example/src/release_tests/nestedTouchables/index.tsx
+++ examples/Example/src/release_tests/nestedTouchables/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { TouchableOpacity, ScrollView } from 'react-native-gesture-handler';
 import { LoremIpsum } from '../../common';
 
 export default function Example() {
   return (
     <ScrollView>
-      <LoremIpsum words={40} />
-      <Text>Touchables with `exclusive` set to true:</Text>
+      <Text style={styles.text}>
+        When the `exclusive` prop is set to true, the touchable is not allowed
+        to begin recognizing gesture (thus it shouldn&apos;t show feedback) when
+        another touchable is pressed. Similarly when the touchable with
+        `exclusive` set to true is pressed, no other touchable should begin
+        recognizing gesture.
+      </Text>
+      <Text style={styles.text}>Touchables with `exclusive` set to true:</Text>
       <Boxes />
-      <Text>Touchables with `exclusive` set to false:</Text>
+      <Text style={styles.text}>
+        When the `exclusive` prop is set to false, the touchable is allowed to
+        begin recognizing gesture when another touchable (with `exclusive` prop
+        set to false) is already pressed.
+      </Text>
+      <Text style={styles.text}>Touchables with `exclusive` set to false:</Text>
       <Boxes exclusive={false} />
-      <Text>Touchables with `exclusive` set to false:</Text>
+      <Text style={styles.text}>Touchables with `exclusive` set to false:</Text>
       <Boxes exclusive={false} />
       <LoremIpsum words={40} />
     </ScrollView>
@@ -47,3 +58,9 @@ function Boxes(props: { exclusive?: boolean }) {
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    padding: 10,
+  },
+});
