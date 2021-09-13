4236385d5e73cdc69cbd1dc92185a9e020d23733
Dawid Jagieła <dawid.jagiela@ext.chime.com>
2021-09-13 09:33:45 +0200

    add velocity drag

--- examples/Example/src/new_api/chat_heads/index.tsx
+++ examples/Example/src/new_api/chat_heads/index.tsx
@@ -2,20 +2,19 @@ import React, { useState } from 'react';
 import { StyleSheet, ImageStyle, LayoutChangeEvent } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
-  useAnimatedReaction,
   useAnimatedStyle,
   useDerivedValue,
   useSharedValue,
   withSpring,
 } from 'react-native-reanimated';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useHeaderHeight } from '@react-navigation/stack';
 
-const HEADER_HEIGHT = 44; // let's assume it's always the same for ease of understanding ;)
 const CHAT_HEADS = [
   { imageUrl: 'https://avatars0.githubusercontent.com/u/379606?v=4&s=460' },
-  { imageUrl: 'https://avatars.githubusercontent.com/u/15989228?v=4&s=460' },
   { imageUrl: 'https://avatars3.githubusercontent.com/u/90494?v=4&s=460' },
   { imageUrl: 'https://avatars3.githubusercontent.com/u/726445?v=4&s=460' },
+  { imageUrl: 'https://avatars.githubusercontent.com/u/15989228?v=4&s=460' },
 ];
 
 interface AnimatedOffset {
@@ -67,11 +66,24 @@ function useOffsetAnimatedValue() {
   };
 }
 
+function clampToValues({
+  value,
+  bottom,
+  top,
+}: {
+  value: number;
+  bottom: number;
+  top: number;
+}) {
+  return Math.max(bottom, Math.min(value, top));
+}
+
 const Example = () => {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
-  const dragOffset = useOffsetAnimatedValue();
+  const panOffset = useOffsetAnimatedValue();
   const mainChatHeadPosition = useOffsetAnimatedValue();
   const chatHeadsOffsets = CHAT_HEADS.map(useOffsetAnimatedValue);
+  const headerHeight = useHeaderHeight();
 
   const onLayout = ({ nativeEvent }: LayoutChangeEvent) => {
     const { width, height } = nativeEvent.layout;
@@ -81,16 +93,27 @@ const Example = () => {
   const panHandler = Gesture.Pan()
     .onUpdate(({ translationX, translationY }) => {
       'worklet';
-      dragOffset.x.value = mainChatHeadPosition.x.value + translationX;
-      dragOffset.y.value = mainChatHeadPosition.y.value + translationY;
+      panOffset.x.value = mainChatHeadPosition.x.value + translationX;
+      panOffset.y.value = mainChatHeadPosition.y.value + translationY;
     })
-    .onEnd(({ absoluteX, absoluteY }) => {
+    .onEnd(({ absoluteX, absoluteY, velocityX, velocityY }) => {
       const { height, width } = dimensions;
 
-      const distFromTop = absoluteY - HEADER_HEIGHT;
-      const distFromBottom = height - absoluteY;
-      const distFromLeft = absoluteX;
-      const distFromRight = width - absoluteX;
+      const velocityDragX = clampToValues({
+        value: velocityX * 0.05,
+        bottom: -100,
+        top: 100,
+      });
+      const velocityDragY = clampToValues({
+        value: velocityY * 0.05,
+        bottom: -100,
+        top: 100,
+      });
+
+      const distFromTop = absoluteY + velocityDragY - headerHeight;
+      const distFromBottom = height + velocityDragY - absoluteY;
+      const distFromLeft = absoluteX + velocityDragX;
+      const distFromRight = width - absoluteX + velocityDragX;
 
       const minDist = Math.min(
         distFromTop,
@@ -102,27 +125,31 @@ const Example = () => {
       // drag to the edge
       switch (minDist) {
         case distFromTop: {
-          dragOffset.y.value = withSpring(-IMAGE_SIZE / 2);
+          panOffset.y.value = withSpring(-IMAGE_SIZE / 2);
+          panOffset.x.value = withSpring(panOffset.x.value + velocityDragX);
           mainChatHeadPosition.y.value = -IMAGE_SIZE / 2;
-          mainChatHeadPosition.x.value = dragOffset.x.value;
+          mainChatHeadPosition.x.value = panOffset.x.value;
           break;
         }
         case distFromBottom: {
-          dragOffset.y.value = withSpring(height - IMAGE_SIZE / 2);
+          panOffset.y.value = withSpring(height - IMAGE_SIZE / 2);
+          panOffset.x.value = withSpring(panOffset.x.value + velocityDragX);
           mainChatHeadPosition.y.value = height - IMAGE_SIZE / 2;
-          mainChatHeadPosition.x.value = dragOffset.x.value;
+          mainChatHeadPosition.x.value = panOffset.x.value;
           break;
         }
         case distFromLeft: {
-          dragOffset.x.value = withSpring(-IMAGE_SIZE / 2);
+          panOffset.x.value = withSpring(-IMAGE_SIZE / 2);
+          panOffset.y.value = withSpring(panOffset.y.value + velocityDragY);
           mainChatHeadPosition.x.value = -IMAGE_SIZE / 2;
-          mainChatHeadPosition.y.value = dragOffset.y.value;
+          mainChatHeadPosition.y.value = panOffset.y.value;
           break;
         }
         case distFromRight: {
-          dragOffset.x.value = withSpring(width - IMAGE_SIZE / 2);
+          panOffset.x.value = withSpring(width - IMAGE_SIZE / 2);
+          panOffset.y.value = withSpring(panOffset.y.value + velocityDragY);
           mainChatHeadPosition.x.value = width - IMAGE_SIZE / 2;
-          mainChatHeadPosition.y.value = dragOffset.y.value;
+          mainChatHeadPosition.y.value = panOffset.y.value;
           break;
         }
       }
@@ -134,7 +161,7 @@ const Example = () => {
       return (
         <GestureDetector gesture={panHandler} key={imageUrl}>
           <FollowingChatHead
-            offsetToFollow={dragOffset}
+            offsetToFollow={panOffset}
             imageUri={imageUrl}
             offset={headOffset}
           />
