e2e56415c9c3fe5f16166831c0d089c78dd520bf
Jakub Piasecki <jakub.piasecki@swmansion.com>
2021-09-20 14:38:48 +0200

    Create a BetterDrawerLayout component (#1623)
    Create a new BetterDrawerLayout component built with the new API of the gesture handler and Reanimated 2. It is supposed to be an almost drop-in replacement for the previous DrawerLayout.

---
+++ examples/Example/src/basic/betterHorizontalDrawer/index.tsx
@@ -0,0 +1,160 @@
+import React, { useRef, useState } from 'react';
+
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+import {
+  DrawerLayoutController,
+  DrawerType,
+  RectButton,
+  BetterDrawerLayout,
+} from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle } from 'react-native-reanimated';
+
+const TYPES: DrawerType[] = ['front', 'back', 'back', 'slide'];
+const PARALLAX = [false, false, true, false];
+
+interface PageProps {
+  fromLeft: boolean;
+  type: DrawerType;
+  parallaxOn: boolean;
+  flipSide: () => void;
+  nextType: () => void;
+  openDrawer: () => void;
+}
+
+function Page({
+  fromLeft,
+  type,
+  parallaxOn,
+  flipSide,
+  nextType,
+  openDrawer,
+}: PageProps) {
+  return (
+    <View style={styles.page}>
+      <Text style={styles.pageText}>Hi 👋</Text>
+      <RectButton style={styles.rectButton} onPress={flipSide}>
+        <Text style={styles.rectButtonText}>
+          Drawer to the {fromLeft ? 'left' : 'right'}! {'->'} Flip
+        </Text>
+      </RectButton>
+      <RectButton style={styles.rectButton} onPress={nextType}>
+        <Text style={styles.rectButtonText}>
+          Type {type} {parallaxOn && 'with parallax!'} -&gt; Next
+        </Text>
+      </RectButton>
+      <RectButton style={styles.rectButton} onPress={openDrawer}>
+        <Text style={styles.rectButtonText}>Open drawer</Text>
+      </RectButton>
+      <TextInput
+        style={styles.pageInput}
+        placeholder="Just an input field to test kb dismiss mode"
+      />
+    </View>
+  );
+}
+
+function DrawerContent(
+  offset: Animated.SharedValue<number>,
+  parallax: boolean,
+  fromLeft: boolean
+) {
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: parallax
+          ? Animated.interpolate(offset.value, [0, 1], [fromLeft ? -50 : 50, 0])
+          : 0,
+      },
+    ],
+  }));
+
+  return (
+    <Animated.View style={[styles.drawerContainer, animatedStyles]}>
+      <Text style={styles.drawerText}>
+        {parallax ? 'Drawer with parallax' : 'Drawer'}
+      </Text>
+    </Animated.View>
+  );
+}
+
+export default function Example() {
+  const [onLeft, setOnLeft] = useState(true);
+  const [type, setType] = useState(0);
+  const controller = useRef<DrawerLayoutController>(null);
+
+  return (
+    <View style={styles.container}>
+      <BetterDrawerLayout
+        drawerPosition={onLeft ? 'left' : 'right'}
+        drawerType={TYPES[type]}
+        renderNavigationView={(offset) => {
+          return DrawerContent(offset, PARALLAX[type], onLeft);
+        }}
+        keyboardDismissMode="on-drag"
+        drawerBackgroundColor="white"
+        ref={controller}>
+        <Page
+          fromLeft={onLeft}
+          flipSide={() => {
+            setOnLeft(!onLeft);
+          }}
+          type={TYPES[type]}
+          nextType={() => {
+            setType((type + 1) % TYPES.length);
+          }}
+          parallaxOn={PARALLAX[type]}
+          openDrawer={() => {
+            controller.current?.open();
+          }}
+        />
+      </BetterDrawerLayout>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  page: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    paddingTop: 40,
+    backgroundColor: 'gray',
+  },
+  pageText: {
+    fontSize: 21,
+    color: 'white',
+  },
+  rectButton: {
+    height: 60,
+    padding: 10,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: 'white',
+  },
+  rectButtonText: {
+    backgroundColor: 'transparent',
+  },
+  drawerContainer: {
+    flex: 1,
+    paddingTop: 10,
+  },
+  pageInput: {
+    height: 60,
+    padding: 10,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    backgroundColor: '#eee',
+  },
+  drawerText: {
+    margin: 10,
+    fontSize: 15,
+    textAlign: 'left',
+  },
+});
