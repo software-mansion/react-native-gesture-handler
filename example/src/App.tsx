8cf5d24e44f0543ef561454ed9b7ce0f05784340
Tomasz Zawadzki <tomasz.zawadzki@swmansion.com>
2021-11-30 09:51:27 +0100

    Update examples status list

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,19 +1,19 @@
 // old examples for old api
-import OverflowParent from './release_tests/overflowParent'; // doesn't work (Animated)
-import DoublePinchRotate from './release_tests/doubleScalePinchAndRotate'; // doesn't work (Animated)
-import DoubleDraggable from './release_tests/doubleDraggable'; // doesn't work (Animated)
+import OverflowParent from './release_tests/overflowParent'; // OK (but balls draggable only within rectangle)
+import DoublePinchRotate from './release_tests/doubleScalePinchAndRotate'; // not checked yet
+import DoubleDraggable from './release_tests/doubleDraggable'; // OK
 import { ComboWithGHScroll } from './release_tests/combo'; // doesn't work ("requireNativeComponent: "RNCSlider" was not found in the UIManager")
 import { TouchablesIndex, TouchableExample } from './release_tests/touchables'; // doesn't work ("undefined is not an object (evaluating 'this.props.route.params')")
 import Rows from './release_tests/rows'; // doesn't work (Unimplemented component: RNGestureHandlerButton)
 import NestedTouchables from './release_tests/nestedTouchables'; // doesn't work (Unimplemented component: RNGestureHandlerButton)
 import NestedGestureHandlerRootViewWithModal from './release_tests/nestedGHRootViewWithModal'; // doesn't work (Unimplemented component: RNGestureHandlerButton)
-import { PinchableBox } from './recipes/scaleAndRotate'; // doesn't work (Animated)
+import { PinchableBox } from './recipes/scaleAndRotate'; // OK
 import PanAndScroll from './recipes/panAndScroll'; // OK
-import { BottomSheet } from './showcase/bottomSheet'; // doesn't work (Animated)
+import { BottomSheet } from './showcase/bottomSheet'; // OK
 import Swipeables from './showcase/swipeable'; // doesn't work (Unimplemented component: RNGestureHandlerButton)
-import ChatHeads from './showcase/chatHeads'; // doesn't work (Animated)
+import ChatHeads from './showcase/chatHeads'; // always sticks to the top for USE_NATIVE_DRIVER=false, animations played sequentially for USE_NATIVE_DRIVER=true
 import { DraggableBox } from './basic/draggable'; // OK
-import MultiTap from './basic/multitap'; // doesn't work (Animated)
+import MultiTap from './basic/multitap'; // OK
 import BouncingBox from './basic/bouncing'; // OK
 import PanResponder from './basic/panResponder'; // doesn't work ("setNativeProps is not currently supported") // TODO: migrate setNativeProps
 import BetterHorizontalDrawer from './basic/betterHorizontalDrawer'; // doesn't work (Unimplemented component: RNGestureHandlerButton)
@@ -35,7 +35,7 @@ import DragNDrop from './new_api/drag_n_drop'; // doesn't work (weird positions)
 // new examples for new api
 import FabricReanimatedExample from './new_api/fabric/FabricReanimatedExample'; // OK
 import FabricCallbacksExample from './new_api/fabric/FabricCallbacksExample'; // OK
-import FabricPanGestureHandlerExample from './new_api/fabric/FabricPanGestureHandlerExample';
+import FabricPanGestureHandlerExample from './new_api/fabric/FabricPanGestureHandlerExample'; // OK
 
 // current example
-export default DraggableBox;
+export default FabricPanGestureHandlerExample;
