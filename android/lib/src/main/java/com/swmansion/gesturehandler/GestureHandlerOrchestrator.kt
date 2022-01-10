77e6072d0b2f1843fed719ade8704373516a9b53
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-01-10 15:45:19 +0100

    Fix awaiting handler not ending (#1774)
    On Android when a handler awaiting another one tries to activate its state is changed to ACTIVE but event with that change is delayed until the other finishes. If the awaiting handler were to fail before that happens, state change event with information about its failure would not be sent.
    
    This PR adds a workaround that sends state change event when the handler is active internally but still awaiting another one.

--- android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerOrchestrator.kt
+++ android/lib/src/main/java/com/swmansion/gesturehandler/GestureHandlerOrchestrator.kt
@@ -142,6 +142,10 @@ class GestureHandlerOrchestrator(
     } else if (prevState == GestureHandler.STATE_ACTIVE || prevState == GestureHandler.STATE_END) {
       if (handler.isActive) {
         handler.dispatchStateChange(newState, prevState)
+      } else if (prevState == GestureHandler.STATE_ACTIVE) {
+        // handle edge case where handler awaiting for another one tries to activate but finishes
+        // before the other would not send state change event upon ending
+        handler.dispatchStateChange(newState, GestureHandler.STATE_BEGAN)
       }
     } else if (prevState != GestureHandler.STATE_UNDETERMINED || newState != GestureHandler.STATE_CANCELLED) {
       // If handler is changing state from UNDETERMINED to CANCELLED, the state change event shouldn't
