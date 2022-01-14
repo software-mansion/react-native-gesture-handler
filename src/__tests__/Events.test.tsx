47b9e1f0b7ef89b0e13236b38aec4bf36aef912d
Jakub Gonet <jakub.gonet@swmansion.com>
2022-01-14 14:37:51 +0100

    Move __tests__ to src/

--- src/__tests__/Events.test.tsx
+++ src/__tests__/Events.test.tsx
@@ -10,9 +10,9 @@ import {
   RotationGestureHandler,
   PinchGestureHandler,
   Gesture,
-  GestureDetector
-} from 'react-native-gesture-handler';
-import { 
+  GestureDetector,
+} from '../index';
+import {
   fireTapGestureHandler,
   firePanGestureHandler,
   fireLongPressGestureHandler,
@@ -20,24 +20,24 @@ import {
   fireFlingGestureHandler,
   firePinchGestureHandler,
   EventDataTypeV1,
-  resetGestureHandlerRegistry
-} from '../src/jestUtils'
+  resetGestureHandlerRegistry,
+} from '../jestUtils';
 import { useAnimatedGestureHandler } from 'react-native-reanimated';
 
 beforeEach(() => {
   resetGestureHandlerRegistry();
-})
+});
 
 const mockEventFunctions = () => {
   return {
-    begin: jest.fn(), 
-    progress: jest.fn(), 
+    begin: jest.fn(),
+    progress: jest.fn(),
     end: jest.fn(),
     fail: jest.fn(),
     cancel: jest.fn(),
-    finish: jest.fn()
+    finish: jest.fn(),
   };
-}
+};
 
 const assertEventCalls = (eventFunctions, counts) => {
   expect(eventFunctions.begin).toHaveBeenCalledTimes(
@@ -49,29 +49,31 @@ const assertEventCalls = (eventFunctions, counts) => {
   expect(eventFunctions.end).toHaveBeenCalledTimes(
     counts?.end !== undefined ? counts.end : 1
   );
-}
+};
 
 const App = (props) => {
   const eventHandler = useAnimatedGestureHandler({
     onStart: () => props.eventFunctions.begin(),
     onActive: () => props.eventFunctions.progress(),
-    onEnd: () => props.eventFunctions.end()
+    onEnd: () => props.eventFunctions.end(),
   });
-
   return (
     <GestureHandlerRootView>
       <TapGestureHandler testId={1} onHandlerStateChange={eventHandler}>
         <Text>TapGestureHandlerTest</Text>
       </TapGestureHandler>
 
-      <PanGestureHandler testId={2} onHandlerStateChange={eventHandler} onGestureEvent={eventHandler}>
+      <PanGestureHandler
+        testId={2}
+        onHandlerStateChange={eventHandler}
+        onGestureEvent={eventHandler}>
         <Text>PanGestureHandlerTest</Text>
       </PanGestureHandler>
 
       <LongPressGestureHandler testId={3} onHandlerStateChange={eventHandler}>
         <Text>LongPressGestureHandlerTest</Text>
       </LongPressGestureHandler>
-      
+
       <RotationGestureHandler testId={4} onHandlerStateChange={eventHandler}>
         <Text>RotationGestureHandlerTest</Text>
       </RotationGestureHandler>
@@ -92,7 +94,6 @@ const App = (props) => {
           </TapGestureHandler>
         </View>
       </PanGestureHandler>
-
     </GestureHandlerRootView>
   );
 };
@@ -100,22 +101,23 @@ const App = (props) => {
 test('test fireTapGestureHandler', () => {
   const eventFunctions = mockEventFunctions();
   render(<App eventFunctions={eventFunctions} />);
-  fireTapGestureHandler(1, 
-    [
-      {x: 1, y: 1}, 
-      {x: 2, y: 2},
-      {x: 4, y: 4, type: EventDataTypeV1.onCancel}
-    ]
-  );
+  fireTapGestureHandler(1, [
+    { x: 1, y: 1 },
+    { x: 2, y: 2 },
+    { x: 4, y: 4, type: EventDataTypeV1.onCancel },
+  ]);
   assertEventCalls(eventFunctions);
 });
 
 test('test firePanGestureHandler', () => {
   const eventFunctions = mockEventFunctions();
   render(<App eventFunctions={eventFunctions} />);
-  firePanGestureHandler(2,
-    [{ x: 1, y: 1 }, { x: 2, y: 2 }, { x: 3, y: 3 }, { x: 4, y: 4 }]
-  );
+  firePanGestureHandler(2, [
+    { x: 1, y: 1 },
+    { x: 2, y: 2 },
+    { x: 3, y: 3 },
+    { x: 4, y: 4 },
+  ]);
   assertEventCalls(eventFunctions, { progress: 3 });
 });
 
@@ -156,22 +158,22 @@ test('test nestedGestureHandler', () => {
   assertEventCalls(eventFunctions, { begin: 3, progress: 3, end: 3 });
 });
 
-const AppAPIv2 = props => {
+const AppAPIv2 = (props) => {
   const tap = Gesture.Tap()
     .onBegin(() => {
-      props.eventFunctions.begin()
+      props.eventFunctions.begin();
     })
     .onEnd(() => {
-      props.eventFunctions.progress()
+      props.eventFunctions.progress();
     })
     .withTestId(1);
 
   const pan = Gesture.Pan()
     .onBegin(() => {
-      props.eventFunctions.begin()
+      props.eventFunctions.begin();
     })
     .onEnd(() => {
-      props.eventFunctions.progress()
+      props.eventFunctions.progress();
     })
     .withTestId(2);
 
