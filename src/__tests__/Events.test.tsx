82a0e452787aa75156d09413eba86a12f4f23886
Jakub Gonet <jakub.gonet@swmansion.com>
2022-01-16 17:49:12 +0100

    WIP: new test API

--- src/__tests__/Events.test.tsx
+++ src/__tests__/Events.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { fireEvent, render } from '@testing-library/react-native';
 import { Text, View } from 'react-native';
 import {
   GestureHandlerRootView,
@@ -12,21 +12,7 @@ import {
   Gesture,
   GestureDetector,
 } from '../index';
-import {
-  fireTapGestureHandler,
-  firePanGestureHandler,
-  fireLongPressGestureHandler,
-  fireRotationGestureHandler,
-  fireFlingGestureHandler,
-  firePinchGestureHandler,
-  EventDataTypeV1,
-  resetGestureHandlerRegistry,
-} from '../jestUtils';
 import { useAnimatedGestureHandler } from 'react-native-reanimated';
-
-beforeEach(() => {
-  resetGestureHandlerRegistry();
-});
 
 const mockEventFunctions = () => {
   return {
@@ -39,160 +25,129 @@ const mockEventFunctions = () => {
   };
 };
 
-const assertEventCalls = (eventFunctions, counts) => {
-  expect(eventFunctions.begin).toHaveBeenCalledTimes(
-    counts?.begin !== undefined ? counts.begin : 1
-  );
-  expect(eventFunctions.progress).toHaveBeenCalledTimes(
-    counts?.progress !== undefined ? counts.progress : 1
-  );
-  expect(eventFunctions.end).toHaveBeenCalledTimes(
-    counts?.end !== undefined ? counts.end : 1
-  );
-};
+interface V1ApiProps {
+  eventFunctions: {
+    begin: () => void;
+    progress: () => void;
+    end: () => void;
+  };
+}
 
-const App = (props) => {
+function V1Api({ eventFunctions: { begin, progress, end } }: V1ApiProps) {
   const eventHandler = useAnimatedGestureHandler({
-    onStart: () => props.eventFunctions.begin(),
-    onActive: () => props.eventFunctions.progress(),
-    onEnd: () => props.eventFunctions.end(),
+    onStart: begin,
+    onActive: progress,
+    onEnd: end,
   });
   return (
     <GestureHandlerRootView>
-      <TapGestureHandler testId={1} onHandlerStateChange={eventHandler}>
-        <Text>TapGestureHandlerTest</Text>
+      <TapGestureHandler testID="tap" onHandlerStateChange={eventHandler}>
+        <Text>Tap handler</Text>
       </TapGestureHandler>
 
       <PanGestureHandler
-        testId={2}
+        testID="pan"
         onHandlerStateChange={eventHandler}
         onGestureEvent={eventHandler}>
-        <Text>PanGestureHandlerTest</Text>
+        <Text>Pan handler</Text>
       </PanGestureHandler>
 
-      <LongPressGestureHandler testId={3} onHandlerStateChange={eventHandler}>
-        <Text>LongPressGestureHandlerTest</Text>
+      <LongPressGestureHandler
+        testID="longPress"
+        onHandlerStateChange={eventHandler}>
+        <Text>Long press handler</Text>
       </LongPressGestureHandler>
 
-      <RotationGestureHandler testId={4} onHandlerStateChange={eventHandler}>
-        <Text>RotationGestureHandlerTest</Text>
+      <RotationGestureHandler
+        testID="rotation"
+        onHandlerStateChange={eventHandler}>
+        <Text>Rotation handler</Text>
       </RotationGestureHandler>
 
-      <FlingGestureHandler testId={5} onHandlerStateChange={eventHandler}>
-        <Text>FlingGestureHandlerTest</Text>
+      <FlingGestureHandler testID="fling" onHandlerStateChange={eventHandler}>
+        <Text>Fling handler</Text>
       </FlingGestureHandler>
 
-      <PinchGestureHandler testId={6} onHandlerStateChange={eventHandler}>
-        <Text>PinchGestureHandlerTest</Text>
+      <PinchGestureHandler testID="pinch" onHandlerStateChange={eventHandler}>
+        <Text>Pinch handler</Text>
       </PinchGestureHandler>
 
-      <PanGestureHandler testId={7} onHandlerStateChange={eventHandler}>
+      <PanGestureHandler
+        testID="pan-nested"
+        onHandlerStateChange={eventHandler}>
         <View>
-          <Text>NestedGestureHandlerTest1</Text>
-          <TapGestureHandler testId={8} onHandlerStateChange={eventHandler}>
-            <Text>NestedGestureHandlerTest2</Text>
+          <Text>Nested gesture handlers - outside</Text>
+          <TapGestureHandler
+            testID="tap-nested"
+            onHandlerStateChange={eventHandler}>
+            <Text>Nested gesture handlers - inside</Text>
           </TapGestureHandler>
         </View>
       </PanGestureHandler>
     </GestureHandlerRootView>
   );
-};
+}
 
-test('test fireTapGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  fireTapGestureHandler(1, [
-    { x: 1, y: 1 },
-    { x: 2, y: 2 },
-    { x: 4, y: 4, type: EventDataTypeV1.onCancel },
-  ]);
-  assertEventCalls(eventFunctions);
-});
-
-test('test firePanGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  firePanGestureHandler(2, [
-    { x: 1, y: 1 },
-    { x: 2, y: 2 },
-    { x: 3, y: 3 },
-    { x: 4, y: 4 },
-  ]);
-  assertEventCalls(eventFunctions, { progress: 3 });
-});
-
-test('test fireLongPressGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  fireLongPressGestureHandler(3);
-  assertEventCalls(eventFunctions);
-});
-
-test('test fireRotationGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  fireRotationGestureHandler(4);
-  assertEventCalls(eventFunctions);
-});
-
-test('test fireFlingGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  fireFlingGestureHandler(5);
-  assertEventCalls(eventFunctions);
-});
-
-test('test firePinchGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  firePinchGestureHandler(6);
-  assertEventCalls(eventFunctions);
-});
-
-test('test nestedGestureHandler', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<App eventFunctions={eventFunctions} />);
-  firePanGestureHandler(7);
-  firePanGestureHandler(7);
-  fireTapGestureHandler(8);
-  assertEventCalls(eventFunctions, { begin: 3, progress: 3, end: 3 });
-});
-
-const AppAPIv2 = (props) => {
-  const tap = Gesture.Tap()
-    .onBegin(() => {
-      props.eventFunctions.begin();
-    })
-    .onEnd(() => {
-      props.eventFunctions.progress();
-    })
-    .withTestId(1);
+const V2Api = ({ eventFunctions: { begin, progress, end } }: AppProps) => {
+  const tap = Gesture.Tap().onBegin(begin).onEnd(end).withTestId('tap');
 
   const pan = Gesture.Pan()
-    .onBegin(() => {
-      props.eventFunctions.begin();
-    })
-    .onEnd(() => {
-      props.eventFunctions.progress();
-    })
-    .withTestId(2);
+    .onBegin(begin)
+    .onUpdate(progress)
+    .onEnd(end)
+    .withTestId('pan');
 
   return (
     <GestureHandlerRootView>
       <GestureDetector gesture={Gesture.Race(tap, pan)}>
-        <View>
-          <Text>Text</Text>
-        </View>
+        <Text>v2 API test</Text>
       </GestureDetector>
     </GestureHandlerRootView>
   );
 };
 
-test('test API v2', () => {
-  const eventFunctions = mockEventFunctions();
-  render(<AppAPIv2 eventFunctions={eventFunctions} />);
-  fireTapGestureHandler(1);
-  firePanGestureHandler(2);
-  expect(eventFunctions.begin).toHaveBeenCalledTimes(2);
-  expect(eventFunctions.progress).toHaveBeenCalledTimes(2);
+// eslint-disable-next-line jest/expect-expect
+it('receives events', () => {
+  const events = mockEventFunctions();
+  const { getByTestId } = render(<V1Api eventFunctions={events} />);
+  fireEvent(getByTestId('tap'), 'onGestureHandlerEvent', {});
 });
+
+// TODO
+
+// v1
+// v2
+
+// Tap
+// Pan
+// Long press
+// Rotation
+// Fling
+// Pinch
+// Nested
+
+// begin: () => void;
+// progress: () => void;
+// end: () => void;
+// fail: () => void;
+// cancel: () => void;
+// finish: () => void;
+
+// .toHaveBeenCalledTimes(number)
+// .toHaveBeenCalledWith(arg1, arg2, ...)
+
+it.todo('calls begin callback only once');
+it.todo(
+  'calls active callback times that correspond to event data list length - 1'
+);
+it.todo('calls callback that was specified in `type` param');
+it.todo("can't call fail and end simultaneously");
+
+// test('API v2', () => {
+//   const eventFunctions = mockEventFunctions();
+//   render(<AppAPIv2 eventFunctions={eventFunctions} />);
+//   fireTapGestureHandler(1);
+//   firePanGestureHandler(2);
+//   expect(eventFunctions.begin).toHaveBeenCalledTimes(2);
+//   expect(eventFunctions.progress).toHaveBeenCalledTimes(2);
+// });
