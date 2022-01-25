f08283c6a9a3c9a72440fdf91f1bade48ae49db1
Jakub Gonet <jakub.gonet@swmansion.com>
2022-01-25 14:15:56 +0100

    Fix impl of withPrevAndCurrent
    Previously it used original array for previous element

--- src/utils.ts
+++ src/utils.ts
@@ -6,11 +6,32 @@ export function toArray<T>(object: T | T[]): T[] {
   return object;
 }
 
-export function withPrevAndCurrent<T>(
-  mapFn: (previous: T | null, current: T) => T
-) {
-  return (currentElement: T, i: number, array: T[]) => {
-    const previousElement = i > 0 ? array[i - 1] : null;
-    return mapFn(previousElement, currentElement);
-  };
+/**
+ * Used with reduce on lists, without initialValue specified
+ * @param mapFn function returning next element based on previous and current one
+ * @returns list of transformed elements
+ */
+
+export type withPrevAndCurrentMapFn<T, Transformed> = (
+  previous: Transformed | null,
+  current: T
+) => Transformed;
+export function withPrevAndCurrent<T, Transformed>(
+  array: T[],
+  mapFn: withPrevAndCurrentMapFn<T, Transformed>
+): Transformed[] {
+  const previousArr: (null | Transformed)[] = [null];
+  const currentArr = [...array];
+  const transformedArr: Transformed[] = [];
+  currentArr.forEach((current, i) => {
+    const previous = previousArr[i];
+    const transformed = mapFn(previous, current);
+    previousArr.push(transformed);
+    transformedArr.push(transformed);
+  });
+  return transformedArr;
+}
+
+export function hasProperty(object: Record<string, unknown>, key: string) {
+  return Object.prototype.hasOwnProperty.call(object, key);
 }
