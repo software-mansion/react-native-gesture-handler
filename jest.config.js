166c0df87e387ddb0793248f6e441d2f7f15417f
Jakub Gonet <jakub.gonet@swmansion.com>
2022-01-26 14:20:29 +0100

    Remove unnecessary config keys

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ['./jestSetup.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   transformIgnorePatterns: [
     "node_modules/?!(react-native-reanimated)",
     "node_modules/?!(react-native)"
