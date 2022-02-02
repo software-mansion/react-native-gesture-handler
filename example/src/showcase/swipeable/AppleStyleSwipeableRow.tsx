54384192bf89c24ad606aab18b16b63530f186c4
Jakub Gonet <jakub.gonet@swmansion.com>
2022-02-02 21:25:45 +0100

    Improve wording

--- example/src/showcase/swipeable/AppleStyleSwipeableRow.tsx
+++ example/src/showcase/swipeable/AppleStyleSwipeableRow.tsx
@@ -98,7 +98,7 @@ export default class AppleStyleSwipeableRow extends Component {
         renderLeftActions={this.renderLeftActions}
         renderRightActions={this.renderRightActions}
         onSwipeableOpen={(direction) => {
-          console.log(`Opening swipeable to the ${direction}`);
+          console.log(`Opening swipeable from the ${direction}`);
         }}
         onSwipeableClose={(direction) => {
           console.log(`Closing swipeable to the ${direction}`);
