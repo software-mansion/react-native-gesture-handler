3f76094bf94748de6851c4cdde1db9674009024d
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-02-28 16:54:32 +0100

    Remove TODO from OnLoad.cpp

--- android/src/main/jni/OnLoad.cpp
+++ android/src/main/jni/OnLoad.cpp
@@ -4,9 +4,6 @@
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
   return facebook::jni::initialize(vm, [] {
-    // TODO: dvacca ramanpreet unify this with the way
-    // "ComponentDescriptorFactory" is defined in Fabric
-
     facebook::react::RNGestureHandlerComponentsRegistry::registerNatives();
   });
 }
