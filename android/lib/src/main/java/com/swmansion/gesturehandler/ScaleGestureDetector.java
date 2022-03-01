186ffc8a8ebeb5e5c3c106cad63623a466480edb
Krzysztof Magiera <krzys@swmansion.com>
2022-03-01 13:03:37 +0100

    Update ScaleGestureDetector.java

--- android/lib/src/main/java/com/swmansion/gesturehandler/ScaleGestureDetector.java
+++ android/lib/src/main/java/com/swmansion/gesturehandler/ScaleGestureDetector.java
@@ -1,17 +1,8 @@
 /*
  * Copyright (C) 2010 The Android Open Source Project
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Copied from https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/view/ScaleGestureDetector.java
+ * Modified line 189 to set initial min span to 0 instead of copying it from the system configuration
  */
 
 package com.swmansion.gesturehandler;
