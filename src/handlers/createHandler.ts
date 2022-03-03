7ecb044c4d8fee1e38fb6d3112211edcbe07b8a0
Vir Desai <virdesai@users.noreply.github.com>
2022-03-03 05:27:45 -0500

    Fix overriding `testID` when `createHandler` props do not have one assigned (#1910)
    Fix related to comment [here](https://github.com/software-mansion/react-native-gesture-handler/pull/1844#issuecomment-1057400208) about `RNGestureHandlerButton` with `handlerType="NativeViewGestureHandler"` having missing testIDs when they were previously provided and assigned. The `testID` on the child being cloned within `createHandler` was being removed by the `testID` being set within the render function even the `testID` on the handler was not set. Now it will only assign a `testID` as a prop on the cloned element if a `testID` was provided to the handler.

--- src/handlers/createHandler.ts
+++ src/handlers/createHandler.ts
@@ -494,7 +494,7 @@ export default function createHandler<
                 handlerTag: this.handlerTag,
               }
             : {}),
-          testID: this.props.testID,
+          testID: this.props.testID ?? child.props.testID,
           ...events,
         },
         grandChildren
