68eff5c93775e1b2324f80de477d7d05fa2dfdc0
kacperkapusciak <kacper.kapusciak@swmansion.com>
2022-03-03 11:40:39 +0100

    chore: bump screens to 3.13.1 in FabricExample

--- FabricExample/index.js
+++ FabricExample/index.js
@@ -12,5 +12,4 @@ AppRegistry.registerComponent(appName, () => App);
 LogBox.ignoreLogs([
   "Seems like you're using an old API with gesture components", // react-native-gesture-handler
   'GestureDetector has received a child that may get view-flattened.', // react-native-gesture-handler
-  'Function components cannot be given refs.', // react-native-screens
 ]);
