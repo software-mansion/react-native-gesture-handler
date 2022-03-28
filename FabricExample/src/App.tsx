eb9d3e1aacf4e95580ad128f0beb74025457901b
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2022-03-28 17:13:39 +0200

    Update App.tsx

--- FabricExample/src/App.tsx
+++ FabricExample/src/App.tsx
@@ -1,3 +1,4 @@
+// nothing to see here, just testing out GitHub Actions
 import * as React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
