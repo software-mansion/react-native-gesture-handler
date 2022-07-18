ba2d8bb3e576c785159565e46f8294aa84cbc854
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-07-18 12:16:40 +0200

    Cancel gestures when an uninterruptible native gesture becomes active

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerInteractionManager.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerInteractionManager.kt
@@ -4,6 +4,7 @@ import android.util.SparseArray
 import com.facebook.react.bridge.ReadableMap
 import com.swmansion.gesturehandler.GestureHandler
 import com.swmansion.gesturehandler.GestureHandlerInteractionController
+import com.swmansion.gesturehandler.NativeViewGestureHandler
 
 class RNGestureHandlerInteractionManager : GestureHandlerInteractionController {
   private val waitForRelations = SparseArray<IntArray>()
@@ -42,8 +43,13 @@ class RNGestureHandlerInteractionManager : GestureHandlerInteractionController {
     otherHandler: GestureHandler<*>,
   ) = false
 
-  override fun shouldHandlerBeCancelledBy(handler: GestureHandler<*>, otherHandler: GestureHandler<*>) = false
+  override fun shouldHandlerBeCancelledBy(handler: GestureHandler<*>, otherHandler: GestureHandler<*>): Boolean {
+    if (otherHandler is NativeViewGestureHandler) {
+      return otherHandler.disallowInterruption
+    }
 
+    return false
+  }
   override fun shouldRecognizeSimultaneously(
     handler: GestureHandler<*>,
     otherHandler: GestureHandler<*>,
