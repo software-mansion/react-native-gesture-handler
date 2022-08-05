7ff2319782f46c5bdcdea998b1132a9c01982c0b
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-08-05 12:29:06 +0200

    Bump FabricExample to React Native 0.70.0-rc2

---
+++ FabricExample/react-native.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    dependencies: {
+      "react-native-gesture-handler": {
+        platforms: {
+          android: {
+            libraryName: null,
+            componentDescriptors: null,
+            androidMkPath: null,
+          },
+        },
+      },
+    },
+  };
+  
