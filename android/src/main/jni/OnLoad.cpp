9b28b3275d6a79670075fb27e98d17633c8dca3d
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-08-24 13:23:54 +0200

    Bump FabricExample to React Native 0.70.0-rc.4 (#2153)
    ## Description
    
    This PR updates version of React native used by the FabricExample app to 0.70.0-rc-4 and replaces NDK with CMake.
    
    ## Test plan
    
    Run the FabricExample app
    
    Co-authored-by: Tomek Zawadzki <tomekzawadzki98@gmail.com>

--- android/src/main/jni/OnLoad.cpp
+++
@@ -1,9 +0,0 @@
-#include <fbjni/fbjni.h>
-
-#include "RNGestureHandlerComponentsRegistry.h"
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
-  return facebook::jni::initialize(vm, [] {
-    facebook::react::RNGestureHandlerComponentsRegistry::registerNatives();
-  });
-}
