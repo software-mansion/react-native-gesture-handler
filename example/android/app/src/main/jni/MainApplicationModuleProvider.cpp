ceedcc4903093952162d4aa5570abeecac2ad57e
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-09-12 12:25:04 +0200

    Bump Example app to RN 0.70 (#2209)
    ## Description
    
    This PR bumps React Native in the Example app to 0.70.0
    
    ## Test plan
    
    Build the app

--- example/android/app/src/main/jni/MainApplicationModuleProvider.cpp
+++ example/android/app/src/main/jni/MainApplicationModuleProvider.cpp
@@ -1,9 +1,12 @@
 #include "MainApplicationModuleProvider.h"
+
+#include <rncli.h>
 #include <rncore.h>
+
 namespace facebook {
 namespace react {
 std::shared_ptr<TurboModule> MainApplicationModuleProvider(
-    const std::string moduleName,
+    const std::string &moduleName,
     const JavaTurboModule::InitParams &params) {
   // Here you can provide your own module provider for TurboModules coming from
   // either your application or from external libraries. The approach to follow
@@ -14,6 +17,12 @@ std::shared_ptr<TurboModule> MainApplicationModuleProvider(
   //    return module;
   // }
   // return rncore_ModuleProvider(moduleName, params);
+  // Module providers autolinked by RN CLI
+  auto rncli_module = rncli_ModuleProvider(moduleName, params);
+  if (rncli_module != nullptr) {
+    return rncli_module;
+  }
+  
   return rncore_ModuleProvider(moduleName, params);
 }
 } // namespace react
--- example/android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
+++ example/android/app/src/main/jni/MainApplicationTurboModuleManagerDelegate.h
@@ -15,16 +15,16 @@ class MainApplicationTurboModuleManagerDelegate
   static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject>);
   static void registerNatives();
   std::shared_ptr<TurboModule> getTurboModule(
-      const std::string name,
-      const std::shared_ptr<CallInvoker> jsInvoker) override;
+      const std::string &name,
+      const std::shared_ptr<CallInvoker> &jsInvoker) override;
   std::shared_ptr<TurboModule> getTurboModule(
-      const std::string name,
+      const std::string &name,
       const JavaTurboModule::InitParams &params) override;
   /**
    * Test-only method. Allows user to verify whether a TurboModule can be
    * created by instances of this class.
    */
-  bool canCreateTurboModule(std::string name);
+  bool canCreateTurboModule(const std::string &name);
 };
 } // namespace react
 } // namespace facebook
