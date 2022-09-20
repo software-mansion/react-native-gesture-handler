f4056390b05eea7d82f001fb6e8d4a1b16b4f8e9
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-09-20 12:20:22 +0200

    Remove unused import

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.kt
@@ -1,7 +1,6 @@
 package com.swmansion.gesturehandler.react
 
 import android.content.Context
-import android.graphics.PointF
 import android.util.Log
 import android.view.MotionEvent
 import android.view.ViewGroup
