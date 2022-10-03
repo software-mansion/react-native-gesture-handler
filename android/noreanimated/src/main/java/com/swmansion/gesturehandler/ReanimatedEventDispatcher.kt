bd85bf7e0b0549a1095bfd280f9109879533a892
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-10-03 11:10:29 +0200

    Make `ReanimatedEventDispatcher` a class and not an object (#2203)
    ## Description
    
    This PR changes `ReanimatedEventDispatcher` from `object` to `class` and
    makes it a field in the `RNGestureHandlerModule`. This way the old
    Reanimated module shouldn't be preserved during reload, as the
    dispatcher will be dropped alongside everything else.

--- android/noreanimated/src/main/java/com/swmansion/gesturehandler/ReanimatedEventDispatcher.kt
+++ android/noreanimated/src/main/java/com/swmansion/gesturehandler/ReanimatedEventDispatcher.kt
@@ -3,7 +3,7 @@ package com.swmansion.gesturehandler
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.uimanager.events.Event
 
-object ReanimatedEventDispatcher {
+class ReanimatedEventDispatcher {
     fun <T : Event<T>>sendEvent(event: T, reactApplicationContext: ReactContext) {
         // no-op
     }
