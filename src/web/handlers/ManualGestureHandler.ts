234ec684e8cc3e6e65a2e42f5ed3ae72c4d6e681
Michał Bert <63123542+Warus15@users.noreply.github.com>
2022-10-13 15:18:12 +0200

    Make handlers respect `shouldCancelWhenOutside` (#2271)
    ## Description
    
    This PR solves web part of #2265 issue. I've modified `onPointerOut` method to act similar as on android. Right now, if pointer leaves handler and `shouldCancelWhenOutside` is set to `true`, handler goes to `cancel` state if it was `active`, or to `fail`, if it was in `began`
    
    This change also affects `ManualGestureHandler`, which now cancels properly.
    
    ## Test plan
    
    Tested on example app

--- src/web/handlers/ManualGestureHandler.ts
+++ src/web/handlers/ManualGestureHandler.ts
@@ -1,9 +1,13 @@
-import { AdaptedEvent } from '../interfaces';
+import { AdaptedEvent, Config } from '../interfaces';
 import GestureHandler from './GestureHandler';
 
 export default class ManualGestureHandler extends GestureHandler {
   public init(ref: number, propsRef: React.RefObject<unknown>) {
     super.init(ref, propsRef);
+  }
+
+  public updateGestureConfig({ enabled = true, ...props }: Config): void {
+    super.updateGestureConfig({ enabled: enabled, ...props });
   }
 
   protected onPointerDown(event: AdaptedEvent): void {
@@ -20,6 +24,11 @@ export default class ManualGestureHandler extends GestureHandler {
   protected onPointerMove(event: AdaptedEvent): void {
     this.tracker.track(event);
     super.onPointerMove(event);
+  }
+
+  protected onPointerOutOfBounds(event: AdaptedEvent): void {
+    this.tracker.track(event);
+    super.onPointerOutOfBounds(event);
   }
 
   protected onPointerUp(event: AdaptedEvent): void {
