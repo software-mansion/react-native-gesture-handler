18a926b48c7cbf938d58dd401d19e181fdf89cdf
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-11-08 12:52:58 +0100

    Setup FabricExample

--- FabricExample/index.js
+++ FabricExample/index.js
@@ -4,7 +4,7 @@
 
 import { AppRegistry, LogBox } from 'react-native';
 
-import App from './src/App';
+import App from './AppTest';
 import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
