2039ce325e769e394c5f687e83b6a5e18bc1e70a
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-11-09 10:15:04 +0100

    WIP no.1

--- android/src/main/jni/cpp-adapter.cpp
+++ android/src/main/jni/cpp-adapter.cpp
@@ -1,13 +1,15 @@
 #include <jni.h>
 #include <jsi/jsi.h>
 
+#include <react/renderer/uimanager/UIManager.h>
 #include <react/renderer/uimanager/primitives.h>
 
 using namespace facebook;
 using namespace react;
 
-void decorateRuntime(jsi::Runtime &runtime)
-{
+// ShadowNodeWrapper::~ShadowNodeWrapper() = default;
+
+void decorateRuntime(jsi::Runtime &runtime) {
   auto isFormsStackingContext = jsi::Function::createFromHostFunction(
       runtime,
       jsi::PropNameID::forAscii(runtime, "isFormsStackingContext"),
@@ -15,27 +17,33 @@ void decorateRuntime(jsi::Runtime &runtime)
       [](jsi::Runtime &runtime,
          const jsi::Value &thisValue,
          const jsi::Value *arguments,
-         size_t count) -> jsi::Value
-      {
-        if (!arguments[0].isObject())
-        {
+         size_t count) -> jsi::Value {
+        if (!arguments[0].isObject()) {
           return jsi::Value::null();
         }
 
-        auto shadowNode = arguments[0].asObject(runtime).getHostObject<ShadowNodeWrapper>(runtime)->shadowNode;
-        bool isFormsStackingContext = shadowNode->getTraits().check(ShadowNodeTraits::FormsStackingContext);
+        auto shadowNode = arguments[0]
+                              .asObject(runtime)
+                              .getHostObject<ShadowNodeWrapper>(runtime)
+                              ->shadowNode;
+        (void)shadowNode;
+        // bool isFormsStackingContext = shadowNode->getTraits().check(
+        //     ShadowNodeTraits::FormsStackingContext);
 
-        return jsi::Value(isFormsStackingContext);
+        // return jsi::Value(isFormsStackingContext);
+        return jsi::Value(false);
       });
-  runtime.global().setProperty(runtime, "isFormsStackingContext", std::move(isFormsStackingContext));
+  runtime.global().setProperty(
+      runtime, "isFormsStackingContext", std::move(isFormsStackingContext));
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_swmansion_gesturehandler_react_RNGestureHandlerModule_decorateRuntime(JNIEnv *env, jobject clazz, jlong jsiPtr)
-{
+Java_com_swmansion_gesturehandler_react_RNGestureHandlerModule_decorateRuntime(
+    JNIEnv *env,
+    jobject clazz,
+    jlong jsiPtr) {
   jsi::Runtime *runtime = reinterpret_cast<jsi::Runtime *>(jsiPtr);
-  if (runtime)
-  {
+  if (runtime) {
     decorateRuntime(*runtime);
   }
 }
