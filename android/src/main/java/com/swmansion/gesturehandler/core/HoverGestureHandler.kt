7a5b0c918c141815b005646882f03a9eca65ddc5
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-03-31 16:52:48 +0200

    Update Android implementation Add proper state transitions Update behavior when views are in parent-child relation to better match iOS

--- android/src/main/java/com/swmansion/gesturehandler/core/HoverGestureHandler.kt
+++ android/src/main/java/com/swmansion/gesturehandler/core/HoverGestureHandler.kt
@@ -1,22 +1,16 @@
 package com.swmansion.gesturehandler.core
 
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.children
 import com.swmansion.gesturehandler.react.RNViewConfigurationHelper
 
 class HoverGestureHandler : GestureHandler<HoverGestureHandler>() {
-  init {
-    setShouldCancelWhenOutside(true)
-  }
-
-  fun isAncestor(view: View?, ancestor: View): Boolean {
-    var current: View? = view
+  private infix fun isAncestorOf(other: GestureHandler<*>): Boolean {
+    var current: View? = other.view
 
     while (current != null) {
-      if (current == ancestor) {
+      if (current == this.view) {
         return true
       }
 
@@ -26,7 +20,7 @@ class HoverGestureHandler : GestureHandler<HoverGestureHandler>() {
     return false
   }
 
-  fun isViewDisplayedOverAnother(view: View, other: View, rootView: View = view.rootView): Boolean? {
+  private fun isViewDisplayedOverAnother(view: View, other: View, rootView: View = view.rootView): Boolean? {
     // TODO: handle elevation?
     if (rootView == other) {
       return true
@@ -44,8 +38,10 @@ class HoverGestureHandler : GestureHandler<HoverGestureHandler>() {
 
   override fun shouldBeCancelledBy(handler: GestureHandler<*>): Boolean {
     if (handler is HoverGestureHandler) {
-      isViewDisplayedOverAnother(handler.view!!, this.view!!, )?.let {
-        return it
+      if (!(handler isAncestorOf this)) {
+        isViewDisplayedOverAnother(handler.view!!, this.view!!)?.let {
+          return it
+        }
       }
     }
 
@@ -54,22 +50,42 @@ class HoverGestureHandler : GestureHandler<HoverGestureHandler>() {
 
   override fun shouldRequireToWaitForFailure(handler: GestureHandler<*>): Boolean {
     if (handler is HoverGestureHandler) {
-      isViewDisplayedOverAnother(this.view!!, handler.view!!)?.let {
-        return it
+      if (!(this isAncestorOf handler) && !(handler isAncestorOf this)) {
+        isViewDisplayedOverAnother(this.view!!, handler.view!!)?.let {
+          return it
+        }
       }
     }
 
     return super.shouldRequireToWaitForFailure(handler)
   }
 
-  override fun onHandle(event: MotionEvent, sourceEvent: MotionEvent) {
-    activate()
+  override fun shouldRecognizeSimultaneously(handler: GestureHandler<*>): Boolean {
+    if (handler is HoverGestureHandler && (this isAncestorOf handler || handler isAncestorOf this)) {
+      return true
+    }
+
+    return super.shouldRecognizeSimultaneously(handler)
   }
 
-  override fun resetConfig() {
-    super.resetConfig()
+  override fun onHandle(event: MotionEvent, sourceEvent: MotionEvent) {
+    if (event.action == MotionEvent.ACTION_HOVER_EXIT || !isWithinBounds) {
+      when (this.state) {
+        STATE_UNDETERMINED, STATE_BEGAN -> fail()
+        STATE_ACTIVE -> end()
+      }
+    } else if (event.action == MotionEvent.ACTION_HOVER_MOVE || event.action == MotionEvent.ACTION_HOVER_ENTER) {
+      when (this.state) {
+        STATE_UNDETERMINED -> begin()
+        STATE_BEGAN -> activate()
+      }
+    }
 
-    setShouldCancelWhenOutside(true)
+    // should hover finish on touch (atm. it does probably `ACTION_HOVER_EXIT`, it doesn't on iOS)?
+    // if so, fix double finalize event when touching hovered view and dragging outside
+
+    // iOS doesn't seem to like absolutely positioned views on top of each other even if
+    // gestures are simultaneous, reproduce here?
   }
 
   companion object {
