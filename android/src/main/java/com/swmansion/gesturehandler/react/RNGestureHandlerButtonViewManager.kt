f0ad85efa909273f3e1e276023fcfe2081763c1a
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-04-05 12:12:02 +0200

    Add some sanity checks

--- android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerButtonViewManager.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerButtonViewManager.kt
@@ -229,7 +229,7 @@ class RNGestureHandlerButtonViewManager : ViewGroupManager<ButtonViewGroup>(), R
           colorDrawable.setCornerRadius(borderRadius)
         }
 
-        val layerDrawable = LayerDrawable(arrayOf(colorDrawable, selectable))
+        val layerDrawable = LayerDrawable(if (selectable != null) arrayOf(colorDrawable, selectable) else arrayOf(colorDrawable))
         background = layerDrawable
       }
     }
@@ -244,7 +244,7 @@ class RNGestureHandlerButtonViewManager : ViewGroupManager<ButtonViewGroup>(), R
 
       // Since Android 13, alpha channel in RippleDrawable is clamped between [128, 255]
       // see https://github.com/aosp-mirror/platform_frameworks_base/blob/c1bd0480261460584753508327ca8a0c6fc80758/graphics/java/android/graphics/drawable/RippleDrawable.java#L1012
-      if (rippleColor == Color.TRANSPARENT) {
+      if (rippleColor == Color.TRANSPARENT && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
         return null
       }
 
