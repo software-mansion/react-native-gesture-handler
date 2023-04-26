651ee827d8f8564f75ba21a3e4663b425277ba72
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-04-26 14:30:12 +0200

    Move `GestureHandlerRootView` to `components` directory (#2468)
    ## Description
    
    Moves `GestureHandlerRootView` related TS files under the `components`
    directory. They are, in fact, components.
    
    ## Test plan
    
    Check if the Example and FabricExample apps work correctly

--- src/components/GestureHandlerRootView.tsx
+++ src/components/GestureHandlerRootView.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { PropsWithChildren } from 'react';
 import { View, ViewProps } from 'react-native';
-import { maybeInitializeFabric } from './init';
-import GestureHandlerRootViewContext from './GestureHandlerRootViewContext';
+import { maybeInitializeFabric } from '../init';
+import GestureHandlerRootViewContext from '../GestureHandlerRootViewContext';
 
 export interface GestureHandlerRootViewProps
   extends PropsWithChildren<ViewProps> {}
