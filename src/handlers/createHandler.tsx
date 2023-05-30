379db54f3fd84f55c755b610852c11e700b74e76
IvanIhnatsiuk <86000012+IvanIhnatsiuk@users.noreply.github.com>
2023-05-30 08:42:20 -0700

    Fix root view error for jest (#2491)
    ## Description
    This PR fixed a problem related to `HandlerName must be used as a
    descendant of GestureHandlerRootView. Otherwise the gestures will not be
    recognized. See
    https://docs.swmansion.com/react-native-gesture-handler/docs/installation
    for more details.` in jest.
    <!--
    Description and motivation for this PR.
    In some project 
    
    Include 'Fixes #<number>' if this is fixing some issue.
    -->
    
    ## Related issues
    
    
    [2488](https://github.com/software-mansion/react-native-gesture-handler/issues/2488)
    
    ## Test plan
    
    tested locally on my project :)

--- src/handlers/createHandler.tsx
+++ src/handlers/createHandler.tsx
@@ -402,7 +402,7 @@ export default function createHandler<
     }
 
     render() {
-      if (__DEV__ && !this.context) {
+      if (__DEV__ && !this.context && !isJestEnv()) {
         throw new Error(
           name +
             ' must be used as a descendant of GestureHandlerRootView. Otherwise the gestures will not be recognized. See https://docs.swmansion.com/react-native-gesture-handler/docs/installation for more details.'
