d508cc612a6a0040a7588bf1aa36dae080aa3b34
Nick Vlug <nick@withblocks.com>
2023-09-14 13:57:47 +0200

    add support for yarn berry

--- example/index.web.js
+++ example/index.web.js
@@ -1,4 +1,4 @@
-import { AppRegistry, Platform } from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
 
 AppRegistry.registerComponent('Example', () => App);
