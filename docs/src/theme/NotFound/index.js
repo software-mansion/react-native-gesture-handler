12383c99db35fe95974c3b6ad4e52a43883bfb33
kacperkapusciak <kacper.kapusciak@swmansion.com>
2023-09-27 10:45:38 +0200

    Redirect users from 404 whenever possible

---
+++ docs/src/theme/NotFound/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import NotFound from '@theme-original/NotFound';
+import { Redirect, useLocation } from '@docusaurus/router';
+import { mapLegacyUrl } from './mapLegacyUrl';
+
+export default function NotFoundWrapper(props) {
+  const location = useLocation();
+
+  const redirect = mapLegacyUrl(location);
+
+  console.log(redirect);
+
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
+
+  return (
+    <>
+      <NotFound {...props} />
+    </>
+  );
+}
