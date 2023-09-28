7936edf125e2042403e8457689970f8a7a683067
Michał Bert <michal.bert@swmansion.com>
2023-09-28 13:15:25 +0200

    Simplify assignment

--- src/queueMicrotask.ts
+++ src/queueMicrotask.ts
@@ -1,7 +1,2 @@
-export const ghQueueMicrotask = (callbackFn: () => void) => {
-  if (typeof queueMicrotask === 'function') {
-    queueMicrotask(callbackFn);
-  } else {
-    setImmediate(callbackFn);
-  }
-};
+export const ghQueueMicrotask =
+  typeof queueMicrotask === 'function' ? queueMicrotask : setImmediate;
