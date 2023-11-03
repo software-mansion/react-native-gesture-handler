ea23d45a61dd85ed8d790c3df10cc1338a8008a6
Jakub Fedak <jakubfedak1030@gmail.com>
2023-11-03 15:54:51 +0100

    Add stack to macos example

--- MacOSExample/src/App.tsx
+++ MacOSExample/src/App.tsx
@@ -1,18 +1,135 @@
 import React from 'react';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {StyleSheet} from 'react-native';
-import Draggable from './basic/draggable';
+import { Text, View, StyleSheet, SectionList, Platform, Pressable } from 'react-native';
+import {
+  createStackNavigator,
+  StackScreenProps,
+} from '@react-navigation/stack';
+import { NavigationContainer, ParamListBase } from '@react-navigation/native';
+import {
+  GestureHandlerRootView,
+  RectButton,
+} from 'react-native-gesture-handler';
+
+import Draggable from './examples/DragableExample';
+
+interface Example {
+  name: string;
+  component: React.ComponentType;
+}
+interface ExamplesSection {
+  sectionTitle: string;
+  data: Example[];
+}
+
+const EXAMPLES: ExamplesSection[] = [
+  {
+    sectionTitle: 'Basic examples',
+    data: [
+      { name: 'Draggable', component: Draggable },
+    ],
+  },
+];
+
+type RootStackParamList = {
+  Home: undefined;
+} & {
+  [Screen: string]: undefined;
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <GestureHandlerRootView style={styles.root}>
-      <Draggable />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            options={{ title: '✌️ Gesture Handler Demo' }}
+            component={MainScreen}
+          />
+          {EXAMPLES.flatMap(({ data }) => data).flatMap(
+            ({ name, component }) => (
+              <Stack.Screen
+                key={name}
+                name={name}
+                getComponent={() => component}
+                options={{ title: name }}
+              />
+            )
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </GestureHandlerRootView>
+  );
+}
+
+function MainScreen({ navigation }: StackScreenProps<ParamListBase>) {
+  return (
+    <SectionList
+      style={styles.list}
+      sections={EXAMPLES}
+      keyExtractor={(example) => example.name}
+      renderItem={({ item }) => (
+        <MainScreenItem
+          name={item.name}
+          onPressItem={(name) => navigation.navigate(name)}
+        />
+      )}
+      renderSectionHeader={({ section: { sectionTitle } }) => (
+        <Text style={styles.sectionTitle}>{sectionTitle}</Text>
+      )}
+      ItemSeparatorComponent={() => <View style={styles.separator} />}
+    />
+  );
+}
+
+interface MainScreenItemProps {
+  name: string;
+  onPressItem: (name: string) => void;
+}
+
+function MainScreenItem({ name, onPressItem }: MainScreenItemProps) {
+  return (
+    // <RectButton style={[styles.button]} onPress={() => onPressItem(name)}>
+    //   <Text>{name}</Text>
+    // </RectButton>
+    <Pressable style={[styles.button]} onPress={() => onPressItem(name)}>
+      <Text>{name}</Text>
+    </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+  },
+  sectionTitle: {
+    ...Platform.select({
+      ios: {
+        fontSize: 17,
+        fontWeight: '500',
+      },
+      android: {
+        fontSize: 19,
+        fontFamily: 'sans-serif-medium',
+      },
+    }),
+    paddingTop: 10,
+    paddingBottom: 5,
+    paddingLeft: 10,
+    backgroundColor: '#efefef',
+  },
+  list: {},
+  separator: {
+    height: 2,
+  },
+  button: {
+    flex: 1,
+    height: 50,
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
   },
 });
---
+++ MacOSExample/src/examples/DragableExample.tsx
@@ -0,0 +1,62 @@
+import React, {FC} from 'react';
+import {ScrollView, StyleProp, StyleSheet, ViewStyle} from 'react-native';
+
+import Animated, {
+  useSharedValue,
+} from 'react-native-reanimated';
+
+import {Gesture, GestureDetector} from 'react-native-gesture-handler';
+
+type DraggableBoxProps = {
+  minDist?: number;
+  boxStyle?: StyleProp<ViewStyle>;
+};
+
+export const DraggableBox: FC<DraggableBoxProps> = props => {
+  const translateY = useSharedValue(0);
+  const translateX = useSharedValue(0);
+
+  let x = useSharedValue(0);
+  let y = useSharedValue(0);
+
+  const panGesture = Gesture.Pan()
+    .minDistance(props.minDist || 0)
+    .onUpdate(e => {
+      translateX.value = x.value + e.translationX;
+      translateY.value = y.value + e.translationY;
+    }).onEnd( e => {
+      x.value = translateX.value;
+      y.value = translateY.value;
+    });
+
+  return (
+    <GestureDetector gesture={panGesture}>
+      <Animated.View style={[styles.box, props.boxStyle,
+      {
+        top: translateY,
+        left: translateX,
+      }]} />
+    </GestureDetector>
+  );
+};
+
+export default function Example() {
+  return (
+    <ScrollView style={styles.scrollView}>
+      <DraggableBox />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+  box: {
+    width: 150,
+    height: 150,
+    position: 'absolute',
+    backgroundColor: 'plum',
+    margin: 10,
+  },
+});
