8e75977e3d0978d7ff0c66d3618e671b662b7702
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-11-22 15:14:20 +0100

    Init events lazily not eagerly

---
+++ android/src/main/java/com/swmansion/gesturehandler/react/eventbuilders/RotationGestureHandlerEventDataBuilder.kt
@@ -0,0 +1,30 @@
+package com.swmansion.gesturehandler.react.eventbuilders
+
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.uimanager.PixelUtil
+import com.swmansion.gesturehandler.core.RotationGestureHandler
+
+class RotationGestureHandlerEventDataBuilder(handler: RotationGestureHandler) : GestureHandlerEventDataBuilder<RotationGestureHandler>(handler) {
+    private val rotation: Double
+    private val anchorX: Float
+    private val anchorY: Float
+    private val velocity: Double
+
+    init {
+        rotation = handler.rotation
+        anchorX = handler.anchorX
+        anchorY = handler.anchorY
+        velocity = handler.velocity
+    }
+
+    override fun buildEventData(eventData: WritableMap) {
+        super.buildEventData(eventData)
+
+        with(eventData) {
+            putDouble("scale", rotation)
+            putDouble("focalX", PixelUtil.toDIPFromPixel(anchorX).toDouble())
+            putDouble("focalY", PixelUtil.toDIPFromPixel(anchorY).toDouble())
+            putDouble("velocity", velocity)
+        }
+    }
+}
