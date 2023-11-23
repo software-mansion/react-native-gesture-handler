2f178642832a5c47135162ab51db8300b2427372
Jakub Piasecki <jakub.piasecki@swmansion.com>
2023-11-23 12:28:34 +0100

    Format android

--- android/src/main/java/com/swmansion/gesturehandler/react/eventbuilders/LongPressGestureHandlerEventDataBuilder.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/eventbuilders/LongPressGestureHandlerEventDataBuilder.kt
@@ -5,29 +5,29 @@ import com.facebook.react.uimanager.PixelUtil
 import com.swmansion.gesturehandler.core.LongPressGestureHandler
 
 class LongPressGestureHandlerEventDataBuilder(handler: LongPressGestureHandler) : GestureHandlerEventDataBuilder<LongPressGestureHandler>(handler) {
-    private val x: Float
-    private val y: Float
-    private val absoluteX: Float
-    private val absoluteY: Float
-    private val duration: Int
+  private val x: Float
+  private val y: Float
+  private val absoluteX: Float
+  private val absoluteY: Float
+  private val duration: Int
 
-    init {
-        x = handler.lastRelativePositionX
-        y = handler.lastRelativePositionY
-        absoluteX = handler.lastPositionInWindowX
-        absoluteY = handler.lastPositionInWindowY
-        duration = handler.duration
+  init {
+    x = handler.lastRelativePositionX
+    y = handler.lastRelativePositionY
+    absoluteX = handler.lastPositionInWindowX
+    absoluteY = handler.lastPositionInWindowY
+    duration = handler.duration
+  }
+
+  override fun buildEventData(eventData: WritableMap) {
+    super.buildEventData(eventData)
+
+    with(eventData) {
+      putDouble("x", PixelUtil.toDIPFromPixel(x).toDouble())
+      putDouble("y", PixelUtil.toDIPFromPixel(y).toDouble())
+      putDouble("absoluteX", PixelUtil.toDIPFromPixel(absoluteX).toDouble())
+      putDouble("absoluteY", PixelUtil.toDIPFromPixel(absoluteY).toDouble())
+      putInt("duration", duration)
     }
-
-    override fun buildEventData(eventData: WritableMap) {
-        super.buildEventData(eventData)
-
-        with(eventData) {
-            putDouble("x", PixelUtil.toDIPFromPixel(x).toDouble())
-            putDouble("y", PixelUtil.toDIPFromPixel(y).toDouble())
-            putDouble("absoluteX", PixelUtil.toDIPFromPixel(absoluteX).toDouble())
-            putDouble("absoluteY", PixelUtil.toDIPFromPixel(absoluteY).toDouble())
-            putInt("duration", duration)
-        }
-    }
+  }
 }
--- android/src/main/java/com/swmansion/gesturehandler/react/eventbuilders/NativeGestureHandlerEventDataBuilder.kt
+++ android/src/main/java/com/swmansion/gesturehandler/react/eventbuilders/NativeGestureHandlerEventDataBuilder.kt
@@ -4,15 +4,15 @@ import com.facebook.react.bridge.WritableMap
 import com.swmansion.gesturehandler.core.NativeViewGestureHandler
 
 class NativeGestureHandlerEventDataBuilder(handler: NativeViewGestureHandler) : GestureHandlerEventDataBuilder<NativeViewGestureHandler>(handler) {
-    private val pointerInside: Boolean
+  private val pointerInside: Boolean
 
-    init {
-        pointerInside = handler.isWithinBounds
-    }
+  init {
+    pointerInside = handler.isWithinBounds
+  }
 
-    override fun buildEventData(eventData: WritableMap) {
-        super.buildEventData(eventData)
+  override fun buildEventData(eventData: WritableMap) {
+    super.buildEventData(eventData)
 
-        eventData.putBoolean("pointerInside", pointerInside)
-    }
+    eventData.putBoolean("pointerInside", pointerInside)
+  }
 }
