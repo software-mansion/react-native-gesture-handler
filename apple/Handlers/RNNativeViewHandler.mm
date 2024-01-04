5bdb89d5397624b876e0293fd31ce4fd74793a02
Jakub Fedak <jakubfedak1030@gmail.com>
2024-01-04 10:20:50 +0100

    Fix message

--- apple/Handlers/RNNativeViewHandler.mm
+++ apple/Handlers/RNNativeViewHandler.mm
@@ -216,7 +216,7 @@
 
 - (instancetype)initWithTag:(NSNumber *)tag
 {
-  RCTLogWarn(@"Manual gesture handler is not supported on macOS");
+  RCTLogWarn(@"Native view gesture handler is not supported on macOS");
   if ((self = [super initWithTag:tag])) {
     _recognizer = [NSGestureRecognizer alloc];
   }
