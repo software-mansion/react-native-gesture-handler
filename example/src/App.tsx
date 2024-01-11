74c01a3fa205f0a1c25693c9be404097d835e5c0
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2024-01-11 12:54:48 +0100

    fix: Add support for borderRadii on RectButton (#2691)
    # Description
    
    
    https://github.com/software-mansion/react-native-gesture-handler/assets/39658211/4ebc5761-c571-4c9a-a9c0-c2e0b7ac0018
    
    
    This PR adds support for border radius style props like:
    `borderTopLeftRadius`, `borderTopRightRadius`, `borderBottomLeftRadius`,
    `borderBottomRightRadius` to RectButton.
    
    It implements the border radii by wrapping a RawButton with two Views -
    the outer one curves the native RawButton and the inner acts like a mask
    cutting the corners of the button. The radius of the inner view had to
    be adjusted by subtracting the borderWidth from it - [see this
    tweet](https://twitter.com/lilykonings/status/1567317037126680576).
    
    FIxes
    https://github.com/software-mansion/react-native-gesture-handler/issues/2594
    
    ---------
    
    Co-authored-by: Tomasz Żelawski <tomasz.zelawski@swmansion.com>
    Co-authored-by: Jakub Piasecki <jakub.piasecki@swmansion.com>

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -19,6 +19,7 @@ import Fling from './release_tests/fling';
 import NestedTouchables from './release_tests/nestedTouchables';
 import NestedButtons from './release_tests/nestedButtons';
 import NestedGestureHandlerRootViewWithModal from './release_tests/nestedGHRootViewWithModal';
+import RoundedButtons from './release_tests/roundedButtons';
 import { PinchableBox } from './recipes/scaleAndRotate';
 import PanAndScroll from './recipes/panAndScroll';
 import { BottomSheet } from './showcase/bottomSheet';
@@ -115,6 +116,7 @@ const EXAMPLES: ExamplesSection[] = [
       { name: 'Fling', component: Fling },
       { name: 'Combo', component: ComboWithGHScroll },
       { name: 'Touchables', component: TouchablesIndex as React.ComponentType },
+      { name: 'Rounded buttons', component: RoundedButtons },
     ],
   },
   {
