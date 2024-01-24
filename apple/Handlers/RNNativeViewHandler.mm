02c193816f424988f3824751da649f5e83894eb1
Jakub Fedak <jakubfedak1030@gmail.com>
2024-01-24 05:56:05 +0100

    Temporary native view handler

--- apple/Handlers/RNNativeViewHandler.mm
+++ apple/Handlers/RNNativeViewHandler.mm
@@ -222,15 +222,117 @@
 
 #pragma mark RNNativeViewGestureHandler
 
-@implementation RNNativeViewGestureHandler
+@implementation RNNativeViewGestureHandler {
+  BOOL _shouldActivateOnStart;
+  BOOL _disallowInterruption;
+}
 
 - (instancetype)initWithTag:(NSNumber *)tag
 {
-  RCTLogWarn(@"NativeViewGestureHandler is not supported on macOS");
   if ((self = [super initWithTag:tag])) {
-    _recognizer = [NSGestureRecognizer alloc];
+    _recognizer = [[RNDummyGestureRecognizer alloc] initWithGestureHandler:self];
   }
   return self;
+}
+
+- (void)configure:(NSDictionary *)config
+{
+  [super configure:config];
+  _shouldActivateOnStart = [RCTConvert BOOL:config[@"shouldActivateOnStart"]];
+  _disallowInterruption = [RCTConvert BOOL:config[@"disallowInterruption"]];
+}
+
+- (void)bindToView:(RNGHUIView *)view
+{
+  // For UIControl based views (UIButton, UISwitch) we provide special handling that would allow
+  // for properties like `disallowInterruption` to work.
+  if ([view isKindOfClass:[NSControl class]]) {
+    NSControl *control = (NSControl *)view;
+  } else {
+    [super bindToView:view];
+  }
+
+  // We can restore default scrollview behaviour to delay touches to scrollview's children
+  // because gesture handler system can handle cancellation of scroll recognizer when JS responder
+  // is set
+#ifdef RCT_NEW_ARCH_ENABLED
+  if ([view isKindOfClass:[RCTScrollViewComponentView class]]) {
+    UIScrollView *scrollView = ((RCTScrollViewComponentView *)view).scrollView;
+    scrollView.delaysContentTouches = YES;
+  }
+#else
+  if ([view isKindOfClass:[RCTScrollView class]]) {
+    // This part of the code is coupled with RN implementation of ScrollView native wrapper and
+    // we expect for RCTScrollView component to contain a subclass of UIScrollview as the only
+    // subview
+    //    UIScrollView *scrollView = [view.subviews objectAtIndex:0];
+    //    scrollView.delaysContentTouches = YES;
+  }
+#endif // RCT_NEW_ARCH_ENABLED
+}
+
+- (void)handleTouchDown:(RNGHUIView *)sender forEvent:(UIEvent *)event
+{
+  [self reset];
+
+  if (_disallowInterruption) {
+    // When `disallowInterruption` is set we cancel all gesture handlers when this UIControl
+    // gets DOWN event
+    for (RNGHUITouch *touch in [event allTouches]) {
+      //      for (UIGestureRecognizer *recogn in [touch gestureRecognizers]) {
+      //        recogn.enabled = NO;
+      //        recogn.enabled = YES;
+      //      }
+    }
+  }
+
+  [self sendEventsInState:RNGestureHandlerStateActive
+           forViewWithTag:sender.reactTag
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES]];
+}
+
+- (void)handleTouchUpOutside:(RNGHUIView *)sender forEvent:(UIEvent *)event
+{
+  [self sendEventsInState:RNGestureHandlerStateEnd
+           forViewWithTag:sender.reactTag
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO]];
+}
+
+- (void)handleTouchUpInside:(RNGHUIView *)sender forEvent:(UIEvent *)event
+{
+  [self sendEventsInState:RNGestureHandlerStateEnd
+           forViewWithTag:sender.reactTag
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES]];
+}
+
+- (void)handleDragExit:(RNGHUIView *)sender forEvent:(UIEvent *)event
+{
+  // Pointer is moved outside of the view bounds, we cancel button when `shouldCancelWhenOutside` is set
+  if (self.shouldCancelWhenOutside) {
+    NSControl *control = (NSControl *)sender;
+    //    [control cancelTrackingWithEvent:event];
+    [self sendEventsInState:RNGestureHandlerStateEnd
+             forViewWithTag:sender.reactTag
+              withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO]];
+  } else {
+    [self sendEventsInState:RNGestureHandlerStateActive
+             forViewWithTag:sender.reactTag
+              withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO]];
+  }
+}
+
+- (void)handleDragEnter:(NSControl *)sender forEvent:(UIEvent *)event
+{
+  [self sendEventsInState:RNGestureHandlerStateActive
+           forViewWithTag:sender.reactTag
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES]];
+}
+
+- (void)handleTouchCancel:(NSControl *)sender forEvent:(UIEvent *)event
+{
+  [self sendEventsInState:RNGestureHandlerStateCancelled
+           forViewWithTag:sender.reactTag
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO]];
 }
 
 @end
