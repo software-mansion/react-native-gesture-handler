c0b6f712f69c5630c636041c0ee547f90c762717
Tomek Zawadzki <tomekzawadzki98@gmail.com>
2024-02-05 12:28:13 +0100

    feat: add visionOS support (#2743)
    ## Description
    
    This PR adds support for visionOS platform. Based on
    https://github.com/software-mansion/react-native-reanimated/pull/5627.
    
    ## Test plan
    
    ```
    npx @callstack/react-native-visionos@latest init VisionApp
    yarn add software-mansion-labs/react-native-gesture-handler#@tomekzaw/visionos
    cd visionos
    pod install
    ```

--- apple/Handlers/RNFlingHandler.m
+++ apple/Handlers/RNFlingHandler.m
@@ -143,7 +143,7 @@
 
   CGPoint viewAbsolutePosition =
       [recognizer.view convertPoint:recognizer.view.bounds.origin
-                             toView:[UIApplication sharedApplication].keyWindow.rootViewController.view];
+                             toView:RCTKeyWindow().rootViewController.view];
   CGPoint locationInView = [recognizer getLastLocation];
 
   return [RNGestureHandlerEventExtraData
