495aae8d7daec42ebcbd7def6804d093dd451a0d
Michał Bert <michal.bert@swmansion.com>
2024-03-01 15:12:38 +0100

    Remove unused eslint rule

--- src/web/handlers/GestureHandlerInterface.ts
+++ src/web/handlers/GestureHandlerInterface.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-empty-function */
 import { PointerType } from '../../PointerType';
 import { MouseButton } from '../../handlers/gestureHandlerCommon';
 import { State } from '../../State';
