22b61b5651d1b798430cf0306769e139a70f363f
Jakub Piasecki <jakubpiasecki67@gmail.com>
2024-03-05 10:17:16 +0100

    Import `RectButton` from RNGH package

--- example/src/release_tests/rectButton/index.tsx
+++ example/src/release_tests/rectButton/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { RectButton } from '../../../../src/components/GestureButtons';
+import { RectButton } from 'react-native-gesture-handler';
 
 export default function RectButtonBorders() {
   return (
