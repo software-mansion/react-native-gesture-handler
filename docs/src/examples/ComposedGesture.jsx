ce70c6de915639aa03d599c76a100d28a8940cf4
LatekVo <latkaignacy@gmail.com>
2024-03-20 17:01:34 +0100

    use composed tap + pan component for flow chart

--- docs/src/examples/ComposedGesture.jsx
+++ docs/src/examples/ComposedGesture.jsx
@@ -1,11 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Easing, StyleSheet, View } from 'react-native';
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
   withSpring,
   withTiming,
+  withSequence,
 } from 'react-native-reanimated';
 import {
   Gesture,
@@ -15,38 +16,55 @@ import {
 
 export default function App() {
   const pressed = useSharedValue(false);
-  // highlight-next-line
-  const offset = useSharedValue(0);
+  const tapped = useSharedValue(false);
 
+  const offset = useSharedValue(0);
+  const scale = useSharedValue(1);
+
+  // highlight-start
   const pan = Gesture.Pan()
     .onBegin(() => {
       pressed.value = true;
     })
-    // highlight-start
+    .onStart(() => {
+      scale.value = withSpring(0.6, { duration: 200 });
+    })
     .onChange((event) => {
       offset.value = event.translationX;
     })
-    // highlight-end
     .onFinalize(() => {
-      // highlight-next-line
       offset.value = withSpring(0);
+      scale.value = withTiming(1);
       pressed.value = false;
     });
 
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      tapped.value = true;
+    })
+    .onStart(() => {
+      scale.value = withSequence(
+        withSpring(1.8, { duration: 80, easing: Easing.ease }),
+        withSpring(1, { duration: 160, easing: Easing.ease })
+      );
+    })
+    .onFinalize(() => {
+      tapped.value = false;
+    });
+  // highlight-end
+
   const animatedStyles = useAnimatedStyle(() => ({
-    transform: [
-      // highlight-next-line
-      { translateX: offset.value },
-      { scale: withTiming(pressed.value ? 1.2 : 1) },
-    ],
-    backgroundColor: pressed.value ? '#FFE04B' : '#b58df1',
+    transform: [{ translateX: offset.value }, { scale: scale.value }],
+    backgroundColor: pressed.value ? '#ffe04b' : '#b58df1',
   }));
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.container}>
-        <GestureDetector gesture={pan}>
-          <Animated.View style={[styles.circle, animatedStyles]} />
+        <GestureDetector gesture={tap}>
+          <GestureDetector gesture={pan}>
+            <Animated.View style={[styles.circle, animatedStyles]} />
+          </GestureDetector>
         </GestureDetector>
       </View>
     </GestureHandlerRootView>
