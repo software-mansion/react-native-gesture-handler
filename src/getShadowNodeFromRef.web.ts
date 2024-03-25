d0e380e643978e902f317cc3d7765d4f36d9bc13
Michał Bert <michal.bert@swmansion.com>
2024-03-25 12:12:29 +0100

    Change web versions of files to default

--- src/getShadowNodeFromRef.web.ts
+++
@@ -1,7 +0,0 @@
-// Used by GestureDetector (unsupported on web at the moment) to check whether the
-// attached view may get flattened on Fabric. Original implementation causes errors
-// on web due to the static resolution of `require` statements by webpack breaking
-// the conditional importing.
-export function getShadowNodeFromRef(_ref: any) {
-  return null;
-}
--- src/handlers/PressabilityDebugView.tsx
+++ src/handlers/PressabilityDebugView.tsx
@@ -1,2 +1,4 @@
-// @ts-ignore it's not exported so we need to import it from path
-export { PressabilityDebugView } from 'react-native/Libraries/Pressability/PressabilityDebug';
+// PressabilityDebugView is not implemented in react-native-web
+export function PressabilityDebugView() {
+  return null;
+}
