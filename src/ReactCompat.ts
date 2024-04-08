a99ec94d7640da398f993bbe2e5a65a38d5844fc
Michał Bert <michal.bert@swmansion.com>
2024-04-08 10:40:24 +0200

    Change any to unknown

--- src/ReactCompat.ts
+++ src/ReactCompat.ts
@@ -5,7 +5,7 @@ export const Platform = { OS: 'web' };
 export const NativeModules = {};
 export const isDEV = process.env.NODE_ENV === 'development';
 export const DeviceEventEmitter = {
-  addListener: (_a: string, _b: any) => null,
+  addListener: (_a: string, _b: unknown) => null,
 };
-export type EmitterSubscription = any;
+export type EmitterSubscription = unknown;
 export const UIManager = {};
