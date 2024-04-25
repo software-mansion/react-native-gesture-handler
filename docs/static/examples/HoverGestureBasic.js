dd5d467dff5791970f0cd7785b0a6c78e52cd5bc
Marek Kotarba <markuskotarba@gmail.com>
2024-04-25 15:34:57 +0200

    adding hover gesture color interpolation

--- docs/static/examples/HoverGestureBasic.js
+++ docs/static/examples/HoverGestureBasic.js
@@ -7,14 +7,19 @@ import {
 import { StyleSheet } from 'react-native';
 import Animated, {
   Easing,
+  interpolateColor,
   useAnimatedStyle,
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
 
+const EASING = Easing.bezier(1, -1, 0.3, 1.43);
+
 export default function App() {
   const translateX = useSharedValue(0);
   const translateY = useSharedValue(0);
+
+  const progress = useSharedValue(0);
 
   const startX = useSharedValue(0);
   const startY = useSharedValue(0);
@@ -27,15 +32,23 @@ export default function App() {
     .onUpdate((event) => {
       translateX.value = (event.x - startX.value) * 0.3;
       translateY.value = (event.y - startY.value) * 0.3;
+
+      const distance = Math.sqrt(Math.pow(translateX.value, 2) + Math.pow(translateY.value, 2));
+
+      progress.value = distance / 35;
     })
     .onEnd(() => {
       translateX.value = withTiming(0, {
         duration: 400,
-        easing: Easing.bezier(1, -1, 0.3, 1.43),
+        easing: EASING,
       });
       translateY.value = withTiming(0, {
         duration: 400,
-        easing: Easing.bezier(1, -1, 0.3, 1.43),
+        easing: EASING,
+      });
+      progress.value = withTiming(0, {
+        duration: 400,
+        easing: EASING,
       });
     });
 
@@ -44,6 +57,11 @@ export default function App() {
       { translateX: translateX.value },
       { translateY: translateY.value },
     ],
+    backgroundColor: interpolateColor(
+      progress.value,
+      [0, 1],
+      ['#b58df1', '#fa7f7c']
+    )
   }));
 
   return (
@@ -65,7 +83,6 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 20,
-    backgroundColor: '#b58df1',
     cursor: 'pointer',
   },
 });
--- docs/static/examples/TapGestureBasic.js
+++ docs/static/examples/TapGestureBasic.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import {
   Gesture,
   GestureDetector,
