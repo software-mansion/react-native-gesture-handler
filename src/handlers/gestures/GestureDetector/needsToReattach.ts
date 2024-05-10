2d26817a9d298f86f6538991e79f0b714c30c914
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-05-10 08:52:18 +0200

    Add comment

--- src/handlers/gestures/GestureDetector/needsToReattach.ts
+++ src/handlers/gestures/GestureDetector/needsToReattach.ts
@@ -1,6 +1,10 @@
 import { GestureType } from '../gesture';
 import { AttachedGestureState } from './types';
 
+// Checks whether the gesture should be reattached to the view, this will happen when:
+// - The number of gestures in the preparedGesture is different than the number of gestures in the gesture
+// - The handlerName is different in any of the gestures
+// - At least one of the gestures changed the thread it runs on
 export function needsToReattach(
   preparedGesture: AttachedGestureState,
   gesture: GestureType[]
