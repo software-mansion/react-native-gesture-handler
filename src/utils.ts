0dad3d2b555c41cbb3c2079a34e4d9b3f8ea5195
Michał Bert <michal.bert@swmansion.com>
2024-05-16 14:04:38 +0200

    Add lodash replacement

--- src/utils.ts
+++ src/utils.ts
@@ -59,3 +59,29 @@ export function isRemoteDebuggingEnabled(): boolean {
     !localGlobal.RN$Bridgeless
   );
 }
+
+export function deepEqual(obj1: any, obj2: any) {
+  if (obj1 === obj2) return true;
+
+  if (
+    typeof obj1 !== 'object' ||
+    typeof obj2 !== 'object' ||
+    obj1 === null ||
+    obj2 === null
+  ) {
+    return obj1 === obj2;
+  }
+
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+
+  if (keys1.length !== keys2.length) return false;
+
+  for (let key of keys1) {
+    if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+      return false;
+    }
+  }
+
+  return true;
+}
