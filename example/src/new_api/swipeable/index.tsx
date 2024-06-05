bf608c0501e8326d5350096f02306300961914a7
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-05 16:58:26 +0200

    cleanup example

--- example/src/new_api/swipeable/index.tsx
+++ example/src/new_api/swipeable/index.tsx
@@ -1,38 +1,30 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Animated, {
-  useAnimatedStyle,
-  useSharedValue,
-} from 'react-native-reanimated';
-import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 import Swipeable from './Swipeable';
 
 function ExampleSwipeable() {
-
-    return <Swipeable/>
+  return <Swipeable />;
 }
 
 export default function Example() {
-    return (
-      <View style={styles.home}>
-        <ExampleSwipeable />
-      </View>
-    );
-  }
-
+  return (
+    <View style={styles.home}>
+      <ExampleSwipeable />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    home: {
-      width: '100%',
-      height: '100%',
-      alignSelf: 'center',
-      backgroundColor: 'plum',
-    },
-    button: {
-      width: 200,
-      height: 200,
-      backgroundColor: 'green',
-      alignSelf: 'center',
-    },
-  });
-  
+  home: {
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+    backgroundColor: 'plum',
+  },
+  button: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'green',
+    alignSelf: 'center',
+  },
+});
