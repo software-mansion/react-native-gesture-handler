68e98ae2607dda0efb0f211632d5943dcf296c11
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-12 16:55:22 +0200

    inti testing example

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,19 +1,119 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, useWindowDimensions } from 'react-native';
+import { SceneMap, TabView } from 'react-native-tab-view';
+import Slide from './Slide';
 
-export default function EmptyExample() {
+const ThirdRoute = () => (
+  <View style={styles.thirdRouteRoot}>
+    <Slide
+      dataSource={Array.from({ length: 10 })}
+      renderItem={(index) => (
+        <View style={{ borderWidth: 2, height: 200 }}>
+          <Text>{index}</Text>
+        </View>
+      )}
+    />
+  </View>
+);
+
+const FourRoute = () => <View style={styles.fourthRouteRoot} />;
+
+const renderScene2 = SceneMap({
+  third: ThirdRoute,
+  four: FourRoute,
+});
+
+const SecondRoute = () => {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'third', title: 'Third' },
+    { key: 'four', title: 'Four' },
+  ]);
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View style={styles.secondRouteRoot}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene2}
+        onIndexChange={setIndex}
+        swipeEnabled={false}
+        animationEnabled={false}
+        initialLayout={{ width: layout.width }}
+      />
+    </View>
+  );
+};
+
+const FirstRoute = () => (
+  <View style={styles.firstRouteRoot}>
+    <Slide
+      dataSource={Array.from({ length: 10 })}
+      renderItem={(index) => {
+        return (
+          <View style={styles.cardIndexWrapper}>
+            <Text>{index.toFixed()}</Text>
+          </View>
+        );
+      }}
+    />
+  </View>
+);
+
+const renderScene = SceneMap({
+  first: FirstRoute,
+  second: SecondRoute,
+});
+
+export default function App() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'first', title: 'First' },
+    { key: 'second', title: 'Second' },
+  ]);
+
+  return (
+    <View style={styles.root}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        swipeEnabled={false}
+        animationEnabled={false}
+        initialLayout={{ width: layout.width }}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    justifyContent: 'center',
+    paddingTop: 100,
+  },
+  firstRouteRoot: {
+    flex: 1,
+    backgroundColor: '#ff4081',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
+  },
+  secondRouteRoot: {
+    flex: 1,
+    backgroundColor: '#673ab7',
+  },
+  thirdRouteRoot: {
+    flex: 1,
+    backgroundColor: '#ff4081',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  fourthRouteRoot: {
+    flex: 1,
+    backgroundColor: '#673ab7',
+  },
+  cardIndexWrapper: {
+    borderWidth: 2,
+    height: 200,
   },
 });
---
+++ example/src/empty/Slide.tsx
@@ -0,0 +1,147 @@
+import React, { memo, useCallback, useEffect, useMemo } from 'react';
+import { Dimensions, StyleSheet, View, ViewStyle } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  Easing,
+  cancelAnimation,
+  runOnJS,
+  useDerivedValue,
+  useSharedValue,
+  withDecay,
+  withDelay,
+  withTiming,
+} from 'react-native-reanimated';
+
+const { width } = Dimensions.get('window');
+
+interface SlideProps {
+  dataSource: any;
+  renderItem: (index: number) => Element;
+  numberClone?: number;
+  widthItem?: number;
+  spacingItem?: number;
+  durationLoop?: number;
+  style: ViewStyle;
+}
+
+const Slide = ({
+  dataSource,
+  renderItem,
+  numberClone = 5,
+  widthItem = 100,
+  spacingItem = 16,
+  durationLoop = 20000,
+  style,
+}: SlideProps) => {
+  const translateX = useSharedValue(0);
+  const prevTranslationX = useSharedValue(0);
+  const lengthDataSource = useMemo(() => dataSource.length, [dataSource]);
+  const dataSourceClone = useMemo(
+    () => dataSource.concat(dataSource.slice(0, numberClone)),
+    [dataSource, numberClone]
+  );
+  const limitUpperBound = useMemo(
+    () => -lengthDataSource * widthItem - spacingItem * lengthDataSource,
+    [lengthDataSource, widthItem, spacingItem]
+  );
+
+  const loop = useCallback(
+    (delay: number) => {
+      translateX.value = withDelay(
+        delay,
+        withTiming(limitUpperBound, {
+          duration:
+            ((limitUpperBound - translateX.value) * durationLoop) /
+            limitUpperBound,
+          easing: Easing.linear,
+        })
+      );
+    },
+    [limitUpperBound, durationLoop, translateX]
+  );
+
+  useDerivedValue(() => {
+    if (translateX.value <= limitUpperBound + 2) {
+      translateX.value = 0;
+      runOnJS(loop)(0);
+    }
+  }, [loop]);
+
+  const panGesture = Gesture.Pan()
+    .onTouchesDown(() => {
+      cancelAnimation(translateX);
+      if (translateX.value < limitUpperBound + width / 2) {
+        translateX.value = 0;
+      } else if (
+        translateX.value > limitUpperBound + width / 2 &&
+        translateX.value < limitUpperBound + width
+      ) {
+        translateX.value = limitUpperBound + width;
+      }
+    })
+    .onStart(() => {
+      prevTranslationX.value = translateX.value;
+    })
+    .onUpdate(({ translationX }) => {
+      translateX.value = clamp(
+        prevTranslationX.value + translationX,
+        limitUpperBound + width,
+        0
+      );
+    })
+    .onFinalize((event) => {
+      translateX.value = withDecay(
+        {
+          velocity: event.velocityX,
+          clamp: [limitUpperBound + width, 0],
+        },
+        (finished) => {
+          if (finished) {
+            runOnJS(loop)(4000);
+          }
+        }
+      );
+    });
+
+  useEffect(() => {
+    loop(1000);
+  }, [loop]);
+
+  return (
+    <GestureDetector gesture={panGesture}>
+      <Animated.View
+        style={[
+          styles.container,
+          style,
+          {
+            gap: spacingItem,
+            transform: [
+              {
+                translateX,
+              },
+            ],
+          },
+        ]}>
+        {dataSourceClone.map((item, index: number) => (
+          <View key={index} style={{ width: widthItem }} collapsable={false}>
+            {renderItem(index)}
+          </View>
+        ))}
+      </Animated.View>
+    </GestureDetector>
+  );
+};
+
+export default memo(Slide);
+
+const clamp = (value: number, lowerBound: number, upperBound: number) => {
+  'worklet';
+  return Math.min(Math.max(lowerBound, value), upperBound);
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignSelf: 'flex-start',
+  },
+});
