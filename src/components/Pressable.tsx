a2235ca8276581c5946d0cd8383adc2547cc7bc3
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-13 15:17:56 +0200

    add delays, remove redundancy, remove old comments

--- src/components/Pressable.tsx
+++ src/components/Pressable.tsx
@@ -22,42 +22,8 @@ import {
 } from '../.';
 import { HoverGestureHandlerEventPayload } from '../handlers/gestures/hoverGesture';
 
-// const hoverEventToMouseEvent = (
-//   hoverEvent: GestureStateChangeEvent<HoverGestureHandlerEventPayload>,
-//   previousHoverEvent?: GestureStateChangeEvent<HoverGestureHandlerEventPayload>
-// ): MouseEvent => {
-//   const mouseEvent: MouseEvent = {
-//     altKey: false,
-//     button: 0, // which button activated event
-//     buttons: 1, // bitmask for all buttons
-//     clientX: hoverEvent.absoluteX,
-//     clientY: hoverEvent.absoluteY,
-//     ctrlKey: false,
-//     layerX: hoverEvent.absoluteX, // non_standard! poor docs!
-//     layerY: hoverEvent.absoluteY, // non_standard! poor docs!
-//     metaKey: false,
-//     movementX:
-//       hoverEvent.absoluteX -
-//       (previousHoverEvent?.absoluteX ?? hoverEvent.absoluteX),
-//     movementY:
-//       hoverEvent.absoluteY -
-//       (previousHoverEvent?.absoluteY ?? hoverEvent.absoluteY),
-//     offsetX: hoverEvent.x,
-//     offsetY: hoverEvent.y,
-//     pageX: hoverEvent.absoluteX,
-//     pageY: hoverEvent.absoluteY,
-//     relatedTarget: null, // add target ref
-//     screenX: hoverEvent.absoluteX,
-//     screenY: hoverEvent.absoluteY,
-//     shiftKey: false,
-//     x: hoverEvent.absoluteX,
-//     y: hoverEvent.absoluteY,
-//     getModifierState: () => false,
-//     initMouseEvent: () => null,
-//   };
-
-//   return mouseEvent;
-// };
+const DEFAULT_LONG_PRESS_DURATION = 500;
+const DEFAULT_HOVER_DELAY = 0;
 
 export interface PressableStateCallbackType {
   readonly pressed: boolean;
@@ -80,8 +46,7 @@ export interface PressableProps
     | null
     | ((
         event: GestureStateChangeEvent<HoverGestureHandlerEventPayload>
-      ) => void)
-    | undefined;
+      ) => void);
 
   /**
    * Called when the hover is deactivated to undo visual feedback.
@@ -90,23 +55,22 @@ export interface PressableProps
     | null
     | ((
         event: GestureStateChangeEvent<HoverGestureHandlerEventPayload>
-      ) => void)
-    | undefined;
+      ) => void);
 
   /**
    * Called when a single tap gesture is detected.
    */
-  onPress?: null | ((event: GestureTouchEvent) => void) | undefined;
+  onPress?: null | ((event: GestureTouchEvent) => void);
 
   /**
    * Called when a touch is engaged before `onPress`.
    */
-  onPressIn?: null | ((event: GestureTouchEvent) => void) | undefined;
+  onPressIn?: null | ((event: GestureTouchEvent) => void);
 
   /**
    * Called when a touch is released before `onPress`.
    */
-  onPressOut?: null | ((event: GestureTouchEvent) => void) | undefined;
+  onPressOut?: null | ((event: GestureTouchEvent) => void);
 
   /**
    * Called when a long-tap gesture is detected.
@@ -115,26 +79,19 @@ export interface PressableProps
     | null
     | ((
         event: GestureStateChangeEvent<LongPressGestureHandlerEventPayload>
-      ) => void)
-    | undefined;
+      ) => void);
 
   /**
    * Called after the element loses focus.
    * @platform macos windows
    */
-  onBlur?:
-    | null
-    | ((event: NativeSyntheticEvent<TargetedEvent>) => void)
-    | undefined;
+  onBlur?: null | ((event: NativeSyntheticEvent<TargetedEvent>) => void);
 
   /**
    * Called after the element is focused.
    * @platform macos windows
    */
-  onFocus?:
-    | null
-    | ((event: NativeSyntheticEvent<TargetedEvent>) => void)
-    | undefined;
+  onFocus?: null | ((event: NativeSyntheticEvent<TargetedEvent>) => void);
 
   /**
    * Either children or a render prop that receives a boolean reflecting whether
@@ -142,62 +99,61 @@ export interface PressableProps
    */
   children?:
     | React.ReactNode
-    | ((state: PressableStateCallbackType) => React.ReactNode)
-    | undefined;
+    | ((state: PressableStateCallbackType) => React.ReactNode);
 
   /**
    * Whether a press gesture can be interrupted by a parent gesture such as a
    * scroll event. Defaults to true.
    */
-  cancelable?: null | boolean | undefined;
+  cancelable?: null | boolean;
 
   /**
    * Duration to wait after hover in before calling `onHoverIn`.
    * @platform macos windows
    */
-  delayHoverIn?: number | null | undefined;
+  delayHoverIn?: number | null;
 
   /**
    * Duration to wait after hover out before calling `onHoverOut`.
    * @platform macos windows
    */
-  delayHoverOut?: number | null | undefined;
+  delayHoverOut?: number | null;
 
   /**
    * Duration (in milliseconds) from `onPressIn` before `onLongPress` is called.
    */
-  delayLongPress?: null | number | undefined;
+  delayLongPress?: null | number;
 
   /**
    * Whether the press behavior is disabled.
    */
-  disabled?: null | boolean | undefined;
+  disabled?: null | boolean;
 
   /**
    * Additional distance outside of this view in which a press is detected.
    */
-  hitSlop?: null | Insets | number | undefined;
+  hitSlop?: null | Insets | number;
 
   /**
    * Additional distance outside of this view in which a touch is considered a
    * press before `onPressOut` is triggered.
    */
-  pressRetentionOffset?: null | Insets | number | undefined;
+  pressRetentionOffset?: null | Insets | number;
 
   /**
    * If true, doesn't play system sound on touch.
    */
-  android_disableSound?: null | boolean | undefined;
+  android_disableSound?: null | boolean;
 
   /**
    * Enables the Android ripple effect and configures its color.
    */
-  android_ripple?: null | PressableAndroidRippleConfig | undefined;
+  android_ripple?: null | PressableAndroidRippleConfig;
 
   /**
    * Used only for documentation or testing (e.g. snapshot testing).
    */
-  testOnly_pressed?: null | boolean | undefined;
+  testOnly_pressed?: null | boolean;
 
   /**
    * Either view styles or a function that receives a boolean reflecting whether
@@ -205,13 +161,12 @@ export interface PressableProps
    */
   style?:
     | StyleProp<ViewStyle>
-    | ((state: PressableStateCallbackType) => StyleProp<ViewStyle>)
-    | undefined;
+    | ((state: PressableStateCallbackType) => StyleProp<ViewStyle>);
 
   /**
    * Duration (in milliseconds) to wait after press down before calling onPressIn.
    */
-  unstable_pressDelay?: number | undefined;
+  unstable_pressDelay?: number;
 }
 
 export default function Pressable(props: PressableProps) {
@@ -231,21 +186,33 @@ export default function Pressable(props: PressableProps) {
 
   const hover = Gesture.Hover()
     .onBegin((event) => {
-      props.onHoverIn?.(event);
+      setTimeout(
+        () => props.onHoverIn?.(event),
+        props.delayHoverIn ?? DEFAULT_HOVER_DELAY
+      );
     })
     .onEnd((event) => {
+      setTimeout(
+        () => props.onHoverIn?.(event),
+        props.delayHoverOut ?? DEFAULT_HOVER_DELAY
+      );
+
       props.onHoverOut?.(event);
     });
+
+  press.minDuration(props.delayLongPress ?? DEFAULT_LONG_PRESS_DURATION);
 
   // onBlur and onFocus don't exist in the docs
 
   touch.hitSlop(props.hitSlop);
+  press.hitSlop(props.hitSlop);
   hover.hitSlop(props.hitSlop);
 
   // todo: add props.pressRetentionOffset to touch and check if relative to pressable or to hitSlop
 
-  touch.enabled(!props.disabled);
-  hover.enabled(!props.disabled);
+  touch.enabled(!(props.disabled ?? false));
+  press.enabled(!(props.disabled ?? false));
+  hover.enabled(!(props.disabled ?? false));
 
   const gesture = Gesture.Race(hover, press, touch);
 
