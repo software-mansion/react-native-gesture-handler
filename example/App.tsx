b9b1b8a0731f56bef5bae7520237cafdd9174682
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-17 17:39:27 +0200

    add example comparing old and the new system

--- example/App.tsx
+++ example/App.tsx
@@ -35,6 +35,7 @@ import ContextMenu from './src/release_tests/contextMenu';
 import NestedTouchables from './src/release_tests/nestedTouchables';
 import NestedButtons from './src/release_tests/nestedButtons';
 import PointerType from './src/release_tests/pointerType';
+import SwipeableReanimation from './src/release_tests/swipeableReanimation';
 import NestedGestureHandlerRootViewWithModal from './src/release_tests/nestedGHRootViewWithModal';
 import { PinchableBox } from './src/recipes/scaleAndRotate';
 import PanAndScroll from './src/recipes/panAndScroll';
@@ -140,6 +141,7 @@ const EXAMPLES: ExamplesSection[] = [
       { name: 'MouseButtons', component: MouseButtons },
       { name: 'ContextMenu (web only)', component: ContextMenu },
       { name: 'PointerType', component: PointerType },
+      { name: 'Swipeable Reanimation', component: SwipeableReanimation },
       { name: 'RectButton (borders)', component: RectButtonBorders },
     ],
   },
---
+++ example/src/release_tests/swipeableReanimation/index.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import { Text, Animated as Laminated, StyleSheet } from 'react-native';
+
+import {
+  GestureHandlerRootView,
+  RectButton,
+} from 'react-native-gesture-handler';
+
+import { Swipeable } from 'react-native-gesture-handler';
+import { default as ReanimatedSwipeable } from 'react-native-gesture-handler/ReanimatedSwipeable';
+import Animated, {
+  SharedValue,
+  useAnimatedStyle,
+} from 'react-native-reanimated';
+
+function LeftAction(prog: SharedValue<number>, drag: SharedValue<number>) {
+  const styleAnimation = useAnimatedStyle(() => {
+    console.log('[R] showLeftProgress:', prog.value);
+    console.log('[R] appliedTranslation:', drag.value);
+
+    return {
+      transform: [{ translateX: drag.value - 50 }],
+    };
+  });
+
+  return (
+    <Animated.View style={styleAnimation}>
+      <Text style={styles.leftAction}>Text</Text>
+    </Animated.View>
+  );
+}
+
+function RightAction(prog: SharedValue<number>, drag: SharedValue<number>) {
+  const styleAnimation = useAnimatedStyle(() => {
+    console.log('[R] showRightProgress:', prog.value);
+    console.log('[R] appliedTranslation:', drag.value);
+
+    return {
+      transform: [{ translateX: drag.value + 50 }],
+    };
+  });
+
+  return (
+    <Animated.View style={styleAnimation}>
+      <Text style={styles.rightAction}>Text</Text>
+    </Animated.View>
+  );
+}
+
+function LegacyLeftAction(prog: any, drag: any) {
+  prog.addListener((value: any) => {
+    console.log('[L] showLeftProgress:', value.value);
+  });
+  drag.addListener((value: any) => {
+    console.log('[L] appliedTranslation:', value.value);
+  });
+
+  const trans = Laminated.subtract(drag, 50);
+
+  return (
+    <RectButton>
+      <Laminated.Text
+        style={[
+          styles.leftAction,
+          {
+            transform: [{ translateX: trans }],
+          },
+        ]}>
+        Text
+      </Laminated.Text>
+    </RectButton>
+  );
+}
+
+function LegacyRightAction(prog: any, drag: any) {
+  prog.addListener((value: any) => {
+    console.log('[L] showRightProgress:', value.value);
+  });
+  drag.addListener((value: any) => {
+    console.log('[L] appliedTranslation:', value.value);
+  });
+
+  const trans = Laminated.add(drag, 50);
+
+  return (
+    <RectButton>
+      <Laminated.Text
+        style={[
+          styles.rightAction,
+          {
+            transform: [{ translateX: trans }],
+          },
+        ]}>
+        Text
+      </Laminated.Text>
+    </RectButton>
+  );
+}
+
+export default function Example() {
+  return (
+    <GestureHandlerRootView>
+      <ReanimatedSwipeable
+        containerStyle={styles.reanimatedContainer}
+        friction={2}
+        leftThreshold={80}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderLeftActions={LeftAction}
+        renderRightActions={RightAction}>
+        <Text>[Reanimated] Swipe me!</Text>
+      </ReanimatedSwipeable>
+
+      <Swipeable
+        containerStyle={styles.legacyContainer}
+        friction={2}
+        leftThreshold={80}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderLeftActions={LegacyLeftAction}
+        renderRightActions={LegacyRightAction}>
+        <Text>[Legacy] Swipe me!</Text>
+      </Swipeable>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  leftAction: { width: 50, height: 50, backgroundColor: 'crimson' },
+  rightAction: { width: 50, height: 50, backgroundColor: 'purple' },
+  reanimatedContainer: {
+    height: 50,
+    backgroundColor: 'papayawhip',
+    alignItems: 'center',
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+  },
+  legacyContainer: {
+    height: 50,
+    backgroundColor: 'papayawhip',
+    alignItems: 'center',
+    borderBottomWidth: 1,
+  },
+});
