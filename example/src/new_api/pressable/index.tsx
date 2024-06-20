0e258ebb5009c34ef71b23f236b72e08f8b5ded3
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-20 12:56:48 +0200

    fix crashing on android

--- example/src/new_api/pressable/index.tsx
+++ example/src/new_api/pressable/index.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   },
   rectText: {
     color: BG_COLOR,
-    fontWeight: 700,
+    fontWeight: '700',
     position: 'absolute',
     right: 5,
     bottom: 2,
