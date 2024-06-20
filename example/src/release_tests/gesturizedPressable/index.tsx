7b2679076c4b70177c7d37ebe196acd3b237d3aa
Ignacy Łątka <latkaignacy@gmail.com>
2024-06-20 10:48:04 +0200

    add hitslop and slop indicators to example

--- example/src/release_tests/gesturizedPressable/index.tsx
+++ example/src/release_tests/gesturizedPressable/index.tsx
@@ -36,40 +36,47 @@ export default function Example() {
     console.log(`[${id}] Long pressed`);
   };
 
+  const hitSlop = 40;
+
   return (
-    <>
-      <View style={styles.container}>
-        <GesturizedPressable
-          style={styles.pressable}
-          onPressIn={() => pressIn('GH')}
-          onPressOut={() => pressOut('GH')}
-          onPress={(event) => press('GH', event)}
-          onHoverIn={() => hoverIn('GH')}
-          onHoverOut={() => hoverOut('GH')}
-          onLongPress={() => longPress('GH')}>
-          <View style={styles.textWrapper}>
-            <Text style={styles.text}>Gesturized press!</Text>
-          </View>
-        </GesturizedPressable>
+    <View style={styles.container}>
+      <View style={styles.slopIndicator}>
+        <Text>Hit Slop</Text>
       </View>
-      <View style={styles.container}>
-        <Pressable
-          style={styles.pressable}
-          onPressIn={() => pressIn('P')}
-          onPressOut={() => pressOut('P')}
-          onPress={(event) => press('P', event)}
-          onHoverIn={() => hoverIn('P')}
-          onHoverOut={() => hoverOut('P')}
-          onLongPress={() => longPress('P')}
-          onFocus={() => focus('P')} // web only
-          onBlur={() => blur('P')} // web only
-        >
-          <View style={styles.textWrapper}>
-            <Text style={styles.text}>Legacy press!</Text>
-          </View>
-        </Pressable>
+      <View style={styles.retentionIndicator}>
+        <Text>Retention Offset</Text>
       </View>
-    </>
+      <GesturizedPressable
+        style={styles.pressable}
+        hitSlop={hitSlop}
+        onPressIn={() => pressIn('GH')}
+        onPressOut={() => pressOut('GH')}
+        onPress={(event) => press('GH', event)}
+        onHoverIn={() => hoverIn('GH')}
+        onHoverOut={() => hoverOut('GH')}
+        onLongPress={() => longPress('GH')}>
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>Gesturized press!</Text>
+        </View>
+      </GesturizedPressable>
+      <Pressable
+        style={styles.pressable}
+        hitSlop={hitSlop} // ios only, check android
+        pressRetentionOffset={hitSlop * 2}
+        onPressIn={() => pressIn('P')}
+        onPressOut={() => pressOut('P')}
+        onPress={(event) => press('P', event)}
+        onHoverIn={() => hoverIn('P')}
+        onHoverOut={() => hoverOut('P')}
+        onLongPress={() => longPress('P')}
+        onFocus={() => focus('P')} // web only
+        onBlur={() => blur('P')} // web only
+      >
+        <View style={styles.textWrapper}>
+          <Text style={styles.text}>Legacy press!</Text>
+        </View>
+      </Pressable>
+    </View>
   );
 }
 
@@ -79,12 +86,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    gap: 60,
   },
   pressable: {
     backgroundColor: 'mediumpurple',
     width: 120,
     height: 120,
-    margin: 'auto',
   },
   textWrapper: {
     flex: 1,
@@ -93,5 +100,23 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#F5FCFF',
+  },
+  slopIndicator: {
+    position: 'absolute',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    height: 380,
+    width: 200,
+    padding: 4,
+    borderRightWidth: StyleSheet.hairlineWidth,
+  },
+  retentionIndicator: {
+    position: 'absolute',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    height: 440,
+    width: 280,
+    padding: 4,
+    borderRightWidth: StyleSheet.hairlineWidth,
   },
 });
