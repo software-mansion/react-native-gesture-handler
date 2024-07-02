2f818abb256cab285815fdfb9666c8ba9faca940
evanbacon <bacon@expo.io>
2024-07-01 21:29:06 -0500

    bind scope to null to prevent issues with inline requires

--- src/ghQueueMicrotask.ts
+++ src/ghQueueMicrotask.ts
@@ -1,5 +1,6 @@
 // We check for typeof requestAnimationFrame because of SSR
 export const ghQueueMicrotask =
-  typeof requestAnimationFrame === 'function'
-    ? requestAnimationFrame
-    : queueMicrotask;
+  typeof requestAnimationFrame === "function"
+    // Functions are bound to null to avoid issues with scope when using Metro inline requires.
+    ? requestAnimationFrame.bind(null)
+    : queueMicrotask.bind(null);
