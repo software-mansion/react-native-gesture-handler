4ebae5d7777c3a183cec7d64f5f6011d76cd0f5e
Michał Bert <michal.bert@swmansion.com>
2024-07-02 08:25:49 +0200

    Fix prettier

--- src/ghQueueMicrotask.ts
+++ src/ghQueueMicrotask.ts
@@ -1,6 +1,6 @@
 // We check for typeof requestAnimationFrame because of SSR
 export const ghQueueMicrotask =
-  typeof requestAnimationFrame === "function"
-    // Functions are bound to null to avoid issues with scope when using Metro inline requires.
-    ? requestAnimationFrame.bind(null)
+  typeof requestAnimationFrame === 'function'
+    ? // Functions are bound to null to avoid issues with scope when using Metro inline requires.
+      requestAnimationFrame.bind(null)
     : queueMicrotask.bind(null);
