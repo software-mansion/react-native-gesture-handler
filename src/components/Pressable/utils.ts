e2098dc59a0a69751265ea7cc6355861c1ae0bad
Ignacy Łątka <latkaignacy@gmail.com>
2024-07-25 11:37:27 +0200

    Fix nested pressable's event propagation. (#2981)
    ### Issue:
    `Test case`: `3` nested pressables
    `Actions`: `click`, `press for <500ms`, `press for >500ms`
    `Expected behaviour`: No propagation
    `Observed behaviour`: Propagation for `500ms` (until long press
    activates), then no propagation.
    
    ### Cause:
    Touch and non-active events disregard component's layer,
    only state respecting component's level is `ACTIVE`.
    
    ### Solution:
    Create a component which activates immediately after pressing
    `Pressable`, capture that event and use it for state setting and
    callback calling.
    
    ### Important:
    This PR also performs a lot of de-cluttering work in the `utils.ts`
    file.
    This is necessary due to the convoluted nature of the handled problem,
    it was especially important to me in this PR to be dealing with clearly
    labelled functions.
    
    closes: #2980

--- src/components/Pressable/utils.ts
+++ src/components/Pressable/utils.ts
@@ -6,7 +6,7 @@ import {
   GestureTouchEvent,
 } from '../../handlers/gestureHandlerCommon';
 import { HoverGestureHandlerEventPayload } from '../../handlers/gestures/hoverGesture';
-import { PressEvent, PressableEvent } from './PressableProps';
+import { InnerPressableEvent, PressableEvent } from './PressableProps';
 
 const numberAsInset = (value: number): Insets => ({
   left: value,
@@ -22,11 +22,11 @@ const addInsets = (a: Insets, b: Insets): Insets => ({
   bottom: (a.bottom ?? 0) + (b.bottom ?? 0),
 });
 
-const touchToPressEvent = (
+const touchDataToPressEvent = (
   data: TouchData,
   timestamp: number,
   targetId: number
-): PressEvent => ({
+): InnerPressableEvent => ({
   identifier: data.id,
   locationX: data.x,
   locationY: data.y,
@@ -38,16 +38,22 @@ const touchToPressEvent = (
   changedTouches: [], // Always empty - legacy compatibility
 });
 
-const changeToTouchData = (
+const gestureToPressEvent = (
   event: GestureStateChangeEvent<
     HoverGestureHandlerEventPayload | LongPressGestureHandlerEventPayload
-  >
-): TouchData => ({
-  id: event.handlerTag,
-  x: event.x,
-  y: event.y,
-  absoluteX: event.absoluteX,
-  absoluteY: event.absoluteY,
+  >,
+  timestamp: number,
+  targetId: number
+): InnerPressableEvent => ({
+  identifier: event.handlerTag,
+  locationX: event.x,
+  locationY: event.y,
+  pageX: event.absoluteX,
+  pageY: event.absoluteY,
+  target: targetId,
+  timestamp: timestamp,
+  touches: [], // Always empty - legacy compatibility
+  changedTouches: [], // Always empty - legacy compatibility
 });
 
 const isTouchWithinInset = (
@@ -60,7 +66,7 @@ const isTouchWithinInset = (
   (touch?.x ?? 0) > -(inset.left ?? 0) &&
   (touch?.y ?? 0) > -(inset.top ?? 0);
 
-const adaptStateChangeEvent = (
+const gestureToPressableEvent = (
   event: GestureStateChangeEvent<
     HoverGestureHandlerEventPayload | LongPressGestureHandlerEventPayload
   >
@@ -70,9 +76,7 @@ const adaptStateChangeEvent = (
   // As far as I can see, there isn't a conventional way of getting targetId with the data we get
   const targetId = 0;
 
-  const touchData = changeToTouchData(event);
-
-  const pressEvent = touchToPressEvent(touchData, timestamp, targetId);
+  const pressEvent = gestureToPressEvent(event, timestamp, targetId);
 
   return {
     nativeEvent: {
@@ -90,23 +94,25 @@ const adaptStateChangeEvent = (
   };
 };
 
-const adaptTouchEvent = (event: GestureTouchEvent): PressableEvent => {
+const gestureTouchToPressableEvent = (
+  event: GestureTouchEvent
+): PressableEvent => {
   const timestamp = Date.now();
 
   // As far as I can see, there isn't a conventional way of getting targetId with the data we get
   const targetId = 0;
 
-  const nativeTouches = event.allTouches.map((touch: TouchData) =>
-    touchToPressEvent(touch, timestamp, targetId)
+  const touchesList = event.allTouches.map((touch: TouchData) =>
+    touchDataToPressEvent(touch, timestamp, targetId)
   );
-  const nativeChangedTouches = event.changedTouches.map((touch: TouchData) =>
-    touchToPressEvent(touch, timestamp, targetId)
+  const changedTouchesList = event.changedTouches.map((touch: TouchData) =>
+    touchDataToPressEvent(touch, timestamp, targetId)
   );
 
   return {
     nativeEvent: {
-      touches: nativeTouches,
-      changedTouches: nativeChangedTouches,
+      touches: touchesList,
+      changedTouches: changedTouchesList,
       identifier: event.handlerTag,
       locationX: event.allTouches.at(0)?.x ?? -1,
       locationY: event.allTouches.at(0)?.y ?? -1,
@@ -167,10 +173,8 @@ const splitStyles = (from: ViewStyle): [ViewStyle, ViewStyle] => {
 export {
   numberAsInset,
   addInsets,
-  touchToPressEvent,
-  changeToTouchData,
   isTouchWithinInset,
-  adaptStateChangeEvent,
-  adaptTouchEvent,
+  gestureToPressableEvent,
+  gestureTouchToPressableEvent,
   splitStyles,
 };
