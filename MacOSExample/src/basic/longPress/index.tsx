b39f8fc45e1f78ad44eefbcd2f83e7d6fde06659
Michał Bert <michal.bert@swmansion.com>
2024-07-29 17:51:00 +0200

    Change logs

--- MacOSExample/src/basic/longPress/index.tsx
+++ MacOSExample/src/basic/longPress/index.tsx
@@ -3,12 +3,12 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
 export default function LongPressExample() {
   const g = Gesture.LongPress()
-    .onBegin((e) => console.log('onBegin'))
-    .onStart((e) => console.log('onStart'))
-    .onFinalize((e, s) => console.log('onFinalize', s))
-    .onTouchesDown((e) => console.log('onTouchesDown'))
-    .onTouchesMove((e) => console.log('onTouchesMove'))
-    .onTouchesUp((e) => console.log('onTouchesUp'))
+    .onBegin(() => console.log('onBegin'))
+    .onStart(() => console.log('onStart'))
+    .onFinalize((_, success) => console.log('onFinalize', success))
+    .onTouchesDown(() => console.log('onTouchesDown'))
+    .onTouchesMove(() => console.log('onTouchesMove'))
+    .onTouchesUp(() => console.log('onTouchesUp'))
     .minDuration(1000);
 
   return (
