8d5644bc5abdc31304ec670530a41b11acef68a0
Michał Bert <63123542+m-bert@users.noreply.github.com>
2024-08-01 09:10:13 +0200

    Update apple/RNGestureHandlerModule.mm
    Co-authored-by: Jakub Piasecki <jakub.piasecki@swmansion.com>

--- apple/RNGestureHandlerModule.mm
+++ apple/RNGestureHandlerModule.mm
@@ -99,8 +99,8 @@ void decorateRuntime(jsi::Runtime &runtime)
         if (!arguments[0].isObject()) {
           return jsi::Value::null();
         }
-        auto shadowNodeWrapper = shadowNodeFromValue(runtime, arguments[0]);
-        bool isFormsStackingContext = shadowNodeWrapper->getTraits().check(ShadowNodeTraits::FormsStackingContext);
+        auto shadowNode = shadowNodeFromValue(runtime, arguments[0]);
+        bool isFormsStackingContext = shadowNode->getTraits().check(ShadowNodeTraits::FormsStackingContext);
         return jsi::Value(isFormsStackingContext);
       });
   runtime.global().setProperty(runtime, "isFormsStackingContext", std::move(isFormsStackingContext));
