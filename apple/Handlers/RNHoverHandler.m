27838e281f5f42df77e18c963ff602f812a80b99
Michał Bert <michal.bert@swmansion.com>
2024-08-05 09:37:11 +0200

    Change pointerInside to true

--- apple/Handlers/RNHoverHandler.m
+++ apple/Handlers/RNHoverHandler.m
@@ -199,21 +199,21 @@ API_AVAILABLE(ios(13.4))
 {
   [self sendEventsInState:RNGestureHandlerStateBegan
            forViewWithTag:_view.reactTag
-            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO withPointerType:_pointerType]];
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES withPointerType:_pointerType]];
   [self sendEventsInState:RNGestureHandlerStateActive
            forViewWithTag:_view.reactTag
-            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO withPointerType:_pointerType]];
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES withPointerType:_pointerType]];
 }
 
 - (void)mouseExited:(NSEvent *)theEvent
 {
   [self sendEventsInState:RNGestureHandlerStateEnd
            forViewWithTag:_view.reactTag
-            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO withPointerType:_pointerType]];
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES withPointerType:_pointerType]];
 
   [self sendEventsInState:RNGestureHandlerStateUndetermined
            forViewWithTag:_view.reactTag
-            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:NO withPointerType:_pointerType]];
+            withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES withPointerType:_pointerType]];
 }
 
 @end
