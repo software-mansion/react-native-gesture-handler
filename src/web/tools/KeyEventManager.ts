1573a9039ff361ae950150bcd70c22333aee9c33
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-08 10:50:43 +0200

    remove unnecessary mapping function and remove unsynchronous measurements + other review suggestions

--- src/web/tools/KeyEventManager.ts
+++ src/web/tools/KeyEventManager.ts
@@ -1,14 +1,12 @@
 import { AdaptedEvent, EventTypes } from '../interfaces';
 import EventManager from './EventManager';
 import { PointerType } from '../../PointerType';
-import { View } from 'react-native';
 
 export default class KeyEventManager extends EventManager<HTMLElement> {
   private activationKeys = ['Enter', 'Space', 'NumpadEnter'];
   private cancelationKeys = ['Tab'];
 
   private keyDownCallback = (event: KeyboardEvent): void => {
-    console.log(event.code);
     if (this.cancelationKeys.indexOf(event.code) !== -1) {
       this.dispatchEvent(event, EventTypes.CANCEL);
     }
@@ -29,29 +27,19 @@ export default class KeyEventManager extends EventManager<HTMLElement> {
   };
 
   private dispatchEvent(event: KeyboardEvent, eventType: EventTypes) {
-    (event.target as unknown as View)?.measure?.(
-      (_x, _y, w, h, pageX, pageY) => {
-        const adaptedEvent = this.adaptEvent(
-          event,
-          eventType,
-          pageX,
-          pageY,
-          w,
-          h
-        );
-        switch (eventType) {
-          case EventTypes.UP:
-            this.onPointerUp(adaptedEvent);
-            break;
-          case EventTypes.DOWN:
-            this.onPointerDown(adaptedEvent);
-            break;
-          case EventTypes.CANCEL:
-            this.onPointerCancel(adaptedEvent);
-            break;
-        }
-      }
-    );
+    this.view.getBoundingClientRect();
+    const adaptedEvent = this.mapEvent(event, eventType);
+    switch (eventType) {
+      case EventTypes.UP:
+        this.onPointerUp(adaptedEvent);
+        break;
+      case EventTypes.DOWN:
+        this.onPointerDown(adaptedEvent);
+        break;
+      case EventTypes.CANCEL:
+        this.onPointerCancel(adaptedEvent);
+        break;
+    }
   }
 
   public registerListeners(): void {
@@ -64,36 +52,22 @@ export default class KeyEventManager extends EventManager<HTMLElement> {
     this.view.addEventListener('keyup', this.keyUpCallback);
   }
 
-  private adaptEvent(
-    event: KeyboardEvent,
-    eventType: EventTypes,
-    pageX: number,
-    pageY: number,
-    elementWidth: number,
-    elementHeight: number
-  ): AdaptedEvent {
-    return {
-      x: pageX + elementWidth / 2,
-      y: pageY + elementHeight / 2,
-      offsetX: pageX + elementWidth / 2,
-      offsetY: pageY + elementHeight / 2,
-      pointerId: 0,
-      eventType: eventType,
-      pointerType: PointerType.KEY,
-      time: event.timeStamp,
-    };
-  }
-
   protected mapEvent(
     event: KeyboardEvent,
     eventType: EventTypes
   ): AdaptedEvent {
-    // unused and disfunctional but has to be present
+    const viewRect = (event.target as HTMLElement)?.getBoundingClientRect?.();
+
+    const pointerPosition = {
+      x: viewRect.x + viewRect.width / 2,
+      y: viewRect.y + viewRect.height / 2,
+    };
+
     return {
-      x: 0,
-      y: 0,
-      offsetX: 0,
-      offsetY: 0,
+      x: pointerPosition.x,
+      y: pointerPosition.y,
+      offsetX: pointerPosition.x,
+      offsetY: pointerPosition.y,
       pointerId: 0,
       eventType: eventType,
       pointerType: PointerType.KEY,
