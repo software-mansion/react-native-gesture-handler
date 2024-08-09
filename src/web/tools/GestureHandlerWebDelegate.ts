6d645fce8324a48e2466b8e4db0fd4b4fc08ab7e
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-09 12:13:19 +0200

    [`web`] Add keyboard support to gesture handler (#3035)
    ## Description
    
    By default, all native android and web components support keyboard
    navigation and option to press elements with `Enter` or `Spacebar` keys.
    
    Gestures and components provided by Gesture Handler lack the latter
    option.
    
    This PR adds keyboard support to the entirety of gesture handler,
    allowing it to replace mouse or finger navigation.
    
    ## Test plan
    
    - try navigating around the example app using just the following keys:
    `Tab`, `Shift-Tab`, `Space`, `Enter`
    
    ## Gallery
    
    <details>
    <summary>
    Expand
    </summary>
    
    
    https://github.com/user-attachments/assets/3523142a-c786-4dc2-aa84-f49d96d03ecd
    
    
    
    https://github.com/user-attachments/assets/488c7716-91ed-42aa-8a6c-49a90db5002a
    </details>

--- src/web/tools/GestureHandlerWebDelegate.ts
+++ src/web/tools/GestureHandlerWebDelegate.ts
@@ -11,6 +11,7 @@ import { isPointerInBounds } from '../utils';
 import EventManager from './EventManager';
 import { Config } from '../interfaces';
 import { MouseButton } from '../../handlers/gestureHandlerCommon';
+import KeyboardEventManager from './KeyboardEventManager';
 
 export class GestureHandlerWebDelegate
   implements GestureHandlerDelegate<HTMLElement, IGestureHandler>
@@ -46,6 +47,7 @@ export class GestureHandlerWebDelegate
 
     this.eventManagers.push(new PointerEventManager(this.view));
     this.eventManagers.push(new TouchEventManager(this.view));
+    this.eventManagers.push(new KeyboardEventManager(this.view));
 
     this.eventManagers.forEach((manager) =>
       this.gestureHandler.attachEventManager(manager)
