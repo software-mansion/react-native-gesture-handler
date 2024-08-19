bfc9ddc79f664de96fefdbf7f678493f418b2d1a
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-19 16:00:26 +0200

    add bug repro

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Canvas } from '@react-three/fiber/native';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
 
-export default function EmptyExample() {
+export default function App() {
+  const [beginCount, setBeginCount] = useState(0);
+  const [startCount, setStartCount] = useState(0);
+  const [endCount, setEndCount] = useState(0);
+  const tap = Gesture.Tap()
+    .runOnJS(true)
+    .onBegin(() => {
+      console.log('begin');
+      setBeginCount(beginCount + 1);
+    })
+    .onStart(() => {
+      console.log('start');
+      setStartCount(startCount + 1);
+    })
+    .onEnd(() => {
+      console.log('end');
+      setEndCount(endCount + 1);
+    });
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <GestureDetector gesture={tap}>
+        <View style={{ flex: 1, backgroundColor: '#000' }}>
+          <Canvas
+            style={{
+              flex: 1,
+            }}>
+            <ambientLight intensity={1} />
+            <directionalLight color="red" position={[0, 0, 5]} />
+            <mesh
+              onClick={() => {
+                console.log('mesh click');
+              }}>
+              <boxGeometry />
+              <meshStandardMaterial color={'#ff0000'} />
+            </mesh>
+          </Canvas>
+        </View>
+      </GestureDetector>
+      <View style={styles.counters}>
+        <Text>Begin count: {beginCount}</Text>
+        <Text>Start count: {startCount}</Text>
+        <Text>End count: {endCount}</Text>
+      </View>
     </View>
   );
 }
@@ -13,7 +54,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  counters: {
+    position: 'absolute',
+    backgroundColor: 'white',
   },
 });
