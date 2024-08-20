2557b902145d425a60227cd95dd54ca85cdc10e0
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-20 14:24:37 +0200

    update example to test for multiple meshes on ios, improve telemetry

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { Canvas } from '@react-three/fiber/native';
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import { Euler } from 'three';
+import { Euler, Vector3 } from 'three';
 
 export default function App() {
   const [beginCount, setBeginCount] = useState(0);
@@ -10,20 +10,25 @@ export default function App() {
   const [endCount, setEndCount] = useState(0);
   const [downCount, setDownCount] = useState(0);
   const [upCount, setUpCount] = useState(0);
-  const [rotation, _setRotation] = useState({ x: 0, y: 1, z: 2 });
+  const [canceledCount, setCanceledCount] = useState(0);
+  const [rotation, _setRotation] = useState({
+    x: 0,
+    y: (45 / 180) * Math.PI,
+    z: (45 / 180) * Math.PI,
+  });
 
   const tap = Gesture.Tap()
     .runOnJS(true)
     .onBegin(() => {
-      console.log('begin');
+      //console.log('begin');
       setBeginCount(beginCount + 1);
     })
     .onStart(() => {
-      console.log('start');
+      //console.log('start');
       setStartCount(startCount + 1);
     })
     .onEnd(() => {
-      console.log('end');
+      //console.log('end');
       setEndCount(endCount + 1);
     })
     .onTouchesDown(() => {
@@ -33,27 +38,46 @@ export default function App() {
     .onTouchesUp(() => {
       console.log('up');
       setUpCount(upCount + 1);
+    })
+    .onTouchesCancelled(() => {
+      console.log('cancelled');
+      setCanceledCount(upCount + 1);
     });
 
   return (
     <View style={styles.container}>
       <GestureDetector gesture={tap}>
-        <View style={{ flex: 1, backgroundColor: '#000' }}>
-          <Canvas
-            style={{
-              flex: 1,
-            }}>
+        <View style={styles.background}>
+          <Canvas>
             <directionalLight
               position={[0, 0, 5]}
               rotation={new Euler(0.2, 0.2, 0)}
             />
-            <mesh
-              onClick={() => {
-                console.log('mesh click');
-              }}
-              rotation={new Euler(rotation.y, rotation.x, rotation.z)}>
-              <boxGeometry />
-              <meshStandardMaterial color={'#ff0000'} />
+            <mesh rotation={new Euler(rotation.y, rotation.x, rotation.z)}>
+              <mesh
+                onClick={() => {
+                  console.log('red click');
+                }}
+                position={new Vector3(0, 0, 1)}>
+                <boxGeometry />
+                <meshStandardMaterial color={'#ff0000'} />
+              </mesh>
+              <mesh
+                onClick={() => {
+                  console.log('blue click');
+                }}
+                position={new Vector3(1, 0, 0)}>
+                <boxGeometry />
+                <meshStandardMaterial color={'#0000ff'} />
+              </mesh>
+              <mesh
+                onClick={() => {
+                  console.log('green click');
+                }}
+                position={new Vector3(0, 1, 0)}>
+                <boxGeometry />
+                <meshStandardMaterial color={'#00ff00'} />
+              </mesh>
             </mesh>
           </Canvas>
         </View>
@@ -64,12 +88,17 @@ export default function App() {
         <Text>End count: {endCount}</Text>
         <Text>Down count: {downCount}</Text>
         <Text>Up count: {upCount}</Text>
+        <Text>Canceled count: {canceledCount}</Text>
       </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
