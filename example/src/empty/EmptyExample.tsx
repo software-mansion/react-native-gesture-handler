cc04e16657ea63d4b196d724c2465b426c090ce5
Ignacy Łątka <latkaignacy@gmail.com>
2024-08-30 12:56:36 +0200

    add testing example

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,19 +1,81 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 
 export default function EmptyExample() {
+  const position = useSharedValue({ x: 0, y: 0 });
+
+  const gesture = Gesture.Pan()
+    .activateAfterLongPress(250)
+    .onUpdate((e) => {
+      position.value = {
+        x: e.translationX,
+        y: e.translationY,
+      };
+    })
+    .onFinalize(() => {
+      position.value = {
+        x: withSpring(0),
+        y: withSpring(0),
+      };
+    });
+  const elementPadding = 7;
+  const elementFiller = (
+    <>
+      {new Array(elementPadding).fill(1).map(() => (
+        <View style={styles.box} key={Math.random()}>
+          <Text>Hello World!</Text>
+        </View>
+      ))}
+    </>
+  );
+
+  const animation = useAnimatedStyle(() => ({
+    ...styles.highlight,
+    transform: [
+      {
+        translateX: position.value.x ?? 0,
+      },
+      {
+        translateY: position.value.y ?? 0,
+      },
+    ],
+  }));
+
+  console.log('rendering');
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {elementFiller}
+      <GestureDetector gesture={gesture}>
+        <Animated.View style={animation}>
+          <Text>Hello World!</Text>
+        </Animated.View>
+      </GestureDetector>
+      {elementFiller}
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  box: {
+    padding: 40,
+    backgroundColor: 'navy',
+    justifyContent: 'center',
+  },
+  highlight: {
+    padding: 40,
+    backgroundColor: 'tomato',
+    color: 'white',
+    zIndex: 99,
   },
 });
