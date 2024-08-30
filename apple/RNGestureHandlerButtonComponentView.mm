f1e8f97ae80ad5b02f3cbfb0a6febe2e3fb718e4
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-08-30 15:57:05 +0200

    feat: add load method (#3063)
    PR adding `load` method needed for dynamic frameworks to work with the library. See https://github.com/facebook/react-native/pull/37274 for more information.

--- apple/RNGestureHandlerButtonComponentView.mm
+++ apple/RNGestureHandlerButtonComponentView.mm
@@ -21,6 +21,12 @@ using namespace facebook::react;
   RNGestureHandlerButton *_buttonView;
 }
 
+// Needed because of this: https://github.com/facebook/react-native/pull/37274
++ (void)load
+{
+  [super load];
+}
+
 - (instancetype)initWithFrame:(CGRect)frame
 {
   if (self = [super initWithFrame:frame]) {
