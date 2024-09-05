bd39127db37f6c45f586438161e04c7aea96a93e
Ignacy Łątka <latkaignacy@gmail.com>
2024-09-05 13:49:44 +0200

    add example and fix pressable

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,10 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Pressable } from 'react-native-gesture-handler';
 
-export default function EmptyExample() {
+const BACKGROUND_COLOR = '#F5FCFF';
+
+const HIT_SLOP = 60;
+const PRESS_RETENTION_OFFSET = HIT_SLOP;
+const BOX_SIZE = 100;
+
+export default function Example() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <View style={styles.retentionIndicator}>
+        <View style={styles.slopIndicator}>
+          <View style={styles.container}>
+            <Pressable
+              style={styles.pressable}
+              hitSlop={HIT_SLOP}
+              // pressRetentionOffset={PRESS_RETENTION_OFFSET}
+            >
+              <Text style={styles.text}>Pressable</Text>
+            </Pressable>
+          </View>
+          <Text style={styles.text}>Hit Slop</Text>
+        </View>
+        <Text style={styles.text}>Retention Offset</Text>
+      </View>
+      <Pressable
+        style={[styles.pressable]}
+        android_ripple={{
+          color: 'green',
+          borderless: false,
+          foreground: false,
+        }}>
+        <Text style={styles.text}>Function indicator</Text>
+      </Pressable>
     </View>
   );
 }
@@ -12,8 +42,34 @@ export default function EmptyExample() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: 450,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    gap: 60,
+  },
+  pressable: {
+    backgroundColor: 'mediumpurple',
+    borderWidth: StyleSheet.hairlineWidth,
+    width: BOX_SIZE,
+    height: BOX_SIZE,
+    justifyContent: 'center',
+  },
+  text: {
+    color: BACKGROUND_COLOR,
+    textAlign: 'center',
+  },
+  slopIndicator: {
+    backgroundColor: 'pink',
+    width: BOX_SIZE + HIT_SLOP * 2,
+    height: BOX_SIZE + HIT_SLOP,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  },
+  retentionIndicator: {
+    backgroundColor: 'plum',
+    alignSelf: 'center',
+    alignItems: 'center',
+    width: BOX_SIZE + HIT_SLOP * 2 + PRESS_RETENTION_OFFSET * 2,
+    height: BOX_SIZE + HIT_SLOP + PRESS_RETENTION_OFFSET,
   },
 });
