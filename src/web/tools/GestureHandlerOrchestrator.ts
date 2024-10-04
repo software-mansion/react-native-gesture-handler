54370bce9e21f5f1e7b260863306899bc1d2f0d8
Michał Bert <michal.bert@swmansion.com>
2024-10-04 16:57:11 +0200

    Add undefined check

--- src/web/tools/GestureHandlerOrchestrator.ts
+++ src/web/tools/GestureHandlerOrchestrator.ts
@@ -337,6 +337,13 @@ export default class GestureHandlerOrchestrator {
     const isPointerWithinBothBounds = (pointer: number) => {
       const point = handler.getTracker().getLastAbsoluteCoords(pointer);
 
+      // In some rare cases it may happen that this method will be called before handler
+      // has started tracking pointers. When it happens, `point` will be undefined, leading to
+      // silent fail
+      if (!point) {
+        return false;
+      }
+
       return (
         handler.getDelegate().isPointerInBounds(point) &&
         otherHandler.getDelegate().isPointerInBounds(point)
