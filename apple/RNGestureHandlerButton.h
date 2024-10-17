d8d177295dced6a4e4e94696df2699f73d1faf19
Michał Bert <michal.bert@swmansion.com>
2024-10-17 12:40:57 +0200

    Remove ifs

--- apple/RNGestureHandlerButton.h
+++ apple/RNGestureHandlerButton.h
@@ -10,9 +10,7 @@
 
 #if TARGET_OS_OSX
 
-#ifdef __cplusplus
 #include <react/renderer/core/LayoutMetrics.h>
-#endif
 
 @protocol RCTComponentViewProtocol;
 
@@ -27,12 +25,10 @@
 @property (nonatomic) BOOL userEnabled;
 
 #if TARGET_OS_OSX
-#ifdef __cplusplus
 - (void)mountChildComponentView:(RNGHUIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index;
 - (void)unmountChildComponentView:(RNGHUIView<RCTComponentViewProtocol> *)childComponentView index:(NSInteger)index;
 - (void)updateLayoutMetrics:(const facebook::react::LayoutMetrics &)layoutMetrics
            oldLayoutMetrics:(const facebook::react::LayoutMetrics &)oldLayoutMetrics;
-#endif
 #endif
 
 @end
