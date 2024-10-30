2aba2a120f1971891f5b1e32a1d32c94e9b6c87d
Ignacy Łątka <latkaignacy@gmail.com>
2024-10-30 15:03:19 +0100

    use common example, config fixes

--- FabricExample/babel.config.js
+++ FabricExample/babel.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+      //react-native-reanimated/plugin has to be listed last.
+      'react-native-reanimated/plugin',
+    ],
+  };
 };
