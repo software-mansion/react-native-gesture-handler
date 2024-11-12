039e2289993ce7be03fcff0aca17a16f81877d7f
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-11-12 17:13:25 +0100

    Bump Reanimated

---
+++ android/src/main/jni/OnLoad.cpp
@@ -0,0 +1,8 @@
+#include <fbjni/fbjni.h>
+#include "RNGestureHandlerModule.h"
+
+JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *) {
+    return facebook::jni::initialize(vm, [] {
+        gesturehandler::RNGestureHandlerModule::registerNatives();
+    });
+}
---
+++ android/src/main/jni/RNGestureHandlerModule.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <ReactCommon/BindingsInstallerHolder.h>
+#include <fbjni/fbjni.h>
+#include <string>
+
+namespace gesturehandler {
+    using namespace facebook;
+    using namespace facebook::jni;
+    using namespace facebook::react;
+
+    class RNGestureHandlerModule : public jni::HybridClass<RNGestureHandlerModule> {
+    public:
+        static auto constexpr kJavaDescriptor =
+                "Lcom/swmansion/gesturehandler/react/RNGestureHandlerModule;";
+        static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject> jThis);
+        static void registerNatives();
+        ~RNGestureHandlerModule();
+
+    private:
+        friend HybridBase;
+        jsi::Runtime* runtime = nullptr;
+
+        jni::global_ref<RNGestureHandlerModule::javaobject> javaPart_;
+        explicit RNGestureHandlerModule(jni::alias_ref<RNGestureHandlerModule::javaobject> jThis);
+        jni::local_ref<BindingsInstallerHolder::javaobject> getBindingsInstallerCxx();
+
+        void setGestureState(int handlerTag, int state);
+        void decorateRuntime(jsi::Runtime& runtime);
+        void decorateUIRuntime();
+    };
+} // namespace newarchtricks
