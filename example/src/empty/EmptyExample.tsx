2d90e7617cf349c9b5871632e6d1e5a8b200ba7c
Michał Bert <michal.bert@swmansion.com>
2024-11-21 16:04:33 +0100

    Add example

--- example/src/empty/EmptyExample.tsx
+++ example/src/empty/EmptyExample.tsx
@@ -1,19 +1,67 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
 
-export default function EmptyExample() {
+import {
+  Text,
+  GestureHandlerRootView,
+  TouchableOpacity,
+} from 'react-native-gesture-handler';
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 64, opacity: 0.25 }}>😞</Text>
-      <Text style={{ fontSize: 24, opacity: 0.25 }}>It's so empty here</Text>
-    </View>
+    <GestureHandlerRootView style={styles.container}>
+      <Text style={{ fontSize: 30 }}>{`Counter: ${counter}`}</Text>
+
+      <TouchableOpacity
+        onPress={() => {
+          console.log('Touchable');
+          setCounter((prev) => prev + 1);
+        }}>
+        <Text
+          style={[styles.textCommon, styles.outerText]}
+          onPress={() => {
+            console.log('Outer text');
+            setCounter((prev) => prev + 1);
+          }}>
+          {'Outer Text '}
+          <Text
+            style={[styles.textCommon, styles.innerText]}
+            onPress={() => {
+              console.log('Nested text');
+              setCounter((prev) => prev + 1);
+            }}>
+            {'Nested Text'}
+          </Text>
+        </Text>
+      </TouchableOpacity>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
+
+    gap: 20,
+  },
+
+  textCommon: {
+    padding: 10,
+    color: 'white',
+  },
+
+  outerText: {
+    fontSize: 30,
+    borderWidth: 2,
+    backgroundColor: '#131313',
+  },
+
+  innerText: {
+    fontSize: 25,
+    backgroundColor: '#F06312',
   },
 });
