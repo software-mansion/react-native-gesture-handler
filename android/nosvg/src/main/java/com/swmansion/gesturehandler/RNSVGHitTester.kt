f79a1fd28eb3805ac8d9661b29d83d81c891cc1c
Ignacy Łątka <latkaignacy@gmail.com>
2024-11-27 15:33:31 +0100

    add initial svg integration code

---
+++ android/nosvg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -0,0 +1,9 @@
+package com.swmansion.gesturehandler
+
+class RNSVGHitTester {
+  @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
+  // This is necessary on new architecture
+  fun <T> hitTest(view: T, posX: Integer, posY: Integer) {
+    // no-op
+  }
+}
---
+++ android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -0,0 +1,8 @@
+package com.swmansion.gesturehandler
+
+class RNSVGHitTester {
+  @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
+  fun <T> hitTest(view: T, posX: Integer, posY: Integer) {
+    // no-op
+  }
+}
