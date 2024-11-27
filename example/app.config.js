e7e0acdfd7673de40725f1549ee0f051fbd1d45e
Ignacy Łątka <latkaignacy@gmail.com>
2024-11-27 14:22:42 +0100

    Add fabric support to the example app (#3204)
    ## Description
    
    <!--
    Description and motivation for this PR.
    
    Include 'Fixes #<number>' if this is fixing some issue.
    -->
    
    Adds `fabric` support to the example app. 
    To use `fabric`, set the optional `FABRIC_ENABLED` environment variable
    to `1` during `yarn` installation.
    
    ## Test plan
    
    - test `fabric`
      - `cd example`
      - `FABRIC_ENABLED=1 yarn`
      - `yarn start`
      - run `Android` via `Android Studio`
      - `cd ios`
      - bundle exec pod install`
      - run `iOS` via `XCode`
    - see how when apps load, the metro displays `bridgeless mode enabled`
    message
    - test `paper`
      - `cd example`
      - `yarn`
      - `yarn start`
      - run `Android` via `Android Studio`
      - `cd ios`
      - `bundle exec pod install`
      - run `iOS` via `XCode`

---
+++ example/app.config.js
@@ -0,0 +1,59 @@
+export default ({ config }) => ({
+  ...config,
+  name: 'example',
+  slug: 'example',
+  version: '0.0.1',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  sdkVersion: '51.0.0',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'cover',
+    backgroundColor: '#F8F9FF',
+  },
+  ios: {
+    bundleIdentifier: 'com.example',
+    buildNumber: '1',
+    supportsTablet: true,
+  },
+  android: {
+    versionCode: 1,
+    package: 'com.example',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#F8F9FF',
+    },
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: [
+    [
+      'expo-camera',
+      {
+        cameraPermission: 'Allow RNGH example to access your camera',
+      },
+    ],
+    [
+      'expo-font',
+      {
+        fonts: [
+          './node_modules/@swmansion/icons/fonts/broken/swm-icons-broken.ttf',
+          './node_modules/@swmansion/icons/fonts/outline/swm-icons-outline.ttf',
+          './node_modules/@swmansion/icons/fonts/curved/swm-icons-curved.ttf',
+        ],
+      },
+    ],
+    [
+      'expo-build-properties',
+      {
+        ios: {
+          newArchEnabled: Boolean(Number(process.env.FABRIC_ENABLED)),
+        },
+        android: {
+          newArchEnabled: Boolean(Number(process.env.FABRIC_ENABLED)),
+        },
+      },
+    ],
+  ],
+});
