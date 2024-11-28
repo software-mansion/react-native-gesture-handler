46a4c4b51398af3418e34e4bd203616c1747c019
Ignacy Łątka <latkaignacy@gmail.com>
2024-11-28 11:57:42 +0100

    remove another draft of swipeable component

--- example/App.tsx
+++ example/App.tsx
@@ -29,7 +29,6 @@ import {
   TouchablesIndex,
   TouchableExample,
 } from './src/release_tests/touchables';
-import Rows from './src/release_tests/rows';
 import NestedFling from './src/release_tests/nestedFling';
 import MouseButtons from './src/release_tests/mouseButtons';
 import ContextMenu from './src/release_tests/contextMenu';
@@ -178,7 +177,6 @@ const EXAMPLES: ExamplesSection[] = [
       },
       { name: 'Double pinch & rotate', component: DoublePinchRotate },
       { name: 'Double draggable', component: DoubleDraggable },
-      { name: 'Rows', component: Rows },
       { name: 'Nested Fling', component: NestedFling },
       {
         name: 'Combo',
