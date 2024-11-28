b09b0bc4c0dea2be794ce20dee2273a93c931774
Ignacy Łątka <latkaignacy@gmail.com>
2024-11-28 11:23:46 +0100

    add helper functions to the integration interface

--- android/nosvg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
+++ android/nosvg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -1,8 +1,12 @@
 package com.swmansion.gesturehandler
 
 class RNSVGHitTester {
-  @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
+  fun <T> isSvgElement(view: T) {
+    return false
+  }
+
   // This is necessary on new architecture
+  @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
   fun <T> hitTest(view: T, posX: Integer, posY: Integer) {
     // no-op
   }
--- android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
+++ android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -3,8 +3,12 @@ package com.swmansion.gesturehandler
 import com.horcrux.svg.RenderableView
 
 class RNSVGHitTester {
+  fun <T> isSvgElement(view: T) {
+    return (view instanceof RenderableView);
+  }
+
   @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
   fun hitTest(view: RenderableView, posX: Integer, posY: Integer) {
-    // no-op
+    // view.hitTest(...);
   }
 }
