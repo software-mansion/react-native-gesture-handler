0ade4c0aee5c8b0fb306a801c3587333d5137c99
Michał Bert <michal.bert@swmansion.com>
2024-11-28 15:11:55 +0100

    Change ref to refHandler

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,4 +1,10 @@
-import React, { forwardRef, Ref, RefObject, useEffect, useRef } from 'react';
+import React, {
+  ForwardedRef,
+  forwardRef,
+  RefObject,
+  useEffect,
+  useRef,
+} from 'react';
 import {
   Platform,
   Text as RNText,
@@ -7,32 +13,47 @@ import {
 
 import { Gesture, GestureDetector } from '../';
 
-export const Text = forwardRef((props: RNTextProps, ref: Ref<RNText>) => {
-  const { children, onPress, ...rest } = props;
+export const Text = forwardRef(
+  (props: RNTextProps, ref: ForwardedRef<RNText>) => {
+    const { children, onPress, ...rest } = props;
+    const textRef = useRef<RNText | null>(null);
+    const native = Gesture.Native().runOnJS(true);
 
-  const textRef = useRef<RNText>(null);
-  const native = Gesture.Native().runOnJS(true);
+    const refHandler = (node: any) => {
+      textRef.current = node;
 
-  useEffect(() => {
-    if (Platform.OS !== 'web') {
-      return;
-    }
+      if (ref === null) {
+        return;
+      }
 
-    const textElement = ref
-      ? (ref as RefObject<RNText>).current
-      : textRef.current;
+      if (typeof ref === 'function') {
+        ref(node);
+      } else {
+        ref.current = node;
+      }
+    };
 
-    // @ts-ignore at this point we are sure that text is div
-    textElement?.setAttribute('rnghtext', 'true');
-  }, []);
+    useEffect(() => {
+      if (Platform.OS !== 'web') {
+        return;
+      }
 
-  return (
-    <GestureDetector gesture={native}>
-      <RNText onPress={onPress} ref={ref ?? textRef} {...rest}>
-        {children}
-      </RNText>
-    </GestureDetector>
-  );
-});
+      const textElement = ref
+        ? (ref as RefObject<RNText>).current
+        : textRef.current;
+
+      // @ts-ignore at this point we are sure that text is div
+      textElement?.setAttribute('rnghtext', 'true');
+    }, []);
+
+    return (
+      <GestureDetector gesture={native}>
+        <RNText onPress={onPress} ref={refHandler} {...rest}>
+          {children}
+        </RNText>
+      </GestureDetector>
+    );
+  }
+);
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Text = typeof Text & RNText;
