4bf1b700a89e5ade4f4a522da8f706010eda46b5
Ignacy Łątka <latkaignacy@gmail.com>
2024-11-29 13:50:25 +0100

    adjust example to highlight ios issues

--- example/src/release_tests/svg/index.tsx
+++ example/src/release_tests/svg/index.tsx
@@ -5,22 +5,27 @@ import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Svg, { Circle } from 'react-native-svg';
 
 export default function EmptyExample() {
-  const tap = Gesture.Tap().onStart(() => console.log('clicked'));
+  const elementTap = Gesture.Tap().onStart(() => console.log('clicked circle'));
+  const containerTap = Gesture.Tap().onStart(() =>
+    console.log('clicked container')
+  );
   return (
     <View style={[{ alignItems: 'center', justifyContent: 'center' }]}>
       <View style={{ backgroundColor: 'tomato' }}>
-        <Svg height="300" width="300">
-          <GestureDetector gesture={tap}>
-            <Circle
-              cx="150"
-              cy="150"
-              r="140"
-              stroke="green"
-              strokeWidth="2.5"
-              fill="green"
-            />
-          </GestureDetector>
-        </Svg>
+        <GestureDetector gesture={containerTap}>
+          <Svg height="300" width="300">
+            <GestureDetector gesture={elementTap}>
+              <Circle
+                cx="150"
+                cy="150"
+                r="140"
+                stroke="green"
+                strokeWidth="2.5"
+                fill="green"
+              />
+            </GestureDetector>
+          </Svg>
+        </GestureDetector>
       </View>
       <Text>
         Clicks should only be registered while pressing on the green area.
