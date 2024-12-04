013c7a3a52f6053608e93533449a3dbb24d9d328
Ignacy Łątka <latkaignacy@gmail.com>
2024-12-04 12:18:43 +0100

    add more tests to the example app

--- example/src/release_tests/svg/index.tsx
+++ example/src/release_tests/svg/index.tsx
@@ -1,36 +1,77 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 
-import Svg, { Circle } from 'react-native-svg';
+import Svg, { Circle, Rect } from 'react-native-svg';
 
 export default function EmptyExample() {
-  const elementTap = Gesture.Tap().onStart(() => console.log('clicked circle'));
+  const circleElementTap = Gesture.Tap().onStart(() =>
+    console.log('clicked circle')
+  );
+  const rectElementTap = Gesture.Tap().onStart(() =>
+    console.log('clicked parallelogram')
+  );
   const containerTap = Gesture.Tap().onStart(() =>
     console.log('clicked container')
   );
+  const vbContainerTap = Gesture.Tap().onStart(() =>
+    console.log('clicked viewbox container (pixels)')
+  );
+  const vbCircleTap = Gesture.Tap().onStart(() =>
+    console.log('clicked viewbox circle (pixels)')
+  );
+
   return (
-    <View style={[{ alignItems: 'center', justifyContent: 'center' }]}>
-      <View style={{ backgroundColor: 'tomato' }}>
-        <GestureDetector gesture={containerTap}>
-          <Svg height="300" width="300">
-            <GestureDetector gesture={elementTap}>
-              <Circle
-                cx="150"
-                cy="150"
-                r="140"
-                stroke="green"
-                strokeWidth="2.5"
-                fill="green"
-              />
-            </GestureDetector>
-          </Svg>
-        </GestureDetector>
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          Overlapping SVGs with gesture detectors
+        </Text>
+        <View style={{ backgroundColor: 'tomato' }}>
+          <GestureDetector gesture={containerTap}>
+            <Svg height="250" width="250">
+              <GestureDetector gesture={circleElementTap}>
+                <Circle cx="125" cy="125" r="125" fill="green" />
+              </GestureDetector>
+              <GestureDetector gesture={rectElementTap}>
+                <Rect skewX="45" width="125" height="250" fill="yellow" />
+              </GestureDetector>
+            </Svg>
+          </GestureDetector>
+        </View>
+        <Text>
+          Clicks should only be registered while pressing on the green area.
+          Pressing the tomato-colored area should not have an effect
+        </Text>
       </View>
-      <Text>
-        Clicks should only be registered while pressing on the green area.
-        Pressing the tomato-colored area should not have an effect
-      </Text>
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          SVG view with ViewBox and pixel dimensions
+        </Text>
+        <View style={{ backgroundColor: 'tomato' }}>
+          <GestureDetector gesture={vbContainerTap}>
+            <Svg height="250" width="250" viewBox="-300 -300 600 600">
+              <GestureDetector gesture={vbCircleTap}>
+                <Circle r="150" fill="green" />
+              </GestureDetector>
+            </Svg>
+          </GestureDetector>
+        </View>
+        <Text>ViewBox property remaps SVG to user coordinate spaces</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 48,
+  },
+  header: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+});
