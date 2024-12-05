9f3370b7d9d6082092abdb7b4c9005da561d2241
Michał Bert <michal.bert@swmansion.com>
2024-12-05 16:20:37 +0100

    Fix TouchableNativeFeedback deprecation

--- src/components/touchables/TouchableNativeFeedback.tsx
+++ src/components/touchables/TouchableNativeFeedback.tsx
@@ -1,6 +1,12 @@
-import { TouchableNativeFeedback } from 'react-native';
+import { TouchableNativeFeedback as RNTouchableNativeFeedback } from 'react-native';
 
 /**
  * @deprecated TouchableNativeFeedback will be removed in Gesture Handler 4
  */
+const TouchableNativeFeedback: React.FC<
+  React.ComponentProps<typeof RNTouchableNativeFeedback>
+> = (props) => {
+  return <RNTouchableNativeFeedback {...props} />;
+};
+
 export default TouchableNativeFeedback;
