292d2ac2a70cdd45c2baed7be1a663311041555e
Michał Bert <michal.bert@swmansion.com>
2024-12-05 16:46:05 +0100

    Add react import

--- src/components/touchables/TouchableNativeFeedback.tsx
+++ src/components/touchables/TouchableNativeFeedback.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { TouchableNativeFeedback as RNTouchableNativeFeedback } from 'react-native';
 import { tagMessage } from '../../utils';
 
