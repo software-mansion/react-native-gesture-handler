c3ceac177a26bf7f4b3ad412fbf3383c3d1bc279
Michał Bert <michal.bert@swmansion.com>
2024-12-05 16:03:29 +0100

    Deprecate TouchableWithoutFeedback

--- src/components/touchables/TouchableWithoutFeedback.tsx
+++ src/components/touchables/TouchableWithoutFeedback.tsx
@@ -3,8 +3,14 @@ import { PropsWithChildren } from 'react';
 import GenericTouchable from './GenericTouchable';
 import type { GenericTouchableProps } from './GenericTouchableProps';
 
+/**
+ * @deprecated TouchableWithoutFeedback will be removed in Gesture Handler 4
+ */
 export type TouchableWithoutFeedbackProps = GenericTouchableProps;
 
+/**
+ * @deprecated TouchableWithoutFeedback will be removed in Gesture Handler 4
+ */
 const TouchableWithoutFeedback = React.forwardRef<
   GenericTouchable,
   PropsWithChildren<TouchableWithoutFeedbackProps>
