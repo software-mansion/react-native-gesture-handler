da986137b3ee67220ce2a179b3b599ef25eb60c9
Michał Bert <63123542+m-bert@users.noreply.github.com>
2024-12-06 16:42:42 +0100

    Deprecate `Touchable` components  (#3260)
    ## Description
    
    This PR deprecates `Touchable` components in our repo
    
    ## Test plan
    
    Try importing any `Touchable` (or its `props`). After importing look at
    the console.
    
    ---------
    
    Co-authored-by: Jakub Piasecki <jakub.piasecki@swmansion.com>

--- src/components/touchables/TouchableNativeFeedback.tsx
+++ src/components/touchables/TouchableNativeFeedback.tsx
@@ -1,3 +1,22 @@
-import { TouchableNativeFeedback } from 'react-native';
+import React, { useEffect } from 'react';
+import { TouchableNativeFeedback as RNTouchableNativeFeedback } from 'react-native';
+import { tagMessage } from '../../utils';
+
+/**
+ * @deprecated TouchableNativeFeedback will be removed in the future version of Gesture Handler.
+ */
+const TouchableNativeFeedback: React.FC<
+  React.ComponentProps<typeof RNTouchableNativeFeedback>
+> = (props) => {
+  useEffect(() => {
+    console.warn(
+      tagMessage(
+        'TouchableOpacity component will be removed in the future version of Gesture Handler.'
+      )
+    );
+  }, []);
+
+  return <RNTouchableNativeFeedback {...props} />;
+};
 
 export default TouchableNativeFeedback;
