82d62c03e7af70442e0e13834f6dc4a5a5a22611
Michał Bert <63123542+m-bert@users.noreply.github.com>
2024-12-06 16:42:42 +0100

    Deprecate `Touchable` components  (#3260)
    ## Description
    
    This PR deprecates `Touchable` components in our repo
    
    ## Test plan
    
    Try importing any `Touchable` (or its `props`). After importing look at
    the console.
    
    ---------
    
    Co-authored-by: Jakub Piasecki <jakub.piasecki@swmansion.com>

--- src/components/touchables/TouchableWithoutFeedback.tsx
+++ src/components/touchables/TouchableWithoutFeedback.tsx
@@ -3,8 +3,14 @@ import { PropsWithChildren } from 'react';
 import GenericTouchable from './GenericTouchable';
 import type { GenericTouchableProps } from './GenericTouchableProps';
 
+/**
+ * @deprecated TouchableWithoutFeedback will be removed in the future version of Gesture Handler.
+ */
 export type TouchableWithoutFeedbackProps = GenericTouchableProps;
 
+/**
+ * @deprecated TouchableWithoutFeedback will be removed in the future version of Gesture Handler.
+ */
 const TouchableWithoutFeedback = React.forwardRef<
   GenericTouchable,
   PropsWithChildren<TouchableWithoutFeedbackProps>
