13b32eb0a9aa992e9fe72577ca8923157f73c9bc
Ignacy Łątka <latkaignacy@gmail.com>
2024-12-17 16:21:42 +0100

    remove console log

--- src/components/touchables/TouchableNativeFeedback.tsx
+++ src/components/touchables/TouchableNativeFeedback.tsx
@@ -1,19 +1,6 @@
-import { TouchableNativeFeedback as RNTouchableNativeFeedback } from 'react-native';
-import { tagMessage } from '../../utils';
+import { TouchableNativeFeedback } from 'react-native';
 
 /**
  * @deprecated TouchableNativeFeedback will be removed in the future version of Gesture Handler.
  */
-class TouchableNativeFeedback extends RNTouchableNativeFeedback {
-  componentDidMount() {
-    console.warn(
-      tagMessage(
-        'TouchableNativeFeedback component will be removed in the future version of Gesture Handler.'
-      )
-    );
-
-    return super.componentDidMount?.();
-  }
-}
-
 export default TouchableNativeFeedback;
