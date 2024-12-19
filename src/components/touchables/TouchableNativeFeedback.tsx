1561c537f6e13166ad999fe2091477fb3a98ca02
Ignacy Łątka <latkaignacy@gmail.com>
2024-12-19 12:10:05 +0100

    Fix `TouchableNativeFeedback` regression (#3294)
    ## Description
    
    #3260 introduced a regression to the `Gesture Handlers`'s
    `TouchableNativeFeedback` component.
    The component used to be a direct reference to the
    `TouchableNativeFeedback`, which has a [couple of static
    methods](https://reactnative.dev/docs/touchablenativefeedback#methods).
    When it was replaced with a function component, the static methods got
    removed, leaving only the render function of the
    `TouchableNativeFeedback`.
    
    ## Test plan
    
    See how the type errors in the
    `example/src/release_tests/touchables/index.tsx` are resolved.

--- src/components/touchables/TouchableNativeFeedback.tsx
+++ src/components/touchables/TouchableNativeFeedback.tsx
@@ -1,22 +1,8 @@
-import React, { useEffect } from 'react';
 import { TouchableNativeFeedback as RNTouchableNativeFeedback } from 'react-native';
-import { tagMessage } from '../../utils';
 
 /**
  * @deprecated TouchableNativeFeedback will be removed in the future version of Gesture Handler.
  */
-const TouchableNativeFeedback: React.FC<
-  React.ComponentProps<typeof RNTouchableNativeFeedback>
-> = (props) => {
-  useEffect(() => {
-    console.warn(
-      tagMessage(
-        'TouchableOpacity component will be removed in the future version of Gesture Handler.'
-      )
-    );
-  }, []);
-
-  return <RNTouchableNativeFeedback {...props} />;
-};
+const TouchableNativeFeedback = RNTouchableNativeFeedback;
 
 export default TouchableNativeFeedback;
