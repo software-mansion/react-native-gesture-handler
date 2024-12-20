cf0bc83f5db4d0e24562668e27a00e4e10c4eff6
Ignacy Łątka <latkaignacy@gmail.com>
2024-12-20 18:03:47 +0100

    initial view-traversing bounds checker, fix SvgView bounds recognition

--- android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
+++ android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -1,27 +1,55 @@
 package com.swmansion.gesturehandler
 
+import android.graphics.Matrix
 import com.horcrux.svg.RenderableView
+import com.horcrux.svg.SvgView
 
 class RNSVGHitTester {
   companion object {
     fun isSvgElement(view: Any): Boolean {
-      return (view is RenderableView)
+      return (view is RenderableView || view is SvgView)
     }
 
     fun hitTest(view: Any, posX: Float, posY: Float): Boolean {
+      if (view is SvgView) {
+        return view.reactTagForTouch(posX, posY) != -1
+      }
+
       if (view is RenderableView) {
+        var highestView = view
+        var highestRoot = view.svgView
+
+        val pointTransformations = mutableListOf<Matrix>()
+
+        while (highestView is SvgView || highestView is RenderableView) {
+          if (highestView is RenderableView) {
+            highestView = highestView.parent
+          } else if (highestView is SvgView) {
+            highestRoot = highestView
+            pointTransformations.add(highestView.mInvViewBoxMatrix)
+            highestView = highestView.parent
+          }
+        }
+
         val viewLocation = intArrayOf(0, 0)
         val rootLocation = intArrayOf(0, 0)
 
         view.getLocationOnScreen(viewLocation)
-        view.getSvgView()?.getLocationOnScreen(rootLocation)
-
-        // convert View-relative coordinates into SvgView-relative coordinates
+        highestRoot?.getLocationOnScreen(rootLocation)
+        // all transformations and bounds are relative to the highest-order SvgView
 
         val rootX = posX + viewLocation[0] - rootLocation[0]
         val rootY = posY + viewLocation[1] - rootLocation[1]
 
-        return view.id == view.getSvgView()?.reactTagForTouch(rootX, rootY)
+        val transformed = floatArrayOf(rootX, rootY)
+
+        pointTransformations.reverse()
+
+        for (matrix in pointTransformations) {
+          matrix.mapPoints(transformed)
+        }
+
+        return view.hitTest(transformed) != -1
       }
       return false
     }
