304b17cdf66492793f1c556fda60c0f6a7fb5092
Ignacy Łątka <latkaignacy@gmail.com>
2025-01-08 13:52:49 +0100

    fix onPress and GestureHandler discrepancy

--- android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
+++ android/svg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -1,5 +1,6 @@
 package com.swmansion.gesturehandler
 
+import androidx.core.view.children
 import com.horcrux.svg.SvgView
 import com.horcrux.svg.VirtualView
 
@@ -11,14 +12,40 @@ class RNSVGHitTester {
 
     fun hitTest(view: Any, posX: Float, posY: Float): Boolean {
       if (view is SvgView) {
-        val hasBeenPressed = view.id == view.reactTagForTouch(posX, posY)
+        // get highest-order SvgView
+        var highestOrderSvgView: SvgView = view
+        while (isSvgElement(highestOrderSvgView.parent)) {
+          if (highestOrderSvgView.parent is SvgView) {
+            highestOrderSvgView = highestOrderSvgView.parent as SvgView
+          }
+          if (highestOrderSvgView.parent is VirtualView) {
+            highestOrderSvgView = (highestOrderSvgView.parent as VirtualView).svgView
+          }
+        }
+
+        val viewLocation = intArrayOf(0, 0)
+        val rootLocation = intArrayOf(0, 0)
+
+        view.getLocationOnScreen(viewLocation)
+        highestOrderSvgView.getLocationOnScreen(rootLocation)
+
+        // convert View-relative coordinates into SvgView-relative coordinates
+        val rootX = posX + viewLocation[0] - rootLocation[0]
+        val rootY = posY + viewLocation[1] - rootLocation[1]
+
+        val pressedId = highestOrderSvgView.reactTagForTouch(rootX, rootY)
+        val childrenIds = view.children.map { it.id }
+
+        val hasBeenPressed = view.id == pressedId
+        val hasChildBeenPressed = pressedId in childrenIds
+
         val pressIsInBounds = 0 < posX && posX < view.width && 0 < posY && posY < view.height
-        // todo: add parent traversal, copying the one used for VirtualView doesn't work
-        return hasBeenPressed && pressIsInBounds
+
+        return (hasBeenPressed || hasChildBeenPressed) && pressIsInBounds
       }
 
       if (view is VirtualView) {
-        // get highest-order parent
+        // get highest-order SvgView
         var highestOrderSvgView = view.svgView
         while (isSvgElement(highestOrderSvgView.parent)) {
           if (highestOrderSvgView.parent is SvgView) {
