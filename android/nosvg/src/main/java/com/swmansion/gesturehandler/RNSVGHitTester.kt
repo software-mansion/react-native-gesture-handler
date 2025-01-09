a2c2ffec096f6f2bd98a6da5e80b5347608e1890
Ignacy Łątka <latkaignacy@gmail.com>
2025-01-09 11:22:53 +0100

    simplify no-svg implementation

--- android/nosvg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
+++ android/nosvg/src/main/java/com/swmansion/gesturehandler/RNSVGHitTester.kt
@@ -3,13 +3,9 @@ package com.swmansion.gesturehandler
 class RNSVGHitTester {
   companion object {
     @Suppress("UNUSED_PARAMETER")
-    fun isSvgElement(view: Any): Boolean {
-      return false
-    }
+    fun isSvgElement(view: Any) = false
 
-    @Suppress("UNUSED_PARAMETER", "COMMENT_IN_SUPPRESSION")
-    fun hitTest(view: Any, posX: Float, posY: Float): Boolean {
-      return false // never accessed
-    }
+    @Suppress("UNUSED_PARAMETER")
+    fun hitTest(view: Any, posX: Float, posY: Float) = false
   }
 }
