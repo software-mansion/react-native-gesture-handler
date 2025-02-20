46dba42135395d374538b36f59a5302f2fe73c4c
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-02-20 18:52:51 +0100

    Setup detector to match child's size

--- packages/react-native-gesture-handler/src/NativeDetector.tsx
+++ packages/react-native-gesture-handler/src/NativeDetector.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import type { NativeProps } from './specs/RNGestureHandlerDetectorNativeComponent';
 import RNGestureHandlerDetectorNativeComponent from './specs/RNGestureHandlerDetectorNativeComponent';
+import { StyleSheet } from 'react-native';
 
 export interface NativeDetectorProps extends NativeProps {
   children?: React.ReactNode;
 }
 
-export function NativeDetector({ handlerTags, ...rest }: NativeDetectorProps) {
+export function NativeDetector({ handlerTags, children }: NativeDetectorProps) {
   return (
     <RNGestureHandlerDetectorNativeComponent
       handlerTags={handlerTags}
-      {...rest}
-    />
+      style={styles.detector}>
+      {children}
+    </RNGestureHandlerDetectorNativeComponent>
   );
 }
+
+const styles = StyleSheet.create({
+  detector: {
+    display: 'contents',
+    // TODO: remove, debug info only
+    backgroundColor: 'red',
+  },
+});
