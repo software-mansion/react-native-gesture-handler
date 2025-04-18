e713b5bc3f76572e84a22755ac3ef39e1bae9146
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-04-18 10:08:24 +0200

    Update android

--- packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.cpp
+++ packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.cpp
@@ -26,7 +26,7 @@ namespace gesturehandler {
     jni::local_ref<BindingsInstallerHolder::javaobject> RNGestureHandlerModule::getBindingsInstallerCxx() {
         return jni::make_local(
             BindingsInstallerHolder::newObjectCxxArgs([&](jsi::Runtime& runtime) {
-                this->runtime = &runtime;
+                this->rnRuntime = &runtime;
                 decorateRuntime(runtime);
             })
         );
@@ -38,49 +38,49 @@ namespace gesturehandler {
         method(this->javaPart_, handlerTag, state);
     }
 
-    void RNGestureHandlerModule::decorateRuntime(jsi::Runtime& runtime) {
+    void RNGestureHandlerModule::decorateRuntime(jsi::Runtime& rnRuntime) {
         auto isViewFlatteningDisabled = jsi::Function::createFromHostFunction(
-            runtime,
-            jsi::PropNameID::forAscii(runtime, "isViewFlatteningDisabled"),
+            rnRuntime,
+            jsi::PropNameID::forAscii(rnRuntime, "isViewFlatteningDisabled"),
             1,
             [](jsi::Runtime &runtime,
-               const jsi::Value &thisValue,
-               const jsi::Value *arguments,
+               const jsi::Value &,
+               const jsi::Value *args,
                size_t count) -> jsi::Value {
-                if (!arguments[0].isObject()) {
+                if (!args[0].isObject()) {
                     return jsi::Value::null();
                 }
 
-                auto shadowNode = shadowNodeFromValue(runtime, arguments[0]);
-                bool isViewFlatteningDisabled = shadowNode->getTraits().check(ShadowNodeTraits::FormsStackingContext);
+                const auto shadowNode = shadowNodeFromValue(runtime, args[0]);
+                const auto isViewFlatteningDisabled = shadowNode->getTraits().check(ShadowNodeTraits::FormsStackingContext);
                 
                 // This is done using component names instead of type checking because
                 // of duplicate symbols for RN types, which prevent RTTI from working.
-                const char *componentName = shadowNode->getComponentName();
-                bool isTextComponent = strcmp(componentName, "Paragraph") == 0 ||
-                                       strcmp(componentName, "Text") == 0;
+                const auto componentName = shadowNode->getComponentName();
+                const auto isTextComponent = strcmp(componentName, "Paragraph") == 0 || strcmp(componentName, "Text") == 0;
 
                 return jsi::Value(isViewFlatteningDisabled || isTextComponent);
             });
 
-        runtime.global().setProperty(
-            runtime, "isViewFlatteningDisabled", std::move(isViewFlatteningDisabled));
+        rnRuntime.global().setProperty(
+            rnRuntime, "isViewFlatteningDisabled", std::move(isViewFlatteningDisabled));
     }
 
     void RNGestureHandlerModule::decorateUIRuntime() {
         // TODO: make sure we are on JS?
-        auto arrayBufferValue = runtime->global().getProperty(*runtime, "_WORKLET_RUNTIME").getObject(*runtime).getArrayBuffer(*runtime).data(*runtime);
-        uintptr_t* uiAddr = reinterpret_cast<uintptr_t*>(&arrayBufferValue[0]);
-        jsi::Runtime* uiRuntime = reinterpret_cast<jsi::Runtime*>(*uiAddr);
+        jsi::Runtime &rt = *rnRuntime;
+        const auto arrayBufferValue = rt.global().getProperty(rt, "_WORKLET_RUNTIME").getObject(rt).getArrayBuffer(rt).data(rt);
+        const auto uiRuntimeAddress = reinterpret_cast<uintptr_t*>(&arrayBufferValue[0]);
+        jsi::Runtime &uiRuntime = *reinterpret_cast<jsi::Runtime*>(*uiRuntimeAddress);
 
         auto setGestureStateNew = jsi::Function::createFromHostFunction(
-                *uiRuntime,
-                jsi::PropNameID::forAscii(*uiRuntime, "_setGestureStateNew"),
+                uiRuntime,
+                jsi::PropNameID::forAscii(uiRuntime, "_setGestureStateNew"),
                 2,
-                [&](jsi::Runtime &rt, const jsi::Value &thisValue, const jsi::Value *arguments, size_t count) -> jsi::Value {
+                [&](jsi::Runtime &rt, const jsi::Value &, const jsi::Value *args, size_t count) -> jsi::Value {
                     if (count == 2) {
-                        auto handlerTag = (int)arguments[0].asNumber();
-                        auto state = (int)arguments[1].asNumber();
+                        const auto handlerTag = static_cast<int>(args[0].asNumber());
+                        const auto state = static_cast<int>(args[1].asNumber());
 
                         // TODO: expose to JS and dispatch to UI thread if called on JS?
                         this->setGestureState(handlerTag, state);
@@ -88,6 +88,6 @@ namespace gesturehandler {
                     return jsi::Value::undefined();
                 });
 
-        uiRuntime->global().setProperty(*uiRuntime, "_setGestureStateNew", std::move(setGestureStateNew));
+        uiRuntime.global().setProperty(uiRuntime, "_setGestureStateNew", std::move(setGestureStateNew));
     }
 } // namespace gesturehandler
