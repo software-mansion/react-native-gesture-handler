96de4eaad081f249c3e3473eb2d4660b292e5e56
Michał Bert <michal.bert@swmansion.com>
2025-05-05 14:50:41 +0200

    Fix android formatter paths

--- packages/react-native-gesture-handler/android/fabric/src/main/java/com/swmansion/gesturehandler/ReactContextExtensions.kt
+++ packages/react-native-gesture-handler/android/fabric/src/main/java/com/swmansion/gesturehandler/ReactContextExtensions.kt
@@ -1,11 +1,11 @@
 package com.swmansion.gesturehandler
 
+import android.annotation.SuppressLint
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.fabric.FabricUIManager
 import com.facebook.react.uimanager.UIManagerHelper
 import com.facebook.react.uimanager.common.UIManagerType
 import com.facebook.react.uimanager.events.Event
-import android.annotation.SuppressLint
 
 fun ReactContext.dispatchEvent(event: Event<*>) {
   @SuppressLint("WrongConstant")
