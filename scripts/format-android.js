fd8fb5119c5d8e65aed28b66e87af983262e4c74
Michał Bert <michal.bert@swmansion.com>
2025-05-13 17:35:26 +0200

    Fix android formatter script

--- scripts/format-android.js
+++ scripts/format-android.js
@@ -1,27 +1,31 @@
-/* 
+/*
  * This script is a wrapper for gradle & spotlessApply to make
  * it work properly with lint-staged.
  */
 
-const { exit } = require("process");
-const { exec } = require("child_process");
+const { exit } = require('process');
+const { exec } = require('child_process');
 
-// spotless ktlint formatting task in android/build.gradle
-const spotlessApply = "./android/gradlew -p android spotlessApply";
-
-// takes file as parameter passed by lint-staged (optional)
-const fileName = process.argv[2];
-
-// https://github.com/diffplug/spotless/blob/main/plugin-gradle/IDE_HOOK.md
-// creates file argument without space between arguments
-const fileArgument = `-PspotlessIdeHook=${fileName}`;
-
-const command = fileName !== undefined ? `${spotlessApply} ${fileArgument}` : spotlessApply;
-
-exec(command, (error, stdout) => {
+const log_error = (error, stdout) => {
   if (error) {
     console.log(error);
     console.log(stdout);
     return exit(1);
   }
-});
+};
+
+// spotless ktlint formatting task in android/build.gradle
+const spotlessApply = './android/gradlew -p android spotlessApply';
+
+const argc = process.argv.length;
+
+if (argc >= 2) {
+  for (let i = 2; i < argc; ++i) {
+    const fileArgument = `-PspotlessIdeHook=${process.argv[i]}`;
+    const command = `${spotlessApply} ${fileArgument}`;
+
+    exec(command, log_error);
+  }
+} else {
+  exec(spotlessApply, log_error);
+}
