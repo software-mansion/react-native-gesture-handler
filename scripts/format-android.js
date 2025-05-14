9aead8ee42957041bbe260375329c34ce284e40c
Michał Bert <michal.bert@swmansion.com>
2025-05-14 09:52:50 +0200

    Fix android formatter script

--- scripts/format-android.js
+++ scripts/format-android.js
@@ -5,27 +5,19 @@
 
 const { exit } = require('process');
 const { exec } = require('child_process');
+const path = require('path');
 
-const log_error = (error, stdout) => {
+const androidPath = path.join(
+  __dirname,
+  '../packages/react-native-gesture-handler/android'
+);
+
+const spotlessApply = `${androidPath}/gradlew -p ${androidPath} spotlessApply`;
+
+exec(spotlessApply, (error, stdout) => {
   if (error) {
     console.log(error);
     console.log(stdout);
     return exit(1);
   }
-};
-
-// spotless ktlint formatting task in android/build.gradle
-const spotlessApply = './android/gradlew -p android spotlessApply';
-
-const argc = process.argv.length;
-
-if (argc >= 2) {
-  for (let i = 2; i < argc; ++i) {
-    const fileArgument = `-PspotlessIdeHook=${process.argv[i]}`;
-    const command = `${spotlessApply} ${fileArgument}`;
-
-    exec(command, log_error);
-  }
-} else {
-  exec(spotlessApply, log_error);
-}
+});
