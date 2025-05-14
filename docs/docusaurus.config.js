1dc6a1179a7bcc43d3979be908f56a18b32e1a0b
Patrycja Kalinska <cysiak0412@gmail.com>
2025-05-14 12:09:41 +0200

    add bash to supported langs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -113,6 +113,7 @@ const config = {
           'All trademarks and copyrights belong to their respective owners.',
       },
       prism: {
+        additionalLanguages: ['bash'],
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
