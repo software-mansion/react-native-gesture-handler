184a72b803f4d9e3eb213711d4bc774e649c9705
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-05-27 12:42:06 +0200

    Format c++

--- packages/react-native-gesture-handler/shared/RuntimeDecorator.h
+++ packages/react-native-gesture-handler/shared/RuntimeDecorator.h
@@ -1,12 +1,16 @@
 #pragma once
 
 namespace gesturehandler {
-    using namespace facebook;
-    using namespace facebook::react;
+using namespace facebook;
+using namespace facebook::react;
 
-    class RuntimeDecorator {
-    public:
-        static void installJSRuntimeBindings(jsi::Runtime& rnRuntime, std::function<void(int, int)>&& setGestureState);
-        static bool installUIRuntimeBindings(jsi::Runtime& rnRuntime, std::function<void(int, int)>&& setGestureState);
-    };
+class RuntimeDecorator {
+ public:
+  static void installJSRuntimeBindings(
+      jsi::Runtime &rnRuntime,
+      std::function<void(int, int)> &&setGestureState);
+  static bool installUIRuntimeBindings(
+      jsi::Runtime &rnRuntime,
+      std::function<void(int, int)> &&setGestureState);
+};
 } // namespace gesturehandler
