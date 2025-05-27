03e88a2e6f785d41c7e1f4709c332bdda3e5dd6d
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-05-27 12:42:06 +0200

    Format c++

--- packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.cpp
+++ packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.cpp
@@ -1,50 +1,57 @@
-#include <react/renderer/uimanager/primitives.h>
 #include <RuntimeDecorator.h>
+#include <react/renderer/uimanager/primitives.h>
 
 #include "RNGestureHandlerModule.h"
 
 namespace gesturehandler {
-    using namespace facebook;
-    using namespace facebook::react;
+using namespace facebook;
+using namespace facebook::react;
 
-    RNGestureHandlerModule::RNGestureHandlerModule(jni::alias_ref<RNGestureHandlerModule::javaobject> jThis)
-            : javaPart_(jni::make_global(jThis)) {
-    }
+RNGestureHandlerModule::RNGestureHandlerModule(
+    jni::alias_ref<RNGestureHandlerModule::javaobject> jThis)
+    : javaPart_(jni::make_global(jThis)) {}
 
-    RNGestureHandlerModule::~RNGestureHandlerModule() {}
+RNGestureHandlerModule::~RNGestureHandlerModule() {}
 
-    void RNGestureHandlerModule::registerNatives() {
-        registerHybrid(
-                {makeNativeMethod("initHybrid", RNGestureHandlerModule::initHybrid),
-                 makeNativeMethod("getBindingsInstallerCxx", RNGestureHandlerModule::getBindingsInstallerCxx),
-                 makeNativeMethod("decorateUIRuntime", RNGestureHandlerModule::decorateUIRuntime)});
-    }
+void RNGestureHandlerModule::registerNatives() {
+  registerHybrid(
+      {makeNativeMethod("initHybrid", RNGestureHandlerModule::initHybrid),
+       makeNativeMethod(
+           "getBindingsInstallerCxx",
+           RNGestureHandlerModule::getBindingsInstallerCxx),
+       makeNativeMethod(
+           "decorateUIRuntime", RNGestureHandlerModule::decorateUIRuntime)});
+}
 
-    jni::local_ref<RNGestureHandlerModule::jhybriddata> RNGestureHandlerModule::initHybrid(jni::alias_ref<jhybridobject> jThis) {
-        return makeCxxInstance(jThis);
-    }
+jni::local_ref<RNGestureHandlerModule::jhybriddata>
+RNGestureHandlerModule::initHybrid(jni::alias_ref<jhybridobject> jThis) {
+  return makeCxxInstance(jThis);
+}
 
-    jni::local_ref<BindingsInstallerHolder::javaobject> RNGestureHandlerModule::getBindingsInstallerCxx() {
-        return jni::make_local(
-            BindingsInstallerHolder::newObjectCxxArgs([&](jsi::Runtime& runtime) {
-                this->rnRuntime = &runtime;
-                RuntimeDecorator::installJSRuntimeBindings(runtime, [&](int handlerTag, int state) {
-                    this->setGestureState(handlerTag, state);
-                });
-            })
-        );
-    }
+jni::local_ref<BindingsInstallerHolder::javaobject>
+RNGestureHandlerModule::getBindingsInstallerCxx() {
+  return jni::make_local(
+      BindingsInstallerHolder::newObjectCxxArgs([&](jsi::Runtime &runtime) {
+        this->rnRuntime = &runtime;
+        RuntimeDecorator::installJSRuntimeBindings(
+            runtime, [&](int handlerTag, int state) {
+              this->setGestureState(handlerTag, state);
+            });
+      }));
+}
 
-    void RNGestureHandlerModule::setGestureState(int handlerTag, int state) {
-        static const auto method = javaClassLocal()->getMethod<void(jint, jint)>("setGestureHandlerState");
+void RNGestureHandlerModule::setGestureState(int handlerTag, int state) {
+  static const auto method =
+      javaClassLocal()->getMethod<void(jint, jint)>("setGestureHandlerState");
 
-        method(this->javaPart_, handlerTag, state);
-    }
+  method(this->javaPart_, handlerTag, state);
+}
 
-    bool RNGestureHandlerModule::decorateUIRuntime() {
-        // TODO: make sure we are on JS?
-        return RuntimeDecorator::installUIRuntimeBindings(*rnRuntime, [&](int handlerTag, int state) {
-          this->setGestureState(handlerTag, state);
-        });
-    }
+bool RNGestureHandlerModule::decorateUIRuntime() {
+  // TODO: make sure we are on JS?
+  return RuntimeDecorator::installUIRuntimeBindings(
+      *rnRuntime, [&](int handlerTag, int state) {
+        this->setGestureState(handlerTag, state);
+      });
+}
 } // namespace gesturehandler
--- packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.h
+++ packages/react-native-gesture-handler/android/src/main/jni/RNGestureHandlerModule.h
@@ -4,28 +4,30 @@
 #include <string>
 
 namespace gesturehandler {
-    using namespace facebook;
-    using namespace facebook::jni;
-    using namespace facebook::react;
+using namespace facebook;
+using namespace facebook::jni;
+using namespace facebook::react;
 
-    class RNGestureHandlerModule : public jni::HybridClass<RNGestureHandlerModule> {
-    public:
-        static auto constexpr kJavaDescriptor =
-                "Lcom/swmansion/gesturehandler/react/RNGestureHandlerModule;";
-        static jni::local_ref<jhybriddata> initHybrid(jni::alias_ref<jhybridobject> jThis);
-        static void registerNatives();
-        ~RNGestureHandlerModule();
+class RNGestureHandlerModule : public jni::HybridClass<RNGestureHandlerModule> {
+ public:
+  static auto constexpr kJavaDescriptor =
+      "Lcom/swmansion/gesturehandler/react/RNGestureHandlerModule;";
+  static jni::local_ref<jhybriddata> initHybrid(
+      jni::alias_ref<jhybridobject> jThis);
+  static void registerNatives();
+  ~RNGestureHandlerModule();
 
-    private:
-        friend HybridBase;
-        jsi::Runtime* rnRuntime = nullptr;
+ private:
+  friend HybridBase;
+  jsi::Runtime *rnRuntime = nullptr;
 
-        jni::global_ref<RNGestureHandlerModule::javaobject> javaPart_;
-        explicit RNGestureHandlerModule(jni::alias_ref<RNGestureHandlerModule::javaobject> jThis);
-        jni::local_ref<BindingsInstallerHolder::javaobject> getBindingsInstallerCxx();
+  jni::global_ref<RNGestureHandlerModule::javaobject> javaPart_;
+  explicit RNGestureHandlerModule(
+      jni::alias_ref<RNGestureHandlerModule::javaobject> jThis);
+  jni::local_ref<BindingsInstallerHolder::javaobject> getBindingsInstallerCxx();
 
-        void setGestureState(int handlerTag, int state);
-        void decorateRuntime(jsi::Runtime& runtime);
-        bool decorateUIRuntime();
-    };
+  void setGestureState(int handlerTag, int state);
+  void decorateRuntime(jsi::Runtime &runtime);
+  bool decorateUIRuntime();
+};
 } // namespace gesturehandler
