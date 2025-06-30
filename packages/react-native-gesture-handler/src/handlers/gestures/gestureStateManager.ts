71f3699e8af65dd0e0a64ea3f94ca85ecd42bb5e
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-06-30 15:44:07 +0200

    Throw error when no methods available

--- packages/react-native-gesture-handler/src/handlers/gestures/gestureStateManager.ts
+++ packages/react-native-gesture-handler/src/handlers/gestures/gestureStateManager.ts
@@ -1,4 +1,5 @@
 import { State } from '../../State';
+import { tagMessage } from '../../utils';
 
 export interface GestureStateManagerType {
   begin: () => void;
@@ -20,6 +21,8 @@ const wrappedSetGestureState = (handlerTag: number, state: State) => {
     globalThis._setGestureStateSync(handlerTag, state);
   } else if (globalThis._setGestureStateAsync) {
     globalThis._setGestureStateAsync(handlerTag, state);
+  } else {
+    throw new Error(tagMessage('Failed to set gesture state'));
   }
 };
 
