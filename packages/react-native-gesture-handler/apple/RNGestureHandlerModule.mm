3dbaa1612f2a0be5d3b58548141d689b324e7dc6
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-07-01 12:53:54 +0200

    Run createGestureHandler on JS thread

--- packages/react-native-gesture-handler/apple/RNGestureHandlerModule.mm
+++ packages/react-native-gesture-handler/apple/RNGestureHandlerModule.mm
@@ -152,7 +152,9 @@ RCT_EXPORT_MODULE()
   });
 }
 
-- (NSNumber *)createGestureHandler:(NSString *)handlerName handlerTag:(double)handlerTag config:(NSDictionary *)config
+- (NSNumber *)createGestureHandler:(NSString *)handlerName
+                        handlerTag:(double)handlerTag
+                            config:(NSDictionary *)config
 {
   if (!_checkedIfReanimatedIsAvailable) {
     _isReanimatedAvailable = [self.moduleRegistry moduleForName:"ReanimatedModule"] != nil;
@@ -165,7 +167,7 @@ RCT_EXPORT_MODULE()
   [self addOperationBlock:^(RNGestureHandlerManager *manager) {
     [manager createGestureHandler:handlerName tag:[NSNumber numberWithDouble:handlerTag] config:config];
   }];
-
+  
   return @1;
 }
 
