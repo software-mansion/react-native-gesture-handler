d933194abcd5b0496272cb8b759b063a09a5912a
Andrzej Antoni Kwaśniewski <81448793+akwasniewski@users.noreply.github.com>
2025-07-18 10:32:13 +0200

    Dropping handlers when components are dropped (#3619)
    ## Description
    
    Fixes #3608. On web, when gesture was active and its component had been
    dropped, the gesture was not dropped. This caused conflicts with other
    handlers, and prevented registering other gestures.
    I added a method to drop handlers in the `GestureOchestrator` and call
    it when component is dropped.
    
    
    -->
    
    ## Test plan
    ```ts
    import React from 'react';
    import { useState } from 'react';
    import { Pressable, TextInput } from 'react-native-gesture-handler';
    export default function EmptyExample() {
      const [shown, setShown] = useState(true)
    
      if (!shown) {
        return (
          <Pressable
            key="1"
            testID="other-pressable"
            style={{ width: 30, height: 30, backgroundColor: 'red' }}
            onPress={() => console.log('pressed')}
          />
        )
      }
    
      return (
        <Pressable key="2" testID="bad-pressable" onPress={() => { }}>
          <TextInput
            style={{ backgroundColor: 'green', width: 100, height: 30 }}
            onSubmitEditing={() => setShown(false)}
          />
        </Pressable>
      )
    };
    ```

--- packages/react-native-gesture-handler/src/web/handlers/GestureHandler.ts
+++ packages/react-native-gesture-handler/src/web/handlers/GestureHandler.ts
@@ -727,6 +727,7 @@ export default abstract class GestureHandler implements IGestureHandler {
   protected resetConfig(): void {}
 
   public onDestroy(): void {
+    GestureHandlerOrchestrator.instance.removeHandlerFromOrchestrator(this);
     this.delegate.destroy(this.config);
   }
 
