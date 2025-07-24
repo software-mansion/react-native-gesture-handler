8c445f6f3d51d039e6b22d9c497060e0e9265662
Michał <michal.bert@swmansion.com>
2025-07-24 10:14:30 +0200

    Beautify ruby

--- scripts/gesture_handler_utils.rb
+++ scripts/gesture_handler_utils.rb
@@ -1,10 +1,12 @@
 def try_to_parse_react_native_package_json(node_modules_dir)
     react_native_package_json_path = File.join(node_modules_dir, 'react-native/package.json')
+
     if !File.exist?(react_native_package_json_path)
-      return nil
+        return nil
     end
+
     return JSON.parse(File.read(react_native_package_json_path))
-  end
+end
 
 def get_react_native_minor_version()
     react_native_node_modules_dir = File.join(File.dirname(`cd "#{Pod::Config.instance.installation_root.to_s}" && node --print "require.resolve('react-native/package.json')"`), '..')
@@ -13,11 +15,11 @@ def get_react_native_minor_version()
     if react_native_json == nil
         node_modules_dir = ENV["REACT_NATIVE_NODE_MODULES_DIR"]
         react_native_json = try_to_parse_react_native_package_json(node_modules_dir)
-      end
+    end
 
     if react_native_json == nil
         raise '[react-native-gesture-handler] Unable to recognize your `react-native` version. Please set environmental variable with `react-native` location: `export REACT_NATIVE_NODE_MODULES_DIR="<path to react-native>" && pod install`.'
-      end
+    end
 
     return react_native_json['version'].split('.')[1].to_i
 end
