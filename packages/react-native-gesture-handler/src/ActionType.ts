cc78f74023c013b966a67d1cbe54d3a20e1f9032
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2025-07-28 12:39:50 +0200

    native detector animated event action type

--- packages/react-native-gesture-handler/src/ActionType.ts
+++ packages/react-native-gesture-handler/src/ActionType.ts
@@ -4,6 +4,7 @@ export const ActionType = {
   JS_FUNCTION_OLD_API: 3,
   JS_FUNCTION_NEW_API: 4,
   NATIVE_DETECTOR: 5,
+  NATIVE_DETECTOR_ANIMATED_EVENT: 6,
 } as const;
 
 // eslint-disable-next-line @typescript-eslint/no-redeclare -- backward compatibility; it can be used as a type and as a value
