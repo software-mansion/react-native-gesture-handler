1cd4bf19dfcad8b201f881bebe3a71ecafce9896
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2025-08-07 14:35:17 +0200

    forAnimated defaults to false

--- packages/react-native-gesture-handler/src/web/handlers/GestureHandler.ts
+++ packages/react-native-gesture-handler/src/web/handlers/GestureHandler.ts
@@ -33,7 +33,7 @@ export default abstract class GestureHandler implements IGestureHandler {
   private viewRef: number | null = null;
   private propsRef: React.RefObject<PropsRef> | null = null;
   private actionType: ActionType | null = null;
-  private forAnimated: boolean | null = null;
+  private forAnimated: boolean = false;
   private _handlerTag!: number;
   private _config: Config = { enabled: false };
 
@@ -82,7 +82,7 @@ export default abstract class GestureHandler implements IGestureHandler {
     this.viewRef = null;
     this.actionType = null;
     this.state = State.UNDETERMINED;
-    this.forAnimated = null;
+    this.forAnimated = false;
 
     this.delegate.detach();
   }
