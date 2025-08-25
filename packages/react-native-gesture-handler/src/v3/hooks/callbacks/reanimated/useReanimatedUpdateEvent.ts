fda23cc2a209ea3a717b79c7176b2b107147a3d2
Michał <michal.bert@swmansion.com>
2025-08-25 15:01:28 +0200

    Fix crash when onUpdate is Animated.Event

--- packages/react-native-gesture-handler/src/v3/hooks/callbacks/reanimated/useReanimatedUpdateEvent.ts
+++ packages/react-native-gesture-handler/src/v3/hooks/callbacks/reanimated/useReanimatedUpdateEvent.ts
@@ -5,6 +5,17 @@ import { onGestureHandlerEvent } from '../onGestureHandlerEvent';
 export function useReanimatedUpdateEvent(handlerTag: number, config: any) {
   const { handlers, changeEventCalculator } = extractUpdateHandlers(config);
 
+  // We don't want to call hooks conditionally, therefore `useHandler` and `useEvent` will be always called.
+  // The only difference is whether we will send events to Reanimated or not.
+  // The problem here is that if someone passes `Animated.event` as `onUpdate` prop,
+  // it won't be workletized and therefore `useHandler` will throw. In that case we override it to empty `worklet`.
+  if (!Reanimated?.isWorkletFunction(handlers.onUpdate)) {
+    handlers.onUpdate = () => {
+      'worklet';
+      // no-op
+    };
+  }
+
   const reanimatedHandler = Reanimated?.useHandler(handlers);
 
   const callback = onGestureHandlerEvent(
