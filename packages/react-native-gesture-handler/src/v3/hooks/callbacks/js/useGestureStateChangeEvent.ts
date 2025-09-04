fb88c8cdf660982114e34cab96ad7a49d9370b0a
Michał <michal.bert@swmansion.com>
2025-09-04 10:52:13 +0200

    Change hook eslint rule to warning

--- packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureStateChangeEvent.ts
+++ packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureStateChangeEvent.ts
@@ -1,7 +1,6 @@
 import { extractStateChangeHandlers } from '../../utils';
 import { getGestureHandlerStateChangeWorkletHandler } from '../GestureHandlerStateChangeWorkletHandler';
 
-// eslint-disable-next-line @eslint-react/hooks-extra/ensure-custom-hooks-using-other-hooks
 export function useGestureStateChangeEvent(handlerTag: number, config: any) {
   const handlers = extractStateChangeHandlers(config);
 
--- packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureTouchEvent.ts
+++ packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureTouchEvent.ts
@@ -1,7 +1,6 @@
 import { extractTouchHandlers } from '../../utils';
 import { getGestureHandlerTouchEventWorkletHandler } from '../GestureHandlerTouchEventWorkletHandler';
 
-// eslint-disable-next-line @eslint-react/hooks-extra/ensure-custom-hooks-using-other-hooks
 export function useGestureTouchEvent(handlerTag: number, config: any) {
   const handlers = extractTouchHandlers(config);
 
