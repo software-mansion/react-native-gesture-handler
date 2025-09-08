4e718b3baae9920c72695405505b36d56fbe5250
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2025-09-08 14:18:30 +0200

    merge fix

--- packages/react-native-gesture-handler/apple/RNGestureHandlerNativeEventUtils.h
+++ packages/react-native-gesture-handler/apple/RNGestureHandlerNativeEventUtils.h
@@ -5,22 +5,16 @@
 @interface RNGestureHandlerEvent (NativeEvent)
 
 - (facebook::react::RNGestureHandlerDetectorEventEmitter::OnGestureHandlerEvent)getNativeEvent;
-<<<<<<< HEAD
-=======
 
 - (facebook::react::RNGestureHandlerDetectorEventEmitter::OnGestureHandlerReanimatedEvent)getReanimatedNativeEvent;
 
->>>>>>> next
 @end
 
 @interface RNGestureHandlerStateChange (NativeEvent)
 
 - (facebook::react::RNGestureHandlerDetectorEventEmitter::OnGestureHandlerStateChange)getNativeEvent;
-<<<<<<< HEAD
-=======
 
 - (facebook::react::RNGestureHandlerDetectorEventEmitter::OnGestureHandlerReanimatedStateChange)
     getReanimatedNativeEvent;
 
->>>>>>> next
 @end
