0a672ee1772e3b32450a014beb8391723ad16286
Michał <michal.bert@swmansion.com>
2025-09-16 11:39:09 +0200

    Remove unnecessary cast

--- packages/react-native-gesture-handler/src/v3/NativeDetector/NativeDetector.tsx
+++ packages/react-native-gesture-handler/src/v3/NativeDetector/NativeDetector.tsx
@@ -21,8 +21,7 @@ const ReanimatedNativeDetector =
 export function NativeDetector({ gesture, children }: NativeDetectorProps) {
   const NativeDetectorComponent = gesture.config.dispatchesAnimatedEvents
     ? AnimatedNativeDetector
-    : // TODO: Remove this cast when we properly type config
-      (gesture.config.shouldUseReanimated as boolean)
+    : gesture.config.shouldUseReanimated
       ? ReanimatedNativeDetector
       : HostGestureDetector;
 
