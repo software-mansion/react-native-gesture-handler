4b3451d439d56140b3b3a4e0d497de4fe88a15c2
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2025-10-06 10:50:26 +0200

    update types in tests and examples

--- apps/common-app/src/new_api/drag_n_drop/Draggable.tsx
+++ apps/common-app/src/new_api/drag_n_drop/Draggable.tsx
@@ -4,8 +4,8 @@ import {
   PanGestureHandlerEventPayload,
   Gesture,
   GestureDetector,
-  PanGesture,
-  TapGesture,
+  PanGestureType,
+  TapGestureType,
 } from 'react-native-gesture-handler';
 import Animated, { runOnJS, useAnimatedStyle } from 'react-native-reanimated';
 
@@ -22,8 +22,8 @@ interface DraggableProps {
   isActive: boolean;
   translation: AnimatedPostion;
   position: { x: number; y: number };
-  dragGesture: PanGesture;
-  tapEndGesture: TapGesture;
+  dragGesture: PanGestureType;
+  tapEndGesture: TapGestureType;
   tileSize: number;
   rowGap: number;
   columnGap: number;
