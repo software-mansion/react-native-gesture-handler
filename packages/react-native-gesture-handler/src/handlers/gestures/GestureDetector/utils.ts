7fcbcc68df6b2fe2576c3a88ff25ba372cc51d3d
Andrzej Antoni Kwaśniewski <81448793+akwasniewski@users.noreply.github.com>
2025-10-23 11:35:15 +0200

    Fix SSR error with `useLayoutEffect` (#3752)
    ## Description
    Using `useLayoutEffect` in combination with SSR results in a warning.
    This PR adds
    [isomorphicLayoutEffect](https://github.com/reduxjs/react-redux/blob/7e2fdd4ee2021e4282e12ba9fc722f09124e30cd/src/utils/useIsomorphicLayoutEffect.ts#L36),
    which acts as a `useEffect` when running on server and as
    `useLayoutEffect` everywhere else. This fixes
    https://github.com/software-mansion/react-native-gesture-handler/issues/3375.
    
    Note: This may cause #2920 to happen when running SSR, however as we
    couldn't replicate the issue on main, probably due to same changes to
    view recycling within react native, I think it would even be ok to
    simply revert
    https://github.com/software-mansion/react-native-gesture-handler/pull/2925,
    and simply only `useEffect`, so it shouldn't be an issue.
    
    I decided to use `isomorphicLayoutEffect` instead of reverting to ensure
    correctness.
    
    ## Test plan
    
    See that there is no warning mentioned in #3375

--- packages/react-native-gesture-handler/src/handlers/gestures/GestureDetector/utils.ts
+++ packages/react-native-gesture-handler/src/handlers/gestures/GestureDetector/utils.ts
@@ -15,7 +15,7 @@ import { hoverGestureHandlerProps } from '../hoverGesture';
 import { nativeViewGestureHandlerProps } from '../../NativeViewGestureHandler';
 import { baseGestureHandlerWithDetectorProps } from '../../gestureHandlerCommon';
 import { RNRenderer } from '../../../RNRenderer';
-import { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { Reanimated } from '../reanimatedWrapper';
 import { onGestureHandlerEvent } from '../eventReceiver';
 import {
@@ -180,3 +180,21 @@ export function useWebEventHandlers() {
     },
   });
 }
+
+// code below is modified version of the code found in:
+// https://github.com/reduxjs/react-redux/blob/7e2fdd4ee2021e4282e12ba9fc722f09124e30cd/src/utils/useIsomorphicLayoutEffect.ts#L36
+// React currently throws a warning when using useLayoutEffect on the server.
+// To get around it, we can conditionally useEffect on the server (no-op) and
+// useLayoutEffect in the browser.
+const isDOM = !!(
+  typeof window !== 'undefined' &&
+  typeof window.document !== 'undefined' &&
+  typeof window.document.createElement !== 'undefined'
+);
+
+// Under React Native, we know that we always want to use useLayoutEffect
+const isReactNative =
+  typeof navigator !== 'undefined' && navigator.product === 'ReactNative';
+
+export const useIsomorphicLayoutEffect =
+  isDOM || isReactNative ? React.useLayoutEffect : React.useEffect;
