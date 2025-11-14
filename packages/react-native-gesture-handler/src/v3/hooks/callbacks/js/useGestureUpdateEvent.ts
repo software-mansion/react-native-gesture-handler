a2216e2e01ff7036b95f3d0d31e9f5ce36249122
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-11-14 09:35:07 +0100

    Memoize internal event handlers (#3812)
    ## Description
    
    - Our non-reanimated "hooks" weren't memoized by the compiler since they
    weren't actually hooks. Due to this, their result was never
    referentially stable.
    - Since we're doing manual memoization inside `useGesture`, it also
    wasn't memoized automatically, and its result wasn't referentially
    stable.
    - Creation of gestures inside `useMemo` was failing with the compiler
    enabled, since it was inlining its content outside of memo.
    
    ## Test plan
    
    Check the basic example. Enable [React
    compiler](https://react.dev/reference/react-compiler/compiling-libraries#setting-up-compilation)
    and see that the app no longer crashes on rerenders.

--- packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureUpdateEvent.ts
+++ packages/react-native-gesture-handler/src/v3/hooks/callbacks/js/useGestureUpdateEvent.ts
@@ -2,18 +2,26 @@ import { extractUpdateHandlers, isAnimatedEvent } from '../../utils';
 import { ReanimatedContext } from '../../../../handlers/gestures/reanimatedWrapper';
 import { getUpdateHandler } from '../updateHandler';
 import { BaseGestureConfig } from '../../../types';
+import { useMemo } from 'react';
 
 export function useGestureUpdateEvent<THandlerData, TConfig>(
   handlerTag: number,
   config: BaseGestureConfig<THandlerData, TConfig>
 ) {
-  const { handlers, changeEventCalculator } = extractUpdateHandlers(config);
+  return useMemo(() => {
+    const { handlers, changeEventCalculator } = extractUpdateHandlers(config);
 
-  const jsContext: ReanimatedContext<THandlerData> = {
-    lastUpdateEvent: undefined,
-  };
+    const jsContext: ReanimatedContext<THandlerData> = {
+      lastUpdateEvent: undefined,
+    };
 
-  return isAnimatedEvent(config.onUpdate)
-    ? undefined
-    : getUpdateHandler(handlerTag, handlers, jsContext, changeEventCalculator);
+    return isAnimatedEvent(config.onUpdate)
+      ? undefined
+      : getUpdateHandler(
+          handlerTag,
+          handlers,
+          jsContext,
+          changeEventCalculator
+        );
+  }, [handlerTag, config]);
 }
