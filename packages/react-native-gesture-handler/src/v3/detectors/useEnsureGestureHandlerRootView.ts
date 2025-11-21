93de5e1ebe2fe8a2fd024371b8f6571a254321c3
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-11-21 13:37:31 +0100

    [General] Make `InterceptingGestureDetector` throw when not rendered under GHRootView (#3829)
    ## Description
    
    Makes `InterceptingGestureDetector` throw when not rendered underneath
    `GestureHandlerRootView`, the same as the normal `GestureDetector` does.
    
    ## Test plan
    
    Render `InterceptingGestureDetector` without root view

---
+++ packages/react-native-gesture-handler/src/v3/detectors/useEnsureGestureHandlerRootView.ts
@@ -0,0 +1,14 @@
+import { use } from 'react';
+import { isTestEnv } from '../../utils';
+import { Platform } from 'react-native';
+import GestureHandlerRootViewContext from '../../GestureHandlerRootViewContext';
+
+export function useEnsureGestureHandlerRootView() {
+  const rootViewContext = use(GestureHandlerRootViewContext);
+
+  if (__DEV__ && !rootViewContext && !isTestEnv() && Platform.OS !== 'web') {
+    throw new Error(
+      'GestureDetector must be used as a descendant of GestureHandlerRootView. Otherwise the gestures will not be recognized. See https://docs.swmansion.com/react-native-gesture-handler/docs/fundamentals/installation for more details.'
+    );
+  }
+}
