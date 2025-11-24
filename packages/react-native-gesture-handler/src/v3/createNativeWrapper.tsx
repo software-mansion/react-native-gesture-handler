23403fa66bdcddcefa3dbe11807221f0f44b0d70
Michał <michal.bert@swmansion.com>
2025-11-24 15:12:58 +0100

    Remove testID from cnw

--- packages/react-native-gesture-handler/src/v3/createNativeWrapper.tsx
+++ packages/react-native-gesture-handler/src/v3/createNativeWrapper.tsx
@@ -39,7 +39,6 @@ export default function createNativeWrapper<P>(
         childProps: {
           enabled: props.enabled,
           hitSlop: props.hitSlop,
-          // testID: props.testID,
         } as P,
       }
     );
