a4f1074851db053969fd2e18bf590dd0ec59db93
Jakub Piasecki <jakub.piasecki@swmansion.com>
2025-11-25 15:53:19 +0100

    Rename gesture composition hooks (#3834)
    ## Description
    
    - Renames composition hooks to include `Gestures` suffix.
    - Renames `useRace` to `useMultipleGestures`
    - `useRaceGesture`/`useRacingGestures` suggests that it sets up some
    kind of relation between gestures. It doesn't - this is the default
    behavior. I hope that `useMultipleGestures` sounds like the answer to "I
    just want to attach pan and pinch to the same view", or "I want to set
    the relations manually because they depend on other components".
    - Changes the directory they are in from `relations` to `composition`.
    - Removes `useGesture` from the public API
    
    ## Test plan
    
    Static checks

--- packages/react-native-gesture-handler/src/v3/hooks/composition/useMultipleGestures.ts
+++ packages/react-native-gesture-handler/src/v3/hooks/composition/useMultipleGestures.ts
@@ -1,6 +1,6 @@
 import { AnyGesture, ComposedGestureName } from '../../types';
 import { useComposedGesture } from './useComposedGesture';
 
-export function useRace(...gestures: AnyGesture[]) {
+export function useMultipleGestures(...gestures: AnyGesture[]) {
   return useComposedGesture(ComposedGestureName.Race, ...gestures);
 }
