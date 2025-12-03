71ab1499bdecb4710c8e0dc2ff83c23e711a18c4
Michał Bert <63123542+m-bert@users.noreply.github.com>
2025-12-03 17:05:50 +0100

    Change components implementation (#3800)
    ## Description
    
    This PR changes components implementations to use new hooks API.
    
    > [!WARNING]
    > It requires changes in module on iOS - call `createGestureHandler` immediately, not adding operation block. 
    
    ## Test plan
    
    Example apps

--- packages/react-native-gesture-handler/src/v3/detectors/index.ts
+++ packages/react-native-gesture-handler/src/v3/detectors/index.ts
@@ -2,3 +2,9 @@ export type { GestureDetectorProps } from './common';
 export { GestureDetector } from './GestureDetector';
 export { VirtualDetector as VirtualGestureDetector } from './VirtualDetector/VirtualDetector';
 export { InterceptingGestureDetector } from './VirtualDetector/InterceptingGestureDetector';
+
+export enum DetectorType {
+  Native,
+  Virtual,
+  Intercepting,
+}
