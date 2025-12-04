88b9edec1adc8258714638fa79c8c17605fb6ae0
Michał <michal.bert@swmansion.com>
2025-12-04 10:54:43 +0100

    Explicit exports

--- packages/react-native-gesture-handler/src/v3/index.ts
+++ packages/react-native-gesture-handler/src/v3/index.ts
@@ -1,5 +1,74 @@
-export * from './detectors';
-export * from './hooks';
-export * from './components';
+export type { GestureDetectorProps } from './detectors';
+export {
+  GestureDetector,
+  VirtualGestureDetector,
+  InterceptingGestureDetector,
+  DetectorType,
+} from './detectors';
+
+export type {
+  TapGesture,
+  TapGestureConfig,
+  TapGestureEvent,
+  FlingGesture,
+  FlingGestureConfig,
+  FlingGestureEvent,
+  LongPressGesture,
+  LongPressGestureConfig,
+  LongPressGestureEvent,
+  PinchGesture,
+  PinchGestureConfig,
+  PinchGestureEvent,
+  RotationGesture,
+  RotationGestureConfig,
+  RotationGestureEvent,
+  HoverGesture,
+  HoverGestureConfig,
+  HoverGestureEvent,
+  ManualGesture,
+  ManualGestureConfig,
+  ManualGestureEvent,
+  NativeGesture,
+  NativeViewGestureConfig,
+  NativeGestureEvent,
+  PanGesture,
+  PanGestureConfig,
+  PanGestureEvent,
+  SingleGesture,
+  SingleGestureEvent,
+} from './hooks';
+export {
+  useSimultaneousGestures,
+  useExclusiveGestures,
+  useCompetingGestures,
+  useTapGesture,
+  useFlingGesture,
+  useLongPressGesture,
+  usePinchGesture,
+  useRotationGesture,
+  useHoverGesture,
+  useManualGesture,
+  useNativeGesture,
+  usePanGesture,
+} from './hooks';
+
+export type {
+  RawButtonProps,
+  BaseButtonProps,
+  RectButtonProps,
+  BorderlessButtonProps,
+} from './components';
+export {
+  RawButton,
+  BaseButton,
+  RectButton,
+  BorderlessButton,
+  PureNativeButton,
+  ScrollView,
+  Switch,
+  TextInput,
+  FlatList,
+  RefreshControl,
+} from './components';
 
 export type { ComposedGesture } from './types';
