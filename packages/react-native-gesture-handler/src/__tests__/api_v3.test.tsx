6afb3788e0a04dbe959873cc803df518a167f83e
Michał Bert <63123542+m-bert@users.noreply.github.com>
2025-12-08 16:37:14 +0100

    Handle `testID` prop (#3838)
    ## Description
    
    This PR adds handling of `testID` property.
    
    ## Test plan
    
    `yarn test api_v3`
    
    ---------
    
    Co-authored-by: Jakub Piasecki <jakub.piasecki@swmansion.com>

---
+++ packages/react-native-gesture-handler/src/__tests__/api_v3.test.tsx
@@ -0,0 +1,60 @@
+import { usePanGesture } from '../v3/hooks/gestures';
+import { render, renderHook } from '@testing-library/react-native';
+import { fireGestureHandler, getByGestureTestId } from '../jestUtils';
+import { State } from '../State';
+import GestureHandlerRootView from '../components/GestureHandlerRootView';
+import { RectButton } from '../v3/components';
+import { act } from 'react';
+
+describe('[API v3] Hooks', () => {
+  test('Pan gesture', () => {
+    const onBegin = jest.fn();
+    const onStart = jest.fn();
+
+    const panGesture = renderHook(() =>
+      usePanGesture({
+        disableReanimated: true,
+        onBegin: (e) => onBegin(e),
+        onActivate: (e) => onStart(e),
+      })
+    ).result.current;
+
+    fireGestureHandler(panGesture, [
+      { oldState: State.UNDETERMINED, state: State.BEGAN },
+      { oldState: State.BEGAN, state: State.ACTIVE },
+      { oldState: State.ACTIVE, state: State.ACTIVE },
+      { oldState: State.ACTIVE, state: State.END },
+    ]);
+
+    expect(onBegin).toHaveBeenCalledTimes(1);
+    expect(onStart).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('[API v3] Components', () => {
+  test('Rect Button', () => {
+    const pressFn = jest.fn();
+
+    const RectButtonExample = () => {
+      return (
+        <GestureHandlerRootView>
+          <RectButton testID="btn" onPress={pressFn} />
+        </GestureHandlerRootView>
+      );
+    };
+
+    render(<RectButtonExample />);
+
+    const nativeGesture = getByGestureTestId('btn');
+
+    act(() => {
+      fireGestureHandler(nativeGesture, [
+        { oldState: State.UNDETERMINED, state: State.BEGAN },
+        { oldState: State.BEGAN, state: State.ACTIVE },
+        { oldState: State.ACTIVE, state: State.END },
+      ]);
+    });
+
+    expect(pressFn).toHaveBeenCalledTimes(1);
+  });
+});
