b2d6281cb9218a9f67ea50c3406b75c42f99fdc2
Andrzej Antoni Kwaśniewski <81448793+akwasniewski@users.noreply.github.com>
2026-01-07 09:17:16 +0100

    State Manager (#3880)
    ## Description
    
    Implements V3 State Manager, to manually manage gestures.
    
    ## Test plan
    
    Tested on the following example, the long press should activate
    instantely, while the minDuration is extremely big.
    <details>
    
    ```ts
    import React from 'react';
    import { View, Text, StyleSheet } from 'react-native';
    import { GestureHandlerRootView, GestureDetector, useLongPressGesture, GestureStateManager } from 'react-native-gesture-handler';
    
    export default function TwoPressables() {
      const longPress = useLongPressGesture({
        onTouchesDown: (e) => {
          'worklet';
          console.log("touches down")
          GestureStateManager.begin(e.handlerTag)
          GestureStateManager.activate(e.handlerTag)
        },
        onActivate: () => {
          'worklet';
          console.log("long pressed")
    
        },
        minDuration: 100000000,
        disableReanimated: true
      })
      return (
        <GestureHandlerRootView>
          <View style={styles.root}>
            <GestureDetector gesture={longPress}>
              <View style={styles.outer}>
                <Text style={styles.label}>Outer</Text>
              </View>
            </GestureDetector>
          </View>
        </GestureHandlerRootView>
      );
    }
    
    const styles = StyleSheet.create({
      root: {
        flex: 1,
        justifyContent: 'center',
        alignItems: 'center',
        backgroundColor: '#f7f7f7',
      },
      outer: {
        padding: 20,
        backgroundColor: '#ddd',
        borderRadius: 12,
      },
      label: {
        fontSize: 18,
        marginBottom: 10,
      },
    });
    ```
    </details>

---
+++ packages/react-native-gesture-handler/src/v3/gestureStateManager.ts
@@ -0,0 +1,43 @@
+import { State } from '../State';
+import { tagMessage } from '../utils';
+
+export type GestureStateManagerType = {
+  begin(handlerTag: number): void;
+  activate(handlerTag: number): void;
+  fail(handlerTag: number): void;
+  end(handlerTag: number): void;
+};
+
+const setGestureState = (handlerTag: number, state: State) => {
+  'worklet';
+
+  if (globalThis._setGestureStateSync) {
+    globalThis._setGestureStateSync(handlerTag, state);
+  } else if (globalThis._setGestureStateAsync) {
+    globalThis._setGestureStateAsync(handlerTag, state);
+  } else {
+    throw new Error(tagMessage('Failed to set gesture state'));
+  }
+};
+
+export const GestureStateManager: GestureStateManagerType = {
+  begin(handlerTag: number) {
+    'worklet';
+    setGestureState(handlerTag, State.BEGAN);
+  },
+
+  activate(handlerTag: number) {
+    'worklet';
+    setGestureState(handlerTag, State.ACTIVE);
+  },
+
+  fail(handlerTag: number) {
+    'worklet';
+    setGestureState(handlerTag, State.FAILED);
+  },
+
+  end(handlerTag: number) {
+    'worklet';
+    setGestureState(handlerTag, State.END);
+  },
+};
