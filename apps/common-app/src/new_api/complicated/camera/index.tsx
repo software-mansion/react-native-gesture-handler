d700fbdee8195eda0b656ad3ea2a3e5013b3cf20
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2026-01-14 20:10:04 +0100

    split camera

--- apps/common-app/src/new_api/complicated/camera/index.tsx
+++ apps/common-app/src/new_api/complicated/camera/index.tsx
@@ -10,17 +10,11 @@ import {
   usePanGesture,
   useExclusiveGestures,
 } from 'react-native-gesture-handler';
-import Animated, {
-  SharedValue,
-  runOnJS,
-  useAnimatedProps,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated';
-import { Circle, Svg } from 'react-native-svg';
+import { runOnJS, useSharedValue, withTiming } from 'react-native-reanimated';
 import AnimatedCameraView from '../../../common_assets/AnimatedCameraView/AnimatedCameraView';
 import { COLORS } from '../../../common';
+import { FilterCarousel, FilterOverlay } from './filters';
+import { CaptureButton } from './capture';
 
 const FILTERS = [
   COLORS.RED,
@@ -32,9 +26,6 @@ const FILTERS = [
 const CAROUSEL_SIZE = 100;
 const FILTER_SIZE = 60;
 const VIDEO_DURATION = 20000;
-const RECORD_INDICATOR_STROKE = 10;
-
-const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 export default function Camera() {
   const [facing, setFacing] = useState<'front' | 'back'>('back');
@@ -134,7 +125,11 @@ export default function Camera() {
         <FilterOverlay filters={FILTERS} selected={selectedFilter} />
         <GestureDetector gesture={filterChangeGesture}>
           <View style={styles.carouselContainer}>
-            <FilterCarousel filters={FILTERS} selected={selectedFilter} />
+            <FilterCarousel
+              filters={FILTERS}
+              selected={selectedFilter}
+              filterSize={FILTER_SIZE}
+            />
             <GestureDetector gesture={exclusiveGesture}>
               <CaptureButton progress={captureProgress} />
             </GestureDetector>
@@ -142,103 +137,6 @@ export default function Camera() {
         </GestureDetector>
       </View>
     </GestureDetector>
-  );
-}
-
-interface FilterCarouselProps {
-  filters: string[];
-  selected: SharedValue<number>;
-}
-
-function FilterCarousel(props: FilterCarouselProps) {
-  const style = useAnimatedStyle(() => {
-    return {
-      flexDirection: 'row',
-      position: 'absolute',
-      left: '50%',
-      gap: FILTER_SIZE * 0.4,
-      transform: [
-        {
-          translateX:
-            -FILTER_SIZE / 2 - FILTER_SIZE * 1.4 * props.selected.value,
-        },
-      ],
-    };
-  });
-
-  return (
-    <Animated.View style={style}>
-      {props.filters.map((filter) => (
-        <View
-          key={filter}
-          style={{
-            backgroundColor: filter,
-            width: FILTER_SIZE,
-            height: FILTER_SIZE,
-            borderRadius: FILTER_SIZE / 2,
-          }}
-        />
-      ))}
-    </Animated.View>
-  );
-}
-
-interface FilterOverlayProps {
-  filters: string[];
-  selected: SharedValue<number>;
-}
-
-export function FilterOverlay(props: FilterOverlayProps) {
-  const style = useAnimatedStyle(() => {
-    const progress = props.selected.value % 1;
-
-    return {
-      opacity: 0.3 * (Math.abs(progress - 0.5) * 2),
-      backgroundColor: props.filters[Math.round(props.selected.value)],
-    };
-  });
-
-  return (
-    <Animated.View
-      style={[StyleSheet.absoluteFill, style]}
-      pointerEvents={'none'}
-    />
-  );
-}
-
-interface CaptureButtonProps {
-  progress: SharedValue<number>;
-}
-
-function CaptureButton(props: CaptureButtonProps) {
-  const radius = CAROUSEL_SIZE / 2;
-  const svgRadius = CAROUSEL_SIZE / 2 - RECORD_INDICATOR_STROKE * 0.5;
-  const circumference = svgRadius * 2 * Math.PI;
-
-  const animatedProps = useAnimatedProps(() => {
-    const svgProgress = 100 - props.progress.value * 100;
-    return {
-      strokeDashoffset: svgRadius * Math.PI * 2 * (svgProgress / 100),
-    };
-  });
-
-  return (
-    <Animated.View style={styles.shutterContainer}>
-      <Animated.View style={styles.shutterButtonBackground} />
-      <Svg style={styles.shutterButtonRecordingIndicator}>
-        <AnimatedCircle
-          cx={radius}
-          cy={radius}
-          r={svgRadius}
-          stroke={'red'}
-          strokeLinecap="round"
-          fill={'transparent'}
-          strokeDasharray={`${circumference} ${circumference}`}
-          strokeWidth={RECORD_INDICATOR_STROKE}
-          animatedProps={animatedProps}
-        />
-      </Svg>
-    </Animated.View>
   );
 }
 
@@ -253,29 +151,5 @@ const styles = StyleSheet.create({
     height: CAROUSEL_SIZE,
     width: '100%',
     justifyContent: 'center',
-  },
-  shutterContainer: {
-    position: 'absolute',
-    top: 0,
-    left: '50%',
-    width: CAROUSEL_SIZE,
-    height: CAROUSEL_SIZE,
-    transform: [{ translateX: -CAROUSEL_SIZE / 2 }],
-  },
-  shutterButtonBackground: {
-    width: CAROUSEL_SIZE,
-    height: CAROUSEL_SIZE,
-    borderRadius: CAROUSEL_SIZE / 2,
-    borderWidth: RECORD_INDICATOR_STROKE,
-    borderColor: 'white',
-    position: 'absolute',
-  },
-  shutterButtonRecordingIndicator: {
-    width: CAROUSEL_SIZE,
-    height: CAROUSEL_SIZE,
-    position: 'absolute',
-    top: 0,
-    left: '50%',
-    transform: [{ translateX: -CAROUSEL_SIZE / 2 }, { rotateZ: '-90deg' }],
   },
 });
