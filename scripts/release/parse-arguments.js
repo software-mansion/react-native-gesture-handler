e85adbb21c0d72e7a007cd7b5694a3e2e16790d5
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-01-29 10:56:19 +0100

    Move parseArguments to a separate file

---
+++ scripts/release/parse-arguments.js
@@ -0,0 +1,58 @@
+const assert = require('assert');
+
+const ReleaseType = {
+  STABLE: 'stable',
+  BETA: 'beta',
+  RELEASE_CANDIDATE: 'rc',
+  COMMITLY: 'commitly',
+};
+
+function parseArguments() {
+  let version = null;
+  let isCommitly = false;
+  let isBeta = false;
+  let isReleaseCandidate = false;
+
+  for (let i = 2; i < process.argv.length; i++) {
+    const arg = process.argv[i];
+    if (arg === '--commitly') {
+      isCommitly = true;
+    } else if (arg === '--beta') {
+      isBeta = true;
+    } else if (arg === '--rc') {
+      isReleaseCandidate = true;
+    } else if (arg === '--version') {
+      if (i + 1 < process.argv.length) {
+        version = process.argv[i + 1];
+        i++;
+      } else {
+        throw new Error('Expected a version after --version');
+      }
+    }
+  }
+
+  assert([isCommitly, isBeta, isReleaseCandidate].filter(Boolean).length <= 1, 'Release flags --commitly, --beta, and --rc are mutually exclusive; specify at most one');
+  assert(version === null || isBeta || isReleaseCandidate, 'Version should not be provided for stable nor commitly releases');
+
+  const releaseType = isCommitly
+    ? ReleaseType.COMMITLY
+    : isBeta
+      ? ReleaseType.BETA
+      : isReleaseCandidate
+        ? ReleaseType.RELEASE_CANDIDATE
+        : ReleaseType.STABLE;
+
+  if (version != null) {
+    const versionRegex = /^(\d+)\.(\d+)\.(\d+)$/;
+    if (!versionRegex.test(version)) {
+      throw new Error(`Provided version "${version}" is not valid. Expected format: x.y.z`);
+    }
+  }
+
+  return { releaseType, version };
+}
+
+module.exports = {
+  ReleaseType,
+  parseArguments,
+};
--- scripts/release/set-package-version.js
+++ scripts/release/set-package-version.js
@@ -2,61 +2,9 @@ const fs = require('fs');
 const { execSync } = require('child_process');
 const { getPackageVersionByTag } = require('./npm-utils');
 const { parseVersion, getStableBranchVersion } = require('./version-utils');
-const assert = require('assert');
+const { parseArguments, ReleaseType } = require('./parse-arguments');
 
 const PACKAGE_PATH = './packages/react-native-gesture-handler/package.json';
-
-const ReleaseType = {
-  STABLE: 'stable',
-  BETA: 'beta',
-  RELEASE_CANDIDATE: 'rc',
-  COMMITLY: 'commitly',
-};
-
-function parseArguments() {
-  let version = null;
-  let isCommitly = false;
-  let isBeta = false;
-  let isReleaseCandidate = false;
-
-  for (let i = 2; i < process.argv.length; i++) {
-    const arg = process.argv[i];
-    if (arg === '--commitly') {
-      isCommitly = true;
-    } else if (arg === '--beta') {
-      isBeta = true;
-    } else if (arg === '--rc') {
-      isReleaseCandidate = true;
-    } else if (arg === '--version') {
-      if (i + 1 < process.argv.length) {
-        version = process.argv[i + 1];
-        i++;
-      } else {
-        throw new Error('Expected a version after --version');
-      }
-    }
-  }
-
-  assert([isCommitly, isBeta, isReleaseCandidate].filter(Boolean).length <= 1, 'Release flags --commitly, --beta, and --rc are mutually exclusive; specify at most one');
-  assert(version === null || isBeta || isReleaseCandidate || !isCommitly, 'Version should not be provided for stable nor commitly releases');
-
-  const releaseType = isCommitly
-    ? ReleaseType.COMMITLY
-    : isBeta
-      ? ReleaseType.BETA
-      : isReleaseCandidate
-        ? ReleaseType.RELEASE_CANDIDATE
-        : ReleaseType.STABLE;
-
-  if (version != null) {
-    const versionRegex = /^(\d+)\.(\d+)\.(\d+)$/;
-    if (!versionRegex.test(version)) {
-      throw new Error(`Provided version "${version}" is not valid. Expected format: x.y.z`);
-    }
-  }
-
-  return { releaseType, version };
-}
 
 function getLatestVersion() {
   const latestVersion = getPackageVersionByTag('react-native-gesture-handler', 'latest');
