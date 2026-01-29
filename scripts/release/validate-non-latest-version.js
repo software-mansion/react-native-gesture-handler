8bbbb25448d2afa31f9b1d59ba7081d80a66fc4f
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-01-29 12:40:26 +0100

    Validate non-latest versions

---
+++ scripts/release/validate-non-latest-version.js
@@ -0,0 +1,30 @@
+const { getPackageVersionByTag } = require('./npm-utils');
+const { parseVersion } = require('./version-utils');
+
+function versionExists(version) {
+  try {
+    getPackageVersionByTag('react-native-gesture-handler', version);
+    return true;
+  } catch (error) {
+    return false;
+  }
+}
+
+function validateNonLatestVersion(version) {
+  const [newMajor, newMinor, newPatch, newPreRelease] = parseVersion(version);
+  
+  if (versionExists(`${newMajor}.${newMinor}.${newPatch}`)) {
+    throw new Error(`Version ${newMajor}.${newMinor}.${newPatch} already exists in the npm registry`);
+  }
+
+  return true;
+}
+
+if (require.main === module) {
+  const version = process.argv[2];
+  console.log(validateNonLatestVersion(version));
+}
+
+module.exports = {
+  validateNonLatestVersion,
+};
