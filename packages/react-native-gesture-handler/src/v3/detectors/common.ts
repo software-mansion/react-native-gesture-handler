b2556187e7c4bd45745eed8ab1b8f889a0ffa0f2
Andrzej Antoni Kwaśniewski <andrzej.kwasniewski@swmansion.com>
2026-02-18 12:14:38 +0100

    not duplicating children

--- packages/react-native-gesture-handler/src/v3/detectors/common.ts
+++ packages/react-native-gesture-handler/src/v3/detectors/common.ts
@@ -31,7 +31,6 @@ export interface InterceptingGestureDetectorProps<THandlerData, TConfig>
 // TODO: Handle CommonGestureDetectorProps inside VirtualGestureDetector
 export interface VirtualDetectorProps<THandlerData, TConfig>
   extends CommonGestureDetectorProps {
-  children?: React.ReactNode;
   gesture: Gesture<THandlerData, TConfig>;
 }
 
