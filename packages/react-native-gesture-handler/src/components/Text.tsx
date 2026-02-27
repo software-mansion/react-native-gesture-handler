f0c0f11b8ece61052d576bc1baaee701154eb2c1
Michał <michal.bert@swmansion.com>
2026-02-27 11:57:26 +0100

    Remove forwardedRef

--- packages/react-native-gesture-handler/src/components/Text.tsx
+++ packages/react-native-gesture-handler/src/components/Text.tsx
@@ -1,10 +1,4 @@
-import React, {
-  ForwardedRef,
-  forwardRef,
-  RefObject,
-  useEffect,
-  useRef,
-} from 'react';
+import React, { Ref, RefObject, useEffect, useRef } from 'react';
 import {
   Platform,
   Text as RNText,
@@ -14,64 +8,62 @@ import {
 import { GestureObjects as Gesture } from '../handlers/gestures/gestureObjects';
 import { GestureDetector } from '../handlers/gestures/GestureDetector';
 
-export const Text = forwardRef(
-  (
-    props: RNTextProps,
-    ref: ForwardedRef<React.ComponentRef<typeof RNText>>
-  ) => {
-    const { onPress, onLongPress, ...rest } = props;
+type TextProps = RNTextProps & { ref?: Ref<RNText | null> };
 
-    const textRef = useRef<RNText | null>(null);
-    const native = Gesture.Native().runOnJS(true);
+export const Text = (props: TextProps) => {
+  const { onPress, onLongPress, ...rest } = props;
 
-    const refHandler = (node: any) => {
-      textRef.current = node;
+  const textRef = useRef<RNText | null>(null);
+  const native = Gesture.Native().runOnJS(true);
 
-      if (ref === null) {
-        return;
-      }
+  const refHandler = (node: RNText | null) => {
+    textRef.current = node;
 
-      if (typeof ref === 'function') {
-        ref(node);
-      } else {
-        ref.current = node;
-      }
-    };
+    if (!props.ref) {
+      return;
+    }
 
-    // This is a special case for `Text` component. After https://github.com/software-mansion/react-native-gesture-handler/pull/3379 we check for
-    // `displayName` field. However, `Text` from RN has this field set to `Text`, but is also present in `RNSVGElements` set.
-    // We don't want to treat our `Text` as the one from `SVG`, therefore we add special field to ref.
-    refHandler.rngh = true;
+    if (typeof props.ref === 'function') {
+      props.ref(node);
+    } else {
+      props.ref.current = node;
+    }
+  };
 
-    useEffect(() => {
-      if (Platform.OS !== 'web') {
-        return;
-      }
+  // This is a special case for `Text` component. After https://github.com/software-mansion/react-native-gesture-handler/pull/3379 we check for
+  // `displayName` field. However, `Text` from RN has this field set to `Text`, but is also present in `RNSVGElements` set.
+  // We don't want to treat our `Text` as the one from `SVG`, therefore we add special field to ref.
+  refHandler.rngh = true;
 
-      const textElement = ref
-        ? (ref as RefObject<React.ComponentRef<typeof RNText>>).current
-        : textRef.current;
+  useEffect(() => {
+    if (Platform.OS !== 'web') {
+      return;
+    }
 
-      // At this point we are sure that textElement is div in HTML tree
-      (textElement as unknown as HTMLDivElement)?.setAttribute(
-        'rnghtext',
-        'true'
-      );
-    }, []);
+    const textElement = props.ref
+      ? (props.ref as RefObject<React.ComponentRef<typeof RNText>>).current
+      : textRef.current;
 
-    return onPress || onLongPress ? (
-      <GestureDetector gesture={native}>
-        <RNText
-          onPress={onPress}
-          onLongPress={onLongPress}
-          ref={refHandler}
-          {...rest}
-        />
-      </GestureDetector>
-    ) : (
-      <RNText ref={ref} {...rest} />
+    // At this point we are sure that textElement is div in HTML tree
+    (textElement as unknown as HTMLDivElement)?.setAttribute(
+      'rnghtext',
+      'true'
     );
-  }
-);
+  }, []);
+
+  return onPress || onLongPress ? (
+    <GestureDetector gesture={native}>
+      <RNText
+        onPress={onPress}
+        onLongPress={onLongPress}
+        ref={refHandler}
+        {...rest}
+      />
+    </GestureDetector>
+  ) : (
+    <RNText ref={props.ref} {...rest} />
+  );
+};
+
 // eslint-disable-next-line @typescript-eslint/no-redeclare
 export type Text = typeof Text & RNText;
