16c73ded09aa15b54769e1c75276a0305122ce6c
Michał <michal.bert@swmansion.com>
2026-03-05 10:45:32 +0100

    Extarct type

--- packages/react-native-gesture-handler/src/components/GestureHandlerButton.web.tsx
+++ packages/react-native-gesture-handler/src/components/GestureHandlerButton.web.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { View, ViewProps } from 'react-native';
 
-export const ButtonComponent = (
-  props: ViewProps & { ref?: React.Ref<React.ComponentRef<typeof View>> }
-) => {
-  return <View accessibilityRole="button" {...props} />;
+type ButtonProps = ViewProps & {
+  ref?: React.Ref<React.ComponentRef<typeof View>>;
 };
+
+export const ButtonComponent = (props: ButtonProps) => (
+  <View accessibilityRole="button" {...props} />
+);
 
 export default ButtonComponent;
