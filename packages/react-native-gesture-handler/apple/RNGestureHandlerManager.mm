f46080a5c3448dce1f8cdf5b2c395cc55d72163a
Michał Bert <63123542+m-bert@users.noreply.github.com>
2026-03-05 10:49:42 +0100

    [iOS] Fix concurrent modification crash (#4008)
    ## Description
    
    In `reattachHandlersIfNeeded` method we are iterating over handlers
    contained in registry. The following crash was reported:
    
    ```
    NSDictionary was mutated while being enumerated
    ```
    
    This PR adds `@synchronized` on `registry._handlers` so attempting to
    modify this value should be safe.
    
    > [!NOTE]
    > For now we have no consistent reproduction. I'm also not sure where
    this concurrent modification takes place. It seems to be indirect, as
    previous keyframe in provided stack trace was `flushOperations`.
    
    
    ## Test plan
    
    Tested on the example code from #3964

--- packages/react-native-gesture-handler/apple/RNGestureHandlerManager.mm
+++ packages/react-native-gesture-handler/apple/RNGestureHandlerManager.mm
@@ -250,15 +250,19 @@ constexpr int NEW_ARCH_NUMBER_OF_ATTACH_RETRIES = 25;
   // display:none and siblings change, the native UIView backing a component may be recycled
   // and replaced. maybeBindHandler is a no-op if the view is nil or unchanged. This is only
   // needed for handlers using the old api.
-  for (RNGestureHandler *handler in _registry.handlers.objectEnumerator) {
-    if (handler.viewTag == nil || [handler usesNativeOrVirtualDetector]) {
-      continue;
-    }
+  NSDictionary *handlers = _registry.handlers;
 
-    [self maybeBindHandler:handler.tag
-             toViewWithTag:handler.viewTag
-            withActionType:handler.actionType
-          withHostDetector:nil];
+  @synchronized(handlers) {
+    for (RNGestureHandler *handler in handlers.objectEnumerator) {
+      if (handler.viewTag == nil || [handler usesNativeOrVirtualDetector]) {
+        continue;
+      }
+
+      [self maybeBindHandler:handler.tag
+               toViewWithTag:handler.viewTag
+              withActionType:handler.actionType
+            withHostDetector:nil];
+    }
   }
 }
 
