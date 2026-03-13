3ad3ca2bbd6b71321eecfdc9255be7c4b544cda8
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-03-13 15:10:38 +0100

    Update legacy borderless button

--- packages/react-native-gesture-handler/src/components/GestureButtons.tsx
+++ packages/react-native-gesture-handler/src/components/GestureButtons.tsx
@@ -228,6 +228,7 @@ class InnerBorderlessButton extends React.Component<BorderlessButtonWithRefProps
   static defaultProps = {
     activeOpacity: 0.3,
     borderless: true,
+    foreground: true,
   };
 
   private opacity: Animated.Value;
