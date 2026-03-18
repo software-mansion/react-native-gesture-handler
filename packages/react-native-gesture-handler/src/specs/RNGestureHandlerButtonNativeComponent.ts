cdd368132ff633447dc95910ef695696fce7c8ae
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-03-18 10:02:51 +0100

    Update native button spec

--- packages/react-native-gesture-handler/src/specs/RNGestureHandlerButtonNativeComponent.ts
+++ packages/react-native-gesture-handler/src/specs/RNGestureHandlerButtonNativeComponent.ts
@@ -22,6 +22,14 @@ interface NativeProps extends ViewProps {
     'box-none' | 'none' | 'box-only' | 'auto',
     'auto'
   >;
+  animationDuration?: WithDefault<Int32, 100>;
+  activeOpacity?: WithDefault<Float, 1>;
+  activeScale?: WithDefault<Float, 1>;
+  activeUnderlayOpacity?: WithDefault<Float, 0>;
+  startOpacity?: WithDefault<Float, 1>;
+  startScale?: WithDefault<Float, 1>;
+  startUnderlayOpacity?: WithDefault<Float, 0>;
+  underlayColor?: ColorValue;
 }
 
 export default codegenNativeComponent<NativeProps>('RNGestureHandlerButton');
