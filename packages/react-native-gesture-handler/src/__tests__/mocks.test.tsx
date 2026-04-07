58ea208e10e680405c440c69e91d40dfa13e6038
Michał <michal.bert@swmansion.com>
2026-04-07 12:27:50 +0200

    Remove unused mocks

--- packages/react-native-gesture-handler/src/__tests__/mocks.test.tsx
+++ packages/react-native-gesture-handler/src/__tests__/mocks.test.tsx
@@ -20,66 +20,8 @@ import {
 
 import LegacyPressable from '../mocks/Pressable';
 
-import {
-  RawButton,
-  BaseButton,
-  RectButton,
-  BorderlessButton,
-  Clickable,
-  ScrollView,
-  FlatList,
-  Switch,
-  TextInput,
-  RefreshControl,
-} from '../mocks/v3GestureComponents';
-
-describe('Jest mocks – v3 components render without crashing', () => {
-  test('RawButton', () => {
-    expect(() => render(<RawButton />)).not.toThrow();
-  });
-
-  test('BaseButton', () => {
-    expect(() => render(<BaseButton />)).not.toThrow();
-  });
-
-  test('RectButton', () => {
-    expect(() => render(<RectButton />)).not.toThrow();
-  });
-
-  test('BorderlessButton', () => {
-    expect(() => render(<BorderlessButton />)).not.toThrow();
-  });
-
-  test('Clickable', () => {
-    expect(() => render(<Clickable />)).not.toThrow();
-  });
-
-  test('Pressable (v3 shares legacy mock)', () => {
-    expect(() => render(<LegacyPressable />)).not.toThrow();
-  });
-
-  test('ScrollView', () => {
-    expect(() => render(<ScrollView />)).not.toThrow();
-  });
-
-  test('FlatList', () => {
-    expect(() =>
-      render(<FlatList data={[]} renderItem={() => null} />)
-    ).not.toThrow();
-  });
-
-  test('Switch', () => {
-    expect(() => render(<Switch />)).not.toThrow();
-  });
-
-  test('TextInput', () => {
-    expect(() => render(<TextInput />)).not.toThrow();
-  });
-
-  test('RefreshControl', () => {
-    expect(() => render(<RefreshControl refreshing={false} />)).not.toThrow();
-  });
-});
+import GestureHandlerRootView from '../components/GestureHandlerRootView';
+import { Clickable } from '../v3/components';
 
 describe('Jest mocks – legacy components render without crashing', () => {
   test('LegacyRawButton', () => {
@@ -134,9 +76,11 @@ describe('Jest mocks – legacy components render without crashing', () => {
 test('Trigger press by text', () => {
   const onPress = jest.fn();
   const { getByText } = render(
-    <Clickable activeOpacity={0.3} onPress={onPress}>
-      <Text>Press Me</Text>
-    </Clickable>
+    <GestureHandlerRootView>
+      <Clickable activeOpacity={0.3} onPress={onPress}>
+        <Text>Press Me</Text>
+      </Clickable>
+    </GestureHandlerRootView>
   );
 
   fireEvent.press(getByText('Press Me'));
