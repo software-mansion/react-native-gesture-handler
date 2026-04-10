9c0fb730fb80c7e2e99ab889d221079360b63198
Michał <michal.bert@swmansion.com>
2026-04-10 10:28:31 +0200

    Change Clickable to Touchable in mocks test

--- packages/react-native-gesture-handler/src/__tests__/mocks.test.tsx
+++ packages/react-native-gesture-handler/src/__tests__/mocks.test.tsx
@@ -21,7 +21,7 @@ import {
 import LegacyPressable from '../mocks/Pressable';
 
 import GestureHandlerRootView from '../components/GestureHandlerRootView';
-import { Clickable } from '../v3/components';
+import { Touchable } from '../v3/components';
 
 describe('Jest mocks – legacy components render without crashing', () => {
   test('LegacyRawButton', () => {
@@ -77,9 +77,9 @@ test('Trigger press by text', () => {
   const onPress = jest.fn();
   const { getByText } = render(
     <GestureHandlerRootView>
-      <Clickable activeOpacity={0.3} onPress={onPress}>
+      <Touchable activeOpacity={0.3} onPress={onPress}>
         <Text>Press Me</Text>
-      </Clickable>
+      </Touchable>
     </GestureHandlerRootView>
   );
 
