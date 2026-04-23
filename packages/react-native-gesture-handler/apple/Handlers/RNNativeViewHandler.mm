ed1c7d3e8cf3823aad07b222fadad4ae844375ad
Michał <michal.bert@swmansion.com>
2026-04-23 12:13:07 +0200

    Revert "Disallow interruption"
    This reverts commit be6a7111d1f2f561504def4dd5f2e2afd260c69a.

--- packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
+++ packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
@@ -201,27 +201,8 @@
   [self sendEventsInState:RNGestureHandlerStateActive forViewWithTag:sender.reactTag withExtraData:extraData];
 }
 
-- (void)startInteraction:(UIEvent *)event
-{
-  [self setCurrentPointerTypeForEvent:event];
-  [self reset];
-
-  if (_disallowInterruption) {
-    // When `disallowInterruption` is set we cancel all gesture handlers when this UIControl
-    // gets DOWN event
-    for (RNGHUITouch *touch in [event allTouches]) {
-      for (UIGestureRecognizer *recogn in [touch gestureRecognizers]) {
-        recogn.enabled = NO;
-        recogn.enabled = YES;
-      }
-    }
-  }
-}
-
 - (void)handleSwitch:(UIView *)sender forEvent:(UIEvent *)event
 {
-  [self startInteraction:event];
-
   [self sendEventsInState:RNGestureHandlerStateBegan
            forViewWithTag:sender.reactTag
             withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES
@@ -237,11 +218,25 @@
             withExtraData:[RNGestureHandlerEventExtraData forPointerInside:YES
                                                        withNumberOfTouches:event.allTouches.count
                                                            withPointerType:_pointerType]];
+
+  [self reset];
 }
 
 - (void)handleTouchDown:(UIView *)sender forEvent:(UIEvent *)event
 {
-  [self startInteraction:event];
+  [self setCurrentPointerTypeForEvent:event];
+  [self reset];
+
+  if (_disallowInterruption) {
+    // When `disallowInterruption` is set we cancel all gesture handlers when this UIControl
+    // gets DOWN event
+    for (RNGHUITouch *touch in [event allTouches]) {
+      for (UIGestureRecognizer *recogn in [touch gestureRecognizers]) {
+        recogn.enabled = NO;
+        recogn.enabled = YES;
+      }
+    }
+  }
 
   [self sendEventsInState:RNGestureHandlerStateBegan
            forViewWithTag:sender.reactTag
