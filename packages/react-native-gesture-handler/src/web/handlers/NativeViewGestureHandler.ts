b18dbd1dd4cffd3af947ca303d9ce81b83705d5a
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-04-23 14:42:32 +0200

    [Web] Fix nested touchables and gestures (#4108)
    ## Description
    
    1. Adds `stopPropagation` call to event handlers in `Touchable` so only
    the inner-most one triggers the active animation.
    2. Changes when buttons without `shouldActivateOnStart` (so `Touchable`)
    activate - instead of doing it on pressDown, now it happens during
    pressUp. This ensures that all other handlers have been extracted and
    can be canceled/can cancel `Touchable` correctly.
    3. Behavior for buttons with `shouldActivateOnStart` (other buttons)
    shouldn't change after this PR, but the proper `shouldActivateOnStart`
    implementation is still to be done.
    
    ## Test plan
    
    Tested on the example from
    https://github.com/software-mansion/react-native-gesture-handler/pull/4106

--- packages/react-native-gesture-handler/src/web/handlers/NativeViewGestureHandler.ts
+++ packages/react-native-gesture-handler/src/web/handlers/NativeViewGestureHandler.ts
@@ -15,8 +15,7 @@ import type IGestureHandler from './IGestureHandler';
 export default class NativeViewGestureHandler extends GestureHandler {
   private buttonRole!: boolean;
 
-  // TODO: Implement logic for activation on start
-  // @ts-ignore Logic yet to be implemented
+  // TODO: Implement logic for activation on start properly
   private shouldActivateOnStart = false;
   private disallowInterruption = false;
 
@@ -72,7 +71,7 @@ export default class NativeViewGestureHandler extends GestureHandler {
     }
 
     view.style['touchAction'] = 'auto';
-    // @ts-ignore Turns on defualt touch behavior on Safari
+    // @ts-ignore Turns on default touch behavior on Safari
     view.style['WebkitTouchCallout'] = 'auto';
   }
 
@@ -102,7 +101,7 @@ export default class NativeViewGestureHandler extends GestureHandler {
     const view = this.delegate.view as HTMLElement;
     const isRNGHText = view.hasAttribute('rnghtext');
 
-    if (this.buttonRole || isRNGHText) {
+    if ((this.buttonRole && this.shouldActivateOnStart) || isRNGHText) {
       this.activate();
     }
   }
@@ -144,6 +143,10 @@ export default class NativeViewGestureHandler extends GestureHandler {
     this.tracker.removeFromTracker(event.pointerId);
 
     if (this.tracker.trackedPointersCount === 0) {
+      if (this.buttonRole && this.state === State.BEGAN) {
+        this.activate();
+      }
+
       if (this.state === State.ACTIVE) {
         this.end();
       } else {
