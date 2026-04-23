1132532f93b51535911450c34fca506a63cf7666
Mateusz Kiełb <relextm19@gmail.com>
2026-04-23 11:40:32 +0200

    fix rotation v3|

--- packages/docs-gesture-handler/static/examples/RotationGestureBasic.js
+++ packages/docs-gesture-handler/static/examples/RotationGestureBasic.js
@@ -85,7 +85,7 @@ export default function App() {
 
   const boxAnimatedStyles = useAnimatedStyle(() => ({
     transform: [{ rotate: `${angle.value}rad` }],
-    cursor: grabbing.value ? '' : 'grab',
+    cursor: grabbing.value ? 'none' : 'grab',
   }));
 
   return (
