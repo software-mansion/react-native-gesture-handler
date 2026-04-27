76df62928206d5cafb9893525d63a477dcfba187
Michał <michal.bert@swmansion.com>
2026-04-27 10:17:12 +0200

    Revert "Simplify v2"
    This reverts commit 769f84a923bbd8fc9795d2670c310a75bac5175f.

--- packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
+++ packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
@@ -17,6 +17,14 @@
 #import <React/RCTTextInputComponentView.h>
 #import <React/UIView+React.h>
 
+#if !TARGET_OS_OSX
+@interface RNNativeViewGestureHandler ()
+- (void)handleTextViewTouchDown:(UIEvent *)event;
+- (void)handleTextViewTouchUp:(UIEvent *)event;
+- (void)handleTextViewTouchCancel:(UIEvent *)event;
+@end
+#endif
+
 #pragma mark RNDummyGestureRecognizer
 
 @implementation RNDummyGestureRecognizer {
@@ -146,6 +154,8 @@
 
 - (void)bindToView:(UIView *)view
 {
+  UIView *textInputChild = nil;
+
   // For UIControl based views (UIButton, UISwitch) we provide special handling that would allow
   // for properties like `disallowInterruption` to work.
   if ([view isKindOfClass:[UIControl class]]) {
