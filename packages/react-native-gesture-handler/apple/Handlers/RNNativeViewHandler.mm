a79dc6c1c9cedd26931b9bc693d235a405100e13
Michał <michal.bert@swmansion.com>
2026-04-27 10:16:55 +0200

    Revert "Simplify v3"
    This reverts commit 9f09a7e7d609111abec721ca05ec126c3be06a59.

--- packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
+++ packages/react-native-gesture-handler/apple/Handlers/RNNativeViewHandler.mm
@@ -32,6 +32,11 @@
 }
 
 #if !TARGET_OS_OSX
+- (BOOL)isAttachedToTextView
+{
+  return [self.view isKindOfClass:[UITextView class]];
+}
+
 - (void)touchesBegan:(NSSet<RNGHUITouch *> *)touches withEvent:(UIEvent *)event
 {
   [_gestureHandler setCurrentPointerTypeForEvent:event];
@@ -47,6 +52,7 @@
 - (void)touchesEnded:(NSSet<RNGHUITouch *> *)touches withEvent:(UIEvent *)event
 {
   [_gestureHandler.pointerTracker touchesEnded:touches withEvent:event];
+
   self.state = UIGestureRecognizerStateFailed;
 
   // For now, we are handling only the scroll view case.
@@ -59,6 +65,7 @@
 - (void)touchesCancelled:(NSSet<RNGHUITouch *> *)touches withEvent:(UIEvent *)event
 {
   [_gestureHandler.pointerTracker touchesCancelled:touches withEvent:event];
+
   self.state = UIGestureRecognizerStateCancelled;
   [self reset];
 }
