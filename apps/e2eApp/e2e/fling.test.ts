25a9fd89ce46f15e74a287776009c74a1755d8b7
Mateusz Kiełb <relextm19@gmail.com>
2026-04-28 12:55:56 +0200

    updated fling tests

--- apps/e2eApp/e2e/fling.test.ts
+++ apps/e2eApp/e2e/fling.test.ts
@@ -1,49 +1,42 @@
-import { beforeAll, beforeEach, describe, it } from "@jest/globals";
-import { by, device, element, expect } from "detox";
+import { beforeAll, beforeEach, describe, it } from '@jest/globals';
+import { by, device, element, expect } from 'detox';
 
-describe("test fling gesture", () => {
+describe('test fling gesture', () => {
   beforeAll(async () => {
     await device.launchApp({ newInstance: false });
-    await element(by.id("nav-fling")).tap();
+    await element(by.id('nav-fling')).tap();
   });
 
-  const wrongElement = element(by.id("wrong-element"));
-  const flingElement = element(by.id("fling-idle"));
-  const flingActivatedElement = element(by.id("fling-activated"));
-  const resetButton = element(by.id("reset"));
+  const wrongElement = element(by.id('wrong-element'));
+  const flingElement = element(by.id('fling-idle'));
+  const flingActivatedElement = element(by.id('fling-activated'));
+  const resetButton = element(by.id('reset'));
 
   beforeEach(async () => {
     await resetButton.tap();
   });
 
-  it("shouldn`t register a fling gesture on tap", async () => {
+  it('shouldn`t register a fling gesture on tap', async () => {
     await expect(flingElement).toExist();
     await flingElement.tap();
     await expect(flingActivatedElement).not.toExist();
   });
 
-  it("shouldn`t register a fling gesture on different element", async () => {
+  it('shouldn`t register a fling gesture on different element', async () => {
     await expect(wrongElement).toExist();
-    await wrongElement.swipe("right", "fast");
+    await wrongElement.swipe('right', 'fast');
     await expect(flingActivatedElement).not.toExist();
   });
 
-  it("should register a fling gesture", async () => {
+  it('should register a fling gesture in the correct direction', async () => {
     await expect(flingElement).toExist();
-    await flingElement.swipe("right", "fast");
+    await flingElement.swipe('right', 'fast');
     await expect(flingActivatedElement).toExist();
   });
 
-  it("multiple taps shouldn`t register a fling gesture", async () => {
+  it('shouldnt register a fling gesture in the wrong direction', async () => {
     await expect(flingElement).toExist();
-    await flingElement.multiTap(3);
+    await flingElement.swipe('left', 'fast');
     await expect(flingActivatedElement).not.toExist();
-  });
-
-  it("shouldnt change state due to multiple flings", async () => {
-    await expect(flingElement).toExist();
-    await flingElement.swipe("right", "fast");
-    await flingActivatedElement.swipe("right", "fast");
-    await expect(flingActivatedElement).toExist();
   });
 });
