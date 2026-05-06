3e6846f216c2e0af06b02c9e15cd6d19fa75c1eb
Mateusz Kiełb <relextm19@gmail.com>
2026-05-06 11:51:13 +0200

    made the pinch test platfrom specific

--- apps/e2eApp/e2e/pinch.test.ts
+++ apps/e2eApp/e2e/pinch.test.ts
@@ -1,51 +1,46 @@
-import { beforeAll, beforeEach, describe, it } from "@jest/globals";
-import { by, device, element, expect } from "detox";
+import { beforeAll, beforeEach, describe, it } from '@jest/globals';
+import { by, device, element, expect } from 'detox';
 
-describe("test pinch gesture", () => {
+describe('test pinch gesture', () => {
   beforeAll(async () => {
     await device.launchApp({ newInstance: false });
-    await element(by.id("nav-gesture-tests")).tap();
-    await element(by.id("nav-pinch")).tap();
+    await element(by.id('nav-gesture-tests')).tap();
+    await element(by.id('nav-pinch')).tap();
   });
 
-  const wrongElement = element(by.id("wrong-element"));
-  const pinchElement = element(by.id("pinch-idle"));
-  const pinchActivatedElement = element(by.id("pinch-activated"));
-  const resetButton = element(by.id("reset"));
+  const wrongElement = element(by.id('wrong-element'));
+  const pinchElement = element(by.id('pinch-idle'));
+  const pinchActivatedElement = element(by.id('pinch-activated'));
+  const resetButton = element(by.id('reset'));
 
   beforeEach(async () => {
-    await expect(element(by.id("reset"))).toExist();
+    await expect(element(by.id('reset'))).toExist();
     await resetButton.tap();
   });
 
-  it("shouldn`t register a pinch gesture on tap", async () => {
+  it('shouldn`t register a pinch gesture on tap', async () => {
     await expect(pinchElement).toExist();
     await pinchElement.tap();
     await expect(pinchActivatedElement).not.toExist();
   });
 
-  it("shouldn`t register a pinch gesture on different element", async () => {
+  it('shouldn`t register a pinch gesture on different element', async () => {
+    if (device.getPlatform() !== 'ios') {
+      return;
+    }
+
     await expect(wrongElement).toExist();
-    await wrongElement.pinch(1.5, "fast");
+    await wrongElement.pinch(1.5, 'fast');
     await expect(pinchActivatedElement).not.toExist();
   });
 
-  it("should register a pinch gesture", async () => {
-    await expect(pinchElement).toExist();
-    await pinchElement.pinch(1.5, "fast");
-    await expect(pinchActivatedElement).toExist();
-  });
+  it('should register a pinch gesture', async () => {
+    if (device.getPlatform() !== 'ios') {
+      return;
+    }
 
-  it("multiple taps shouldn`t register a pinch gesture", async () => {
     await expect(pinchElement).toExist();
-    await pinchElement.multiTap(3);
-    await expect(pinchActivatedElement).not.toExist();
-  });
-
-  it("shouldnt change state due to multiple pinches", async () => {
-    await expect(pinchElement).toExist();
-    await pinchElement.pinch(1.5, "fast");
-    await pinchActivatedElement.pinch(1.2, "fast");
+    await pinchElement.pinch(1.5, 'fast');
     await expect(pinchActivatedElement).toExist();
   });
 });
