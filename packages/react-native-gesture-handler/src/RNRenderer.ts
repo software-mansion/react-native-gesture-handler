ddb3e4dfdf1d40cbc568ea915c53ae7ea630625e
Mathieu Acthernoene <zoontek@gmail.com>
2026-05-08 15:09:49 +0200

    Fix `RNRenderer` import for React Native 0.86+

--- packages/react-native-gesture-handler/src/RNRenderer.ts
+++ packages/react-native-gesture-handler/src/RNRenderer.ts
@@ -1,3 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-nocheck
-export { default as RNRenderer } from 'react-native/Libraries/Renderer/shims/ReactNative';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let RNRenderer: any;
+
+try {
+  // React Native >= 0.86 removed `Libraries/Renderer/shims/ReactNative`.
+  RNRenderer = require('react-native/Libraries/ReactNative/RendererProxy');
+} catch (e) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const shim = require('react-native/Libraries/Renderer/shims/ReactNative');
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  RNRenderer = shim.default;
+}
+
+export { RNRenderer };
