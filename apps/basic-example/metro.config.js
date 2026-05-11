0de69dc2f53d36159323fc7819e9671d389a1413
Mateusz Kiełb <relextm19@gmail.com>
2026-05-11 11:12:38 +0200

    updated worklet and react-native versions

--- apps/basic-example/metro.config.js
+++ apps/basic-example/metro.config.js
@@ -2,21 +2,16 @@ const { getDefaultConfig } = require('@react-native/metro-config');
 const { mergeConfig } = require('metro-config');
 
 const path = require('path');
-
 const exclusionList =
   require('metro-config/private/defaults/exclusionList').default;
 const escape = require('escape-string-regexp');
-const pack = require('./package.json');
 
-const modulesBlacklist = Object.keys(pack.dependencies);
-modulesBlacklist.push(...Object.keys(pack.devDependencies));
-modulesBlacklist.push('react-native-gesture-handler');
+const modulesBlacklist = [];
 
 const projectRoot = __dirname;
 const monorepoRoot = path.resolve(projectRoot, '../..');
-const commonAppRoot = path.resolve(monorepoRoot, 'apps/common-app');
 
-const config = getDefaultConfig(projectRoot);
+const config = getDefaultConfig(__dirname);
 
 config.watchFolders = [monorepoRoot];
 
@@ -26,10 +21,9 @@ config.resolver.nodeModulesPaths = [
 ];
 
 config.resolver.blacklistRE = exclusionList(
-  [monorepoRoot, commonAppRoot].flatMap((root) =>
-    modulesBlacklist.map(
-      (m) => new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-    )
+  modulesBlacklist.map(
+    (m) =>
+      new RegExp(`^${escape(path.join(monorepoRoot, 'node_modules', m))}\\/.*$`)
   )
 );
 
@@ -39,3 +33,5 @@ config.transformer.getTransformOptions = async () => ({
     inlineRequires: true,
   },
 });
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
