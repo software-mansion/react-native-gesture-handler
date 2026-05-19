b43d8922014654c336c33b9ef31f5470f84c7ae1
Jakub Piasecki <jakub.piasecki@swmansion.com>
2026-05-19 08:16:02 +0200

    Snap on sub-frame animations

--- packages/react-native-gesture-handler/android/src/main/java/com/swmansion/gesturehandler/react/Extensions.kt
+++ packages/react-native-gesture-handler/android/src/main/java/com/swmansion/gesturehandler/react/Extensions.kt
@@ -1,10 +1,12 @@
 package com.swmansion.gesturehandler.react
 
 import android.content.Context
+import android.view.Display
 import android.view.MotionEvent
 import android.view.accessibility.AccessibilityManager
 import com.facebook.react.bridge.ReactContext
 import com.facebook.react.modules.core.DeviceEventManagerModule
+import kotlin.compareTo
 
 val ReactContext.deviceEventEmitter: DeviceEventManagerModule.RCTDeviceEventEmitter
   get() = this.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
@@ -15,3 +17,17 @@ fun Context.isScreenReaderOn() =
 fun MotionEvent.isHoverAction(): Boolean = action == MotionEvent.ACTION_HOVER_MOVE ||
   action == MotionEvent.ACTION_HOVER_ENTER ||
   action == MotionEvent.ACTION_HOVER_EXIT
+
+val Display.minimumFrameTime: Float
+  get() {
+    val supportedModes = this.supportedModes
+    var maxRefreshRate = 0f
+
+    supportedModes.forEach { mode ->
+      if (mode.refreshRate > maxRefreshRate) {
+        maxRefreshRate = mode.refreshRate
+      }
+    }
+
+    return 1000.0f / maxRefreshRate
+  }
