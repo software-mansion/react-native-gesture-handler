0a64f9690a6d752ac1b899567ae94f703e8d47dd
Michał <michal.bert@swmansion.com>
2026-06-03 12:15:28 +0200

    Handled disabled

--- packages/react-native-gesture-handler/apple/RNGestureHandlerButtonComponentView.mm
+++ packages/react-native-gesture-handler/apple/RNGestureHandlerButtonComponentView.mm
@@ -71,21 +71,37 @@ static RNGestureHandlerPointerEvents RCTPointerEventsToEnum(facebook::react::Poi
 #if TARGET_OS_TV
 - (void)emitPressInEvent
 {
+  if (!_buttonView.userEnabled) {
+    return;
+  }
+
   [_buttonView sendActionsForControlEvents:UIControlEventTouchDown];
 }
 
 - (void)emitPressOutEvent
 {
+  if (!_buttonView.userEnabled) {
+    return;
+  }
+
   [_buttonView sendActionsForControlEvents:UIControlEventTouchUpInside];
 }
 
 - (void)animatePressIn
 {
+  if (!_buttonView.userEnabled) {
+    return;
+  }
+
   [_buttonView handleAnimatePressIn];
 }
 
 - (void)animatePressOut
 {
+  if (!_buttonView.userEnabled) {
+    return;
+  }
+
   [_buttonView handleAnimatePressOut];
 }
 #endif // TARGET_OS_TV
