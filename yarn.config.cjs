96dfc0099613a95a85b104f6dc7f2c1b666b5b8a
Michał Bert <63123542+m-bert@users.noreply.github.com>
2026-06-10 13:03:34 +0200

    Support React Native 0.86 (#4166)
    ## Description
    
    This PR brings support for React Native 0.86
    
    ## Required changes
    
    -
    https://github.com/software-mansion/react-native-gesture-handler/pull/4160
    - Module has not been registered as callable
    ([6c29e4e](https://github.com/software-mansion/react-native-gesture-handler/commit/6c29e4ed721886a66c5400f47f770c8d396d8501))
    
    ## Status
    
    - ## 0.86.0 ✅
    - 0.86.0-rc.3 ✅
    - 0.86.0-rc.2 ✅
    - 0.86.0-rc.1 ✅
    - 0.86.0-rc.0 ✅
     
    ## Test plan
    
    Tested that `basic-example` builds and works correctly.

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   // eslint-disable-next-line @typescript-eslint/require-await
   async constraints({ Yarn }) {
     const sharedVersions = {
-      'react-native-reanimated': '~4.3.1',
-      'react-native-worklets': '0.8.1',
+      'react-native-reanimated': '~4.4.1',
+      'react-native-worklets': '0.9.1',
     };
 
     for (const [ident, expectedVersion] of Object.entries(sharedVersions)) {
