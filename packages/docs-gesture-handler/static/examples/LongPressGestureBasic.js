0eff0abe28b7a416641e6481ecdc6622972f81b4
Grzegorz Karolczyk <42680223+haxonadora@users.noreply.github.com>
2026-07-10 11:53:47 +0200

    Fix Easing import in LongPress example (#4298)
    ## Description
    
    `Easing` should be imported from `react-native-reanimated` instead of
    `react-native`. `Easing` from `react-native` is not a worklet like the
    one from `react-native-reanimated`.
    
    Using `Easing` from `react-native` here throws an error `[Worklets]
    Tried to synchronously call a Remote Function. Called "BezierEasing" on
    the UI Runtime`

--- packages/docs-gesture-handler/static/examples/LongPressGestureBasic.js
+++ packages/docs-gesture-handler/static/examples/LongPressGestureBasic.js
@@ -4,8 +4,9 @@ import {
   GestureDetector,
   GestureHandlerRootView,
 } from 'react-native-gesture-handler';
-import { Easing, StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native';
 import Animated, {
+  Easing,
   interpolateColor,
   useAnimatedStyle,
   useSharedValue,
