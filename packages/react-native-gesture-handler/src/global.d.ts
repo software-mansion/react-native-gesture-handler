f7a6120297305a1c30951049bc27cfeb13d5d2fc
Dawid Malecki <dawid.malecki21@gmail.com>
2026-07-21 17:49:45 +0200

    disable eslint no-var rule for entire global.d.ts

--- packages/react-native-gesture-handler/src/global.d.ts
+++ packages/react-native-gesture-handler/src/global.d.ts
@@ -1,17 +1,15 @@
+/* eslint-disable no-var */
 export {};
 
 declare global {
-  // eslint-disable-next-line no-var
   var _setGestureStateSync:
     | ((handlerTag: number, state: number) => void)
     | undefined;
 
-  // eslint-disable-next-line no-var
   var _setGestureStateAsync:
     | ((handlerTag: number, state: number) => void)
     | undefined;
 
   // Temporary holder used to pass the Worklets UI runtime to native code.
-  // eslint-disable-next-line no-var
   var __RNGH_UI_WORKLET_RUNTIME_HOLDER: object | undefined;
 }
