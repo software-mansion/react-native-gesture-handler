aa0dddaab6904fcec1307f69c0bad70b244d4c32
Michał <michal.bert@swmansion.com>
2026-08-12 15:06:23 +0200

    Pin nightlies

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -2,8 +2,9 @@ module.exports = {
   // eslint-disable-next-line @typescript-eslint/require-await
   async constraints({ Yarn }) {
     const sharedVersions = {
-      'react-native-reanimated': 'nightly',
-      'react-native-worklets': 'nightly',
+      'react-native-reanimated': '4.6.0-nightly-20260811-248dee712',
+      'react-native-worklets':
+        'patch:react-native-worklets@npm%3A0.12.0-nightly-20260810-fb9cb5596#~/.yarn/patches/react-native-worklets-npm-0.12.0-nightly-20260810-fb9cb5596-f08a72b88e.patch',
     };
 
     for (const [ident, expectedVersion] of Object.entries(sharedVersions)) {
