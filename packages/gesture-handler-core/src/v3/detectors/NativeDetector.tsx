ba2f1919e5ff1df1943430c1915ecb68ad1975e5
Michał <michal.bert@swmansion.com>
2026-08-25 13:13:18 +0200

    Remove unused React imports

--- packages/gesture-handler-core/src/v3/detectors/NativeDetector.tsx
+++ packages/gesture-handler-core/src/v3/detectors/NativeDetector.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import { useMemo } from 'react';
 
 import { useJSResponderHandler } from '../hooks/useJSResponderHandler';
 import { isComposedGesture } from '../hooks/utils/relationUtils';
--- packages/gesture-handler-core/src/v3/detectors/VirtualDetector/InterceptingGestureDetector.tsx
+++ packages/gesture-handler-core/src/v3/detectors/VirtualDetector/InterceptingGestureDetector.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 
 import type {
   TouchAction,
