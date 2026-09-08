d676b8f685158d573136408b2f7805e6140d5ecb
Michał <michal.bert@swmansion.com>
2026-09-08 12:02:48 +0200

    Bump dependencies

--- yarn.config.cjs
+++ yarn.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   // eslint-disable-next-line @typescript-eslint/require-await
   async constraints({ Yarn }) {
     const sharedVersions = {
-      'react-native-reanimated': '~4.4.1',
-      'react-native-worklets': '0.9.1',
+      'react-native-reanimated': '~4.6.0',
+      'react-native-worklets': '0.12.1',
     };
 
     for (const [ident, expectedVersion] of Object.entries(sharedVersions)) {
