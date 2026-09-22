dfda93b62ee0b4c21985ad7413f1495e5e16a5cc
Michał <michal.bert@swmansion.com>
2026-09-22 15:09:50 +0200

    Don't check pointers on begin

--- packages/react-native-gesture-handler/android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.kt
+++ packages/react-native-gesture-handler/android/src/main/java/com/swmansion/gesturehandler/react/RNGestureHandlerModule.kt
@@ -146,12 +146,15 @@ class RNGestureHandlerModule(reactContext: ReactApplicationContext?) :
 
     registry.getHandler(handlerTag)?.let { handler ->
       if (newState == GestureHandler.STATE_ACTIVE) {
-        if (!handler.hasTrackedPointers ||
-          (handler.state != GestureHandler.STATE_UNDETERMINED && handler.state != GestureHandler.STATE_BEGAN)
-        ) {
+        // The first touch event reaches JS before the handler begins, so a handler
+        // that already tracks a pointer may still be UNDETERMINED here.
+        if (handler.hasTrackedPointers && handler.state == GestureHandler.STATE_UNDETERMINED) {
+          handler.begin()
+        }
+
+        if (handler.state != GestureHandler.STATE_BEGAN) {
           return
         }
-        handler.begin()
         handler.recordHandlerIfNotPresent()
       }
 
